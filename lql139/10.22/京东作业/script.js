b7d00a2c3129a8962c37ee3524a8f9c64b49ad66
1123781921 <1123781921@qq.com>
2018-10-22 21:07:59 +0800

    10.22

---
+++ lql139/10.22/京东作业/script.js
@@ -0,0 +1,15 @@
+var dd=document.getElementById("ddl");
+var ss=document.getElementById("sc");
+ss.onmouseover=function(){
+    dd.className="xx";
+}
+ss.onmouseout=function(){
+    dd.className="zz";
+}
+dd.onmouseover=function(){
+    dd.className="xx";
+}
+dd.onmouseout=function(){
+    dd.className="zz";
+}
+
