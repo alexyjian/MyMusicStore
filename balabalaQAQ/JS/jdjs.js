7cd83af4880aefe4b25b11ceb788e12f90e091c9
balabalaQAQ <1124048070@qq.com>
2018-10-22 19:10:59 +0800

    10.22

---
+++ balabalaQAQ/JS/jdjs.js
@@ -0,0 +1,9 @@
+var a ,b;
+//事件
+//悬停
+a.onmouseover=function(){
+    alert('鼠标经过！');
+}
+b.onmouseout=function(){
+    alert('鼠标离家！');
+}
---
+++ balabalaQAQ/JS/js.js
@@ -0,0 +1,38 @@
+        // document.write("Hello JavaScript");
+        //  alert("Hello JS!!!");
+        //  prompt("请输入你的名字")
+
+
+        //var name=prompt('输入你的名字');
+        //console.log(name);
+        //var a="欢迎来到";
+        //console.log(a);
+        // var b="JS";
+        // console.log(b);
+        // alert(name+',欢迎你'+a+b);
+
+        //一维数组
+        // var zz,ll,yy;
+        // zz=new Array('班长','学习委员','问题委员','团支书','副班长');
+        // yy="到";
+        // //遍历数组中的元素
+        // for(ll in zz){
+        //     alert(zz[ll]);
+        //     alert(yy);
+        // }   
+
+        //二维数组
+        var student,i,j;
+        student =new Array();
+        student[0]= new Array('00','11','22');
+        student[1]= new Array('000','111','222');
+        student[2]= new Array('000','111','222');
+        /*      for(i in student)
+        {
+            for(j in student[i]){
+                document.write(student[i][j]+'\t');
+            }
+            document.write("<br>");
+        }
+        */
+ 
