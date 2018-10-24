f06c7da2e8ed14966c283e3c40a769718001fd36
1123781921 <1123781921@qq.com>
2018-10-24 18:07:14 +0800

    10.24

--- lql139/10.22/京东作业/script.js
+++ lql139/10.22/京东作业/script.js
@@ -1,15 +1,29 @@
+
+/* //方法1
 var dd=document.getElementById("ddl");
 var ss=document.getElementById("sc");
 ss.onmouseover=function(){
-    dd.className="xx";
+    dd.style.display='block';
 }
 ss.onmouseout=function(){
-    dd.className="zz";
+    dd.style.display='none';
 }
 dd.onmouseover=function(){
-    dd.className="xx";
+    dd.style.display='block';
 }
 dd.onmouseout=function(){
-    dd.className="zz";
+    dd.style.display='none';
 }
+*/
+//方法2
+function change(id,mode){
+    document.getElementById(id).style.display=mode;
+}
+ if(mode=='block'){
+    document.getElementById(id).parentNode.style.backgroundColor='#fff';
+    
+ }
+ else{
+    
+ }
 
