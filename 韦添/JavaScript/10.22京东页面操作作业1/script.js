48379db5954137babfdf74347be72ce581659704
weitian666 <1935836331@qq.com>
2018-10-24 17:25:27 +0800

    3

--- 韦添/JavaScript/10.22京东页面操作作业1/script.js
+++ 韦添/JavaScript/10.22京东页面操作作业1/script.js
@@ -1,3 +1,27 @@
-//按F12，在chrome的控制台查看变量值
-a=new Array('班长','学习委员','文体委员','团支书','副班长');
+//id传入的空间id，mode变换的属性值
+function change(id,mode){
+    document.getElementById(id).style.display=mode;
 
+    if(mode=='block'){
+//设置下拉菜单的边框
+document.getElementById(id).style.border='1px solid #eee';
+document.getElementById(id).style.borderTop='none';
+
+//设置鼠标划过li的效果
+document.getElementById(id).parentNode.style.backgroundColor='#fff';
+document.getElementById(id).parentNode.style.border='1px solid #eee';
+document.getElementById(id).parentNode.style.borderBottom='none';
+
+    }
+
+    else{
+
+        //不显示下拉菜单时  鼠标划过LI的效果
+        document.getElementById(id).parentNode.style.backgroundColor='';
+        document.getElementById(id).parentNode.style.border='';
+    }
+}
+//设置下拉菜单的边框
+
+
+//设置鼠标划过li的效果
