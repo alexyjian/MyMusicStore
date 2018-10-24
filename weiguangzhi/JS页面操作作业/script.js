192dbde2e714986ac6097a7d676635fc35cba7b2
wqeasd <281437024@qq.com>
2018-10-24 17:49:50 +0800

    京东菜单

--- weiguangzhi/JS页面操作作业/script.js
+++
@@ -1,5 +0,0 @@
-function showSubMenu(li){
-    var subMenu = li.getElementsByTagName("ul")[0];                  /*传了一个参数li进来，表示当前鼠标经过的li，
-                                                                     再通过查找当前li下面的第一个ul，注意数组从0开始*/
-    subMenu.style.display = "block";
-}
