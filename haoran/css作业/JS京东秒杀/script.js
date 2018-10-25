3d7dba5d46d470a962d36604c348565de959b6de
QAQhaoran <1317898963@qq.com>
2018-10-25 12:07:37 +0800

    0

---
+++ haoran/css作业/JS京东秒杀/script.js
@@ -0,0 +1,8 @@
+function updateTime() {
+    //秒杀结束的时间
+    var endtime = new Date('2018/10/25,12：00：00');
+    //取当前时间
+    var currentTIme = new Date();
+    //计算剩余的时间，用秒作为单位
+    var leftSsecond
+}
