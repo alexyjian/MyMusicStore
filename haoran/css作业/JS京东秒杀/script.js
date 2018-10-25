b6897e48c7a037f4d11e2368397bf047c37a0666
QAQhaoran <1317898963@qq.com>
2018-10-25 12:07:58 +0800

    0

--- haoran/css作业/JS京东秒杀/script.js
+++ haoran/css作业/JS京东秒杀/script.js
@@ -4,5 +4,13 @@ function updateTime() {
     //取当前时间
     var currentTIme = new Date();
     //计算剩余的时间，用秒作为单位
-    var leftSsecond
+    var leftSsecond = parseInt((endtime.getTime() - currentTime.getTime())/1000);
+    //计算剩余小时
+    h = parseInt(leftSsecond / 3600);
+    //计算剩余分钟
+    m = parseInt(leftSsecond / 60 % 60);
+     //计算剩余秒
+    s = parseInt(leftSsecond % 60);
+    //调整
+    
 }
