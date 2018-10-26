6da1fc9ca52a2ed085b64d0a9cf62ab0b22b1fb3
ShroudzZ <1833549764@qq.com>
2018-10-26 12:36:05 +0800

    10.26

--- Shroud/10.26/01/js.js
+++ Shroud/10.26/01/js.js
@@ -5,24 +5,24 @@ window.onload = function () {
     var move = 11;
     var toppos=player.offsetTop;
     var leftpos=player.offsetLeft;
-    var b=1,t=1,l=1,r=1;
+    var t=1,l=1;
     
     document.onkeydown = function (event) {
         event = event ? event : window.event;
         
         if (event.keyCode == "87") {   
-            b+=5;      
-            player.style.bottom=i+'px';
+            t-=5;      
+            player.style.top=t+'px';
         }else if(event.keyCode == "65"){
-            r+=5;      
-            player.style.right=i+'px';
+            l-=5;      
+            player.style.left=l+'px';
             player.style.left='';
         }else if(event.keyCode == "83"){
             t+=5;      
-            player.style.top=i+'px';
+            player.style.top=t+'px';
         }else if(event.keyCode == "68"){
             l+=5;      
-            player.style.left=i+'px';
+            player.style.left=l+'px';
         }
 
     }
