3cee79bc853cc4192ecc097abbd60362486589b4
ShroudzZ <1833549764@qq.com>
2018-10-26 12:49:35 +0800

    10.26

--- Shroud/10.26/01/js.js
+++ Shroud/10.26/01/js.js
@@ -3,28 +3,35 @@
 window.onload = function () {
     var player = document.getElementById('bal');
     var move = 11;
-    var toppos=player.offsetTop;
-    var leftpos=player.offsetLeft;
-    var t=1,l=1;
-    
+
+    var t = 1,
+        l = 1;
+
     document.onkeydown = function (event) {
         event = event ? event : window.event;
-        
-        if (event.keyCode == "87") {   
-            t-=5;      
-            player.style.top=t+'px';
-        }else if(event.keyCode == "65"){
-            l-=5;      
-            player.style.left=l+'px';
-            player.style.left='';
-        }else if(event.keyCode == "83"){
-            t+=5;      
-            player.style.top=t+'px';
-        }else if(event.keyCode == "68"){
-            l+=5;      
-            player.style.left=l+'px';
+        var toppos = player.offsetTop;
+        var leftpos = player.offsetLeft;
+        if(toppos<=0||toppos>=236||leftpos<=0||leftpos>=441){
+            
+            alert('game over!');
+            location.reload();
+            return;
+        }
+        console.log(leftpos);
+        if (event.keyCode == "87") {
+            t -= 5;
+            player.style.top = t + 'px';
+        } else if (event.keyCode == "65") {
+            l -= 5;
+            player.style.left = l + 'px';
+        } else if (event.keyCode == "83") {
+            t += 5;
+            player.style.top = t + 'px';
+        } else if (event.keyCode == "68") {
+            l += 5;
+            player.style.left = l + 'px';
         }
 
     }
-    
-}
+
+};
