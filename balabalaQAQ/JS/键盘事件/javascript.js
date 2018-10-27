fa7479545143466af802f28d885c8884eb2d8a1a
balabalaQAQ <1124048070@qq.com>
2018-10-27 18:06:50 +0800

    10.27

--- balabalaQAQ/JS/键盘事件/javascript.js
+++ balabalaQAQ/JS/键盘事件/javascript.js
@@ -1,18 +1,27 @@
 window.onload = function () {
+    img = document.getElementById('img')
+    img.style.left = 0;
+    img.style.top = 0;
     document.body.onkeydown = function (evt) {
         var e = evt ? evt : window.event;
-        //left 37
-        if (e.keyCode == 37){
-            document.getElementById('img').style.transform='10px';
-        }
+        switch (e.keyCode) {
+            //left 37
+            case 37:
+                img.style.left = (parseInt(img.style.left) - 3) + 'px';
+                break;
             //top  38
-        if (e.keyCode == 38)
-            //right 39
-        if (e.keyCode == 39)
-        //bottom 40
-        if (e.keyCode == 40)
-        
+            case 38:
+                img.style.top = (parseInt(img.style.top) - 3) + 'px';
+                break;
+            case 39:
+                //right 39
+                img.style.left = (parseInt(img.style.left) + 3) + 'px';
+                break;
+            //bottom 40
+            case 40:
+                img.style.top = (parseInt(img.style.top) + 3) + 'px';
+                break;
+        }
     }
 }
-
 
