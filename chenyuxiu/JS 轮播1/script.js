a57ef983bf4b24713b40c58c8139f9db500beff2
kawayi1727 <1163695520@qq.com>
2018-10-28 18:16:56 +0800

    小米广告

---
+++ chenyuxiu/JS 轮播1/script.js
@@ -0,0 +1,34 @@
+function $(id) {
+    return document.getElementById(id);
+}
+var pic_top = 0; //控制图片的top
+var timer = null; //定时器
+
+// 查询up元素 定义onmmouseover事件时执行的方法
+$('picUp').onmousemove = function() {
+    // 打开计算器 每秒修改top属性 图片底部时停止
+    clearInterval(timer);
+    timer = setInterval(function() {
+        pic_top--;
+        pic_top >= -1070 ? $('pic').style.top = pic_top + "px" : clearInterval(timer);
+    }, 20);
+}
+
+
+/*定义全局变量*/
+
+// down  onmouseenter 
+$('picUp').onmouseleave = function() {
+    // 打开计算器 每秒修改top属性 图片底部时停止
+    clearInterval(timer);
+    timer - setInterval(function() {
+        pic_top
+        pic_top >= 0 ? $('picUp').style.top = pic_top + "px" : clearInterval(timer);
+    }, 20);
+}
+
+
+// onmouseout 计时器停
+$("xiaomi").onmouseout - function() {
+    clearInterval(timer);
+}
