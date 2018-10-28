927ff5fb0343533e9a36b804be69ae3f2a737f74
alexyjian <alex.yjian@gmail.com>
2018-10-28 18:16:36 +0800

    JS小米广告

--- 03Practice/JS轮播1/script.js
+++ 03Practice/JS轮播1/script.js
@@ -6,7 +6,6 @@ window.onload = function () {
     var pic_top = 0;   //控制图片的top
     var timer = null;  //定时器
 
-
     //查询up元素 定义onmouseover事件时执行的方法
     $("picUp").onmouseover = function () {
         // 打开计时器 每秒修改top属性  图片顶部时停止
