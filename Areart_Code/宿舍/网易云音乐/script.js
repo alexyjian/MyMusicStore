4b2d1a74385847e74a97992d3f7dac952f3e5ff0
Areart <15778250484@163.com>
2018-10-28 21:41:53 +0800

    127

--- Areart_Code/宿舍/网易云音乐/script.js
+++ Areart_Code/宿舍/网易云音乐/script.js
@@ -1,7 +1,11 @@
 var ji = 0;
 var widths=0;
 var kais = 0;
+var time;
 function jis() {
+     if(widths>540){
+        clearInterval(time);
+    }
     var jist = 'rotate(' + ji + 'deg' + ')';
     var min = document.getElementById('min');
     var jindu=document.getElementById('jindu');
@@ -9,15 +13,16 @@ function jis() {
     jindu.style.width=widths+'px';
     min.style.transform = jist;
     ji = ji + 0.1;
-    widths=widths+0.1;
+    widths=widths+0.03;
 }
 function opon() {
     kais++;
     if (kais == 1) {
-        time = setInterval(jis, 40);
+        time = setInterval(jis, 10);
     }
     else {
        clearInterval(time);
        kais=0;
     }
+    
 }
