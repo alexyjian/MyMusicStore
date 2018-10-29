1b4516ce7d1c963f87410d899ee74cf74f3500c6
balabalaQAQ <1124048070@qq.com>
2018-10-29 11:55:10 +0800

    news

---
+++ balabalaQAQ/作业/JS轮播1/javascript.js
@@ -0,0 +1,31 @@
+function $(id) {
+    return document.getElementById(id);
+}
+var pic_top = 0;    //控制图片的top
+var timer = null;   //定时器
+
+//查询up元素  定义onmouseover事件时执行的方法
+$('picUp').onmouseover = function () {
+    //打开计时器 每秒修改top属性 图片顶部时停止
+    clearInterval(timer);
+    timer = setInterval(function () {
+        pic_top--;
+        pic_top >= -970 ? $('pic').style.top = pic_top + 'px' : clearInterval(timer);
+    }, 10);
+}
+
+//down onmouseover
+$('picDown').onmouseover = function () {
+    //打开计时器 每秒修改top属性 图片底部时停止
+    clearInterval(timer);
+    timer = setInterval(function () {
+        pic_top++;
+        pic_top < 0 ? $('pic').style.top = pic_top + 'px' : clearInterval(timer);
+    }, 10);
+}
+
+
+//onmouseout 计时器停
+$("box").onmouseout = function () {
+    clearInterval(timer);
+}
---
+++ balabalaQAQ/作业/键盘事件/javascript.js
@@ -0,0 +1,71 @@
+//为网页加载键盘事件
+/* window.onload = function () {
+    img = document.getElementById('img')
+    img.style.left = 0;
+    img.style.top = 0;
+    document.body.onkeydown = function (evt) {
+        var e = evt ? evt : window.event;
+        switch (e.keyCode) {
+            //left 37
+            case 37:
+                img.style.left = (parseInt(img.style.left) - 3) + 'px';
+                break;
+            //top  38
+            case 38:
+                img.style.top = (parseInt(img.style.top) - 3) + 'px';
+                break;
+            case 39:
+                //right 39
+                img.style.left = (parseInt(img.style.left) + 3) + 'px';
+                break;
+            //bottom 40
+            case 40:
+                img.style.top = (parseInt(img.style.top) + 3) + 'px';
+                break;
+        }
+    }
+}
+*/
+window.onload = function () {
+    document.body.onkeydown = function (evt) {
+        //定义小球的步长
+        var step = 10;
+        //获取球台和小球的元素;
+        var box = document.getElementById('box');
+        var ball = document.getElementById('img');
+        //获取小球相对于球台div左侧和顶部的距离
+        var left = ball.offsetLeft;
+        var top = ball.offsetTop;
+
+        //获取球桌的高和宽
+        var box_width = box.offsetWidth;
+        var box_height = box.offsetHeight;
+
+        //小球的高和宽
+        var ball_width = ball.width;
+        var ball_height = ball.height;
+
+        //小球距离球桌左侧和顶部的最大距离
+        var x_max = box_width - ball_width - step;
+        var y_max = box_height - ball_height - step;
+
+        //创建键盘事件的对象
+        var e = evt ? evt : window.event;
+        //左键37
+        if (e.keyCode == 37 && (left-10)> step) {
+            ball.style.left =left -step+'px';
+        }
+        //右键39
+        if (e.keyCode == 39 && left <= (x_max-20)) {
+            ball.style.left =left +step+'px';
+        }
+        //上键38
+        if (e.keyCode == 38 && (top-10)> step) {
+            ball.style.top =top -step+'px';
+        }
+        //下键40
+        if (e.keyCode == 40 && top <= (y_max-20)) {
+            ball.style.top =top +step+'px';
+        }
+    }
+}
