adaf4a38f31accbf9950127fb6217ce31f32a509
Areart <15778250484@163.com>
2018-10-29 18:08:39 +0800

    2018-10-29

--- 我的作业和练习区/宿舍自娱自乐/JS焦点轮播/script.js
+++ 我的作业和练习区/宿舍自娱自乐/JS焦点轮播/script.js
@@ -15,7 +15,7 @@ window.onload = function () {
     for (var i = 0; i < lis.length; i++) {
         lis[i].onmouseover = function () {
             //清除计时器
-            clearInterval(time);  
+            clearInterval(time);
             //图片标码和当前标码不同时隐藏图片          
             if (i != current_index) {
                 imgs[current_index].style.display = 'none';
@@ -24,17 +24,17 @@ window.onload = function () {
             for (var i = 0; i < imgs.length; i++) {
                 if (lis[i] == this) {
                     imgs[i].style.display = 'block';
-                    lis[i].style.backgroundColor='rgb(70, 64, 64)';
+                    lis[i].style.backgroundColor = 'rgb(70, 64, 64)';
                 }
                 else {
                     imgs[i].style.display = 'none';
-                    lis[i].style.backgroundColor='#ccc';
+                    lis[i].style.backgroundColor = '#ccc';
                 }
             }
         }
         //鼠标移出当前时启动计时器
-        lis[i].onmouseout=function(){
-            time=setInterval(autuCheck, 1000);
+        lis[i].onmouseout = function () {
+            time = setInterval(autuCheck, 3000);
         }
     }
 
@@ -45,14 +45,14 @@ window.onload = function () {
             current_index = 0;
         for (var i = 0; i < imgs.length; i++) {
             if (i == current_index) {
-                imgs[i].style.display = 'block';               
-                lis[i].style.backgroundColor='rgb(70, 64, 64)';
+                imgs[i].style.display = 'block';
+                lis[i].style.backgroundColor = 'rgb(70, 64, 64)';
             }
             else {
                 imgs[i].style.display = 'none';
-                lis[i].style.backgroundColor='#ccc';
+                lis[i].style.backgroundColor = '#ccc';
             }
-        }        
+        }
     }
 
 }
