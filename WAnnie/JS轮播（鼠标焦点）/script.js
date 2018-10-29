057dfb4ab3d7279a672efeb4d984b954458859ed
Pretty33 <321838579@qq.com>
2018-10-29 22:38:54 +0800

    2018.10.29 JS轮播（鼠标焦点轮播）

---
+++ WAnnie/JS轮播（鼠标焦点）/script.js
@@ -0,0 +1,29 @@
+window.onabort = function(){
+    var slider = document.getElementById("slider");
+    //取子元素
+    var ul = document.getElementById("ad_ul");
+    var ol = document.getElementById("ad_ol");
+
+    var ollist = ol.children;
+
+    var leader = 0;
+    var target = 0;
+
+    for (var i = 0; i < ollist.length;i++){
+        //每个li的索引号
+        ollist[i].index = i;
+        ollist[i].onmouseover= function(){
+            for (var j = 0; j < ollist.length; j++){
+                ollist[j].className = "";  //去掉所有的class="current"
+            }
+            this.className ="current";
+
+            target = -this.index *490;  //目标位置 = 当前index乘以图片宽度
+        }
+    }
+
+    setInterval(function(){
+        leader = leader + (target - leader) / 10;
+        ul.style.left = leader + "px";
+    },20)
+}
