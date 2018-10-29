ea6f302bdb2eab077ab55fd150ecd06c089ef240
HappyWindManS <3066004354@qq.com>
2018-10-29 13:54:11 +0800

    MyComputer

--- MR/作业/JS鼠标轮滑/script.js
+++ MR/作业/JS鼠标轮滑/script.js
@@ -1,37 +1,35 @@
-window.onload = function(){ 
-    function $(id){
-    return document.getElementById(id);
+window.onload = function () {
+    function $(id) {
+        return document.getElementById(id);
     }
 
-var pic_top=0; //控制图片的top
-var time = null; //定时器
+    var pic_top = 0;   //控制图片的top
+    var timer = null;  //定时器
+
+    //查询up元素 定义onmouseover事件时执行的方法
+    $("picUp").onmouseover = function () {
+        // 打开计时器 每秒修改top属性  图片顶部时停止
+        clearInterval(timer);
+        timer = setInterval(function () {
+            pic_top--;
+            pic_top >= -1070 ? $('pic').style.top = pic_top + "px" : clearInterval(timer);
+        }, 20);
+    }
 
 
-
-//查询up元素 定义onmouseover事件时执行的方法
-$('picUp').onmouseover = function(){
-    //打开计时器  每秒修改top属性  图片顶部时停止
-    clearInterval(timer);
-    timer = setInterval(function(){
-        pic_top--;
-        pic_top>=-1070? $('picUp').style.top = pic_top+'px':clearInterval(timer);
-    },100);
-}
-
-
-//down onmouseover
-$('picDown').onmouseover = function(){
-    //打开计时器  每秒修改top属性  图片底部时停止
-    clearInterval(timer);
-    timer = setInterval(function(){
-        pic_top++;
-        pic_top<0? $('picDown').style.top = pic_top+'px':clearInterval(timer);
-    },100);
-}
+    //down  onmouseover  
+    $("picDown").onmouseover = function () {
+        // 打开计时器 每秒修改top属性 图片底部时停止
+        clearInterval(timer);
+        timer = setInterval(function () {
+            pic_top++;
+            pic_top < 0 ? $('pic').style.top = pic_top + "px" : clearInterval(timer);
+        }, 20);
+    }
 
 
-//onmouseout 计时器停止
-screen("xiaomi").onmouseout=function(){
-    clearInterval(timer);
-}
+    //onmouseout 计时器停
+    $("xiaomi").onmouseout = function () {
+        clearInterval(timer);
+    }
 }
