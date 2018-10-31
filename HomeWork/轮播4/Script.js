3f65ea9d95dd27b84b8786d0a622a050a8a9d003
ChaoPiPi <1806616500@qq.com>
2018-10-31 16:05:56 +0800

    update

--- HomeWork/轮播4/Script.js
+++ HomeWork/轮播4/Script.js
@@ -2,10 +2,21 @@ window.onload = function () {
     var ul = document.getElementById("list_img");
     var s_left = document.getElementById("s_left");
     var s_right = document.getElementById("s_right");
+    var scoll = document.getElementById("scoll");
     var target = 0;
     var lender = 0;
     var index = 0;
     var timer = null;
+
+    scoll.onmouseover = function() {
+        s_left.style.display = "block";
+        s_right.style.display = "block";
+    }
+
+    scoll.onmouseout = function() {
+        s_left.style.display = "none";
+        s_right.style.display = "none";
+    }
 
     function rightPlay() {
         target = -index * 555;
