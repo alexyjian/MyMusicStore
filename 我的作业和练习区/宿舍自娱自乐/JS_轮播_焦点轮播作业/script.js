226fe047992911e7773768aae94f09626b9edbdc
Areart <15778250484@163.com>
2018-10-31 18:05:42 +0800

    2018-10-31

--- 我的作业和练习区/宿舍自娱自乐/JS_轮播_焦点轮播作业/script.js
+++ 我的作业和练习区/宿舍自娱自乐/JS_轮播_焦点轮播作业/script.js
@@ -1,14 +1,18 @@
 //页面加载完成时
 window.onload = function () {
     var ul = document.getElementById('head_ul');
+    var ul2 = document.getElementById('head_ul2');
     var ol = document.getElementById('head_ol');
     //取所有子元素
     var ollis = ol.children;
     //用来控制left
     var leader = 0;
+    var leader2 = 567;
     var target = 0;
     //计算自动轮播页数
     var intr = 0;
+    var intrs = 0;
+    var intrr = 0;
     //计时器
     var timer = null;
     //遍历ol
@@ -24,33 +28,48 @@ window.onload = function () {
             }
             //自动轮播页数同步
             intr = this.index;
-
             this.className = 'current';
             target = -this.index * 576;//目标位置=当前index索引号*图片宽度
         }
         ollis[i].onmouseout = function () {
             //重启计时器
-            timer = setInterval(autoPlay, 4000);
+            timer = setInterval(autoPlay, 2500);
         }
     }
-    timer = setInterval(autoPlay, 4000);
+    timer = setInterval(autoPlay, 2500);
+
+
+    //过度动画
+    var st = 0;
+    var sr = 0;
+    setInterval(function () {
+
+            leader = leader + (target - leader) / 100;
+            ul.style.left = leader + 'px';
+    }, 2);
+
     function autoPlay() {
+        intr++;
+        intrs++;
+        //目标位置=当前index索引号*图片宽度    
+        target = -intr * 576; 
+        if (intr == ollis.length + 1) {
+            sr++;
+            
+        }
         for (var j = 0; j < ollis.length; j++) {
             ollis[j].className = '';//去掉所有的class='current'
         }
-        ollis[intr].className = 'current';
-        //目标位置=当前index索引号*图片宽度
-        target = -intr * 576;
-        intr++;
         //当页数到达最大时重置
-        if (intr == ollis.length) {
-            intr = 0;
+        if (intrs == ollis.length) {
+            intrs = 0;
+            st++;
         }
-    }
+        ollis[intrs].className = 'current';
 
-    //过度动画
-    setInterval(function () {
-        leader = leader + (target - leader) / 100;
-        ul.style.left = leader + 'px';
-    }, 2)
+
+
+
+
+    }
 }
