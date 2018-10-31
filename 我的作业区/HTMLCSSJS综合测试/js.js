dc4b8a3a83a1fae12bb8d58939313acecf27dd38
2876416732 <723817914@qq.com>
2018-11-01 00:15:00 +0800

    2018.10.31

--- 我的作业区/HTMLCSSJS综合测试/js.js
+++ 我的作业区/HTMLCSSJS综合测试/js.js
@@ -1,31 +1,58 @@
-var currentImage = 1;   //从1开始
-var imageCount = 5;     //图片总数
+window.onload=function(){
 
-window.onload = function () {
-    var ul = document.getElementById('ul');
-    var ol = document.getElementById('ol');
-
-    var ollis = ol.children;
-
-    var leader = 0;
-    var target = 0;
-
-    for (var i = 0; i < ollis.length; i++) {
-        //每个li的索引号
-        ollis[i].index = i;
-        ollis[i].onmouseover = function () {
-            for (var j = 0; j < ollis.length; j++) {
-                ollis[j].className = '';   //去掉所有的class='current'
-            }
-            this.className = 'current';
-
-            target = -this.index * 500;  //目标位置=当前index乘以图片宽度
-        }
-    }
-
-    //动画 每20ms left的值如何变化
-    setInterval(function () {
-        leader = leader + (target - leader) / 10;
-        ul.style.left = leader + 'px';
-    }, 20)
+    var wrap=document.getElementById('slider'),
+        ul=document.getElementById('ul'),
+        ol=document.getElementById('ol').getElementsByTagName('li'),
+        index=0,
+        timer=null;
+ 
+      // 定义并调用自动播放函数
+      if(timer){
+          
+          clearInterval(timer);
+          timer=null;
+      }
+timer=setInterval(autoplay,2000);
+      // 定义图片切换函数
+      function autoplay(){
+          index++;
+          if(index>=ol.length){
+              index=0;
+          }
+         changeoptions(index);
+          
+          
+      }
+     
+     // 鼠标划过整个容器时停止自动播放
+wrap.onmouseover=function(){
+    
+    clearInterval(timer);
+	
 }
+     // 鼠标离开整个容器时继续播放至下一张
+    wrap.onmouseout=function(){
+    
+    timer=setInterval(autoplay,2000);
+}
+     // 遍历所有数字导航实现划过切换至对应的图片
+	 for(var i=0;i<ol.length;i++){
+		 ol[i].id=i;
+		 ol[i].onmouseover=function(){
+			 clearInterval(timer);
+			 changeoptions(this.id);
+			 
+			 }
+		 }
+		function changeoptions(curindex){
+			for(var j=0;j<ol.length;j++){
+              ol[j].className='';
+              ul.style.top=0;
+              
+          }
+          ol[curindex].className='active';
+          ul.style.top=-curindex*170+'px';
+		  index=curindex;
+            }
+   }
+   
