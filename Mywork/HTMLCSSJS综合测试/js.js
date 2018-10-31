c08375123914d8e62d519d8c42eb3afc6e499648
Baiguia <1942152252@qq.com>
2018-10-31 22:39:00 +0800

    综合测试

--- Mywork/HTMLCSSJS综合测试/js.js
+++ Mywork/HTMLCSSJS综合测试/js.js
@@ -1,24 +1,53 @@
 window.onload = function () {
+
     var ul = document.getElementById("ad_ul");
     var ol = document.getElementById("ad_ol");
     var ollis = ol.children;
     var leader = 0;
     var target = 0;
+
+    var current_index=0;
+    var timer=null;
+
+    timer =this.setInterval(autoPlay,1500);
+
     for (var i = 0; i < ollis.length; i++) {
-        //每个li的索引号
         ollis[i].index = i;
         ollis[i].onmouseover = function () {
             for (var j = 0; j < ollis.length; j++) {
-                ollis[j].className = "";  //去掉所哟普的class="current"
+                ollis[j].className = "";  
             }
             this.className = "current";
-            target = -this.index * 490;  //目标位置-当前index乘以图片宽度
+            target = -this.index * 490;  
         }
+
+    } 
+
+    ul.onmouseover=function(){
+        clearInterval(timer);
     }
-    //动画 每20ms left的值如何变化
+
+    ul.onmouseout=function(){
+        timer=setInterval(autoPlay,1500);
+    }
+
+    
     setInterval(function () {
         
         leader = leader + (target - leader) / 10;
         ul.style.left = leader + 'px';
     }, 20)
+
+    function autoPlay() {
+        if(current_index == 4) {
+            current_index = -1;
+        }
+        current_index++;
+        for (var i = 0; i < nums.length; i++) {
+            nums[i].className = "";
+        }
+        nums[current_index].className = "current";
+        target = -current_index * 555;
+    }
+
 }
