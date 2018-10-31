e28a52146b8a990376b12b8d57a0eab3a46594e4
ChaoPiPi <1806616500@qq.com>
2018-10-31 18:09:40 +0800

    10-31

--- HomeWork/轮播图/Script.js
+++ HomeWork/轮播图/Script.js
@@ -3,30 +3,41 @@ function $(id) {
 }
 
 window.onload = function () {
-    var list_Nums = $("list_No").getElementsByTagName("li");
+    var target = 0;
+    var lender = 0;
+    var nums = $("list_num").children;
+    var imgs = $("list_img").children;
+    var ul = $("list_img");
+    var ol = $("list_num");
+    //当前索引
+    var img_current_index = 0;
+    var num_current_index = 0;
+    var timer = null;
 
-    var currend_Index = 0;
-
-
-    for (var i = 0; i < list_Nums.length; i++) {
-        list_Nums[i].onmouseover = function () {
-            if (i != currend_Index) {
-                list_Nums[currend_Index].style.backgroundColor = "";
-                list_Nums[currend_Index].style.color = "";
+    for (var i = 0; i < nums.length; i++) {
+        nums[i].index = i;
+        imgs[i].index = i;
+        nums[i].onmouseover = function () {
+            if (this.index == img_current_index) {
+                this.className = "";
+            } else {
+                this.className = "current";
             }
+            img_current_index = num_current_index = this.index;
+            console.log(img_current_index);
+            console.log(num_current_index);
+            target = -this.index * 555;
+        }
 
-            var list_imgs = document.getElementById("list_img").getElementsByTagName("li");
-
-            for (var i = 0; i < list_imgs.length; i++) {
-                //将所有的元素隐藏 去掉current类名
-                list_imgs[i].className = list_imgs[i].className.replace(" current", "");
-                list_Nums[i].className = list_Nums[i].className.replace(" current", "");
-                //将当前索引对应的元素设为显示
-                if (list_Nums[i] == this) {
-                    this.className += " current";
-                    list_imgs[i].className += " current";
-                }
-            }
+        nums[i].onmouseout = function () {
+            num_current_index = 0;
+            console.log(img_current_index);
+            console.log(num_current_index);
         }
     }
+
+    setInterval(function () {
+        lender = lender + (target - lender) / 10;
+        ul.style.left = lender + "px";
+    }, 20)
 }
