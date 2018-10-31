f22fad1ba37b21330aea871f4dc87c461eeb88ae
kawayi1727 <1163695520@qq.com>
2018-10-31 17:58:34 +0800

    JD京东秒杀

--- chenyuxiu/京东秒杀JD/script.js
+++ chenyuxiu/京东秒杀JD/script.js
@@ -32,9 +32,9 @@ function updateTime() {
 
 
     //秒杀结束的操作
-    if (leftSecond < -0) {
+    if (leftSecond <= 0) {
         // 显示相对应信息“秒杀已经结束”
-        document.getElementById('end-box').style.background = 'url(images/flash_end.png) no-repeat';
+        document.getElementById('end-box').style.background = 'url(images/flash_end.png)no-repeat';
         document.getElementById('end-box').style.display = 'block';
         document.getElementById('end-box').innerHTML = "秒杀结束"
 
