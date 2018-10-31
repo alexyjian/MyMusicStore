8a3e84dc2b0ff5380fe1a253c5e94016ec49fce0
Baiguia <1942152252@qq.com>
2018-11-01 00:03:49 +0800

    综合测试

--- Mywork/HTMLCSSJS综合测试/js.js
+++ Mywork/HTMLCSSJS综合测试/js.js
@@ -1,53 +1,53 @@
 window.onload = function () {
-
+    var slider=document.getElementById("slider");
     var ul = document.getElementById("ad_ul");
-    var ol = document.getElementById("ad_ol");
-    var ollis = ol.children;
+    var ol = document.getElementById("ad_ol").getElementsByTagName("li")
+    var timer=null;
+    var index=0;
     var leader = 0;
     var target = 0;
-
-    var current_index=0;
-    var timer=null;
-
-    timer =this.setInterval(autoPlay,1500);
-
-    for (var i = 0; i < ollis.length; i++) {
-        ollis[i].index = i;
-        ollis[i].onmouseover = function () {
-            for (var j = 0; j < ollis.length; j++) {
-                ollis[j].className = "";  
-            }
-            this.className = "current";
-            target = -this.index * 490;  
-        }
-
-    } 
-
-    ul.onmouseover=function(){
-        clearInterval(timer);
-    }
-
-    ul.onmouseout=function(){
-        timer=setInterval(autoPlay,1500);
-    }
-
     
+    timer =setInterval(autoPlay,1000);
+
+    function autoPlay() {
+        index++;
+        if (index >= ol.length) {
+            index = 0;
+        }
+        cutimages(index);
+    }
+   
+    function cutimages(cuindex) {
+        for (var i = 0; i < ol.length; i++) {
+
+            for (var i = 0; i < ol.length; i++) {
+                ol[i].className = '';
+            }
+            ol[cuindex].className = "current";
+            target = ol[cuindex].index * -490;
+        }
+    }
+
     setInterval(function () {
         
         leader = leader + (target - leader) / 10;
         ul.style.left = leader + 'px';
     }, 20)
 
-    function autoPlay() {
-        if(current_index == 4) {
-            current_index = -1;
-        }
-        current_index++;
-        for (var i = 0; i < nums.length; i++) {
-            nums[i].className = "";
-        }
-        nums[current_index].className = "current";
-        target = -current_index * 555;
+    slider.onmouseover=function(){
+        clearInterval(timer);
     }
 
+    slider.onmouseout=function(){
+        timer=setInterval(autoPlay,1500);
+    }
+
+    for (var i = 0; i < ol.length; i++) {
+        ol[i].index = i;
+        ol[i].onmouseover = function () {
+            clearInterval(timer);
+            cutimages(this.index);
+            index = this.index;
+        }
+    }
 }
