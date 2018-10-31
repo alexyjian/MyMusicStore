a9abc5bd6039b55b337a1dfb6d1ef7d6c226415a
mwailxx <1650098394@qq.com>
2018-10-31 18:44:13 +0800

    10-31xw综合显示

---
+++ KLYN/03Practice/HTMLCSSJS综合测试/js.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    var slider = document.getElementById('slider');
+    var ul = document.getElementById('ad_ul');
+    var ol = document.getElementById('ad_ol');
+    var ollis = ol.children;
+    var leader = 0;
+    var target = 0;
+
+    for (var i = 0; i < ollis.length; i++) {
+        ollis[i].index = i;
+        ollis[i].onmouseover = function() {
+            for (var j = 0; j < ollis.length; j++) {
+                ollis[j].className = '';
+            }
+
+            this.className = 'current';
+            target = -this.index * 500;
+        }
+    }
+    setInterval(function() {
+        leader = leader + (target - leader) / 10;
+        ul.style.left = leader + 'px';
+    }, 20);
+}
