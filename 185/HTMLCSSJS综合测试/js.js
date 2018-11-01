16dbf2884aa554bc07b32520dcb7246617d69cb6
WaxdM <1575908620@qq.com>
2018-11-01 08:53:32 +0800

    11.1

--- 185/HTMLCSSJS综合测试/js.js
+++ 185/HTMLCSSJS综合测试/js.js
@@ -1,26 +1,55 @@
-window.onload = function(){
-    
-    var ul =document.getElementById('ad_ul');
-    var ol =document.getElementById('ad_ol');
-
-  var ollis = ol.children;
-
+window.onload = function () {
+    var slider=document.getElementById("slider");
+    var ul = document.getElementById("ad_ul");
+    var ol = document.getElementById("ad_ol").getElementsByTagName("li")
+    var timer=null;
+    var index=0;
     var leader = 0;
     var target = 0;
-    for(var i = 0;i < ollis.length; i++){
-        //每个li的索引号
-        ollis[i].index = i;
-        ollis[i].onmouseover = function(){
-            for(var j=0;j<ollis.length;j++){
-                ollis[j].classname = '';//去掉所有的class='current'
-            }
-            this.classname ='current';
-            target = -this.index * 490;//目标位置=当前index乘以图片宽度
+  
+    timer =setInterval(autoPlay,2000);
+ 
+    function autoPlay() {
+        index++;
+        if (index >= ol.length) {
+            index = 0;
         }
-        
+        cutimages(index);
+        console.log(index);
     }
-    setInterval(function (){
-        leader = leader +(target -leader)/10;
-        ul.style.left = leader+'px';
-    },20)
+   
+    function cutimages(cuindex) {
+        for (var i = 0; i < ol.length; i++) {
+
+            for (var i = 0; i < ol.length; i++) {
+                ol[i].className = '';
+            }
+            ol[cuindex].className = "current";
+            target = ol[cuindex].index * -490;
+        }
+    }
+
+    setInterval(function () {
+        
+        leader = leader + (target - leader) / 10;
+        ul.style.left = leader + 'px';
+    }, 20)
+
+    slider.onmouseover=function(){
+        clearInterval(timer);
+    }
+
+    slider.onmouseout=function(){
+        timer=setInterval(autoPlay,2000);
+    }
+
+    for (var i = 0; i < ol.length; i++) {
+        ol[i].index = i;
+        ol[i].onmouseover = function () {
+            clearInterval(timer);
+            cutimages(this.index);
+            index = this.index;
+            console.log(index);
+        }
+    }
 }
