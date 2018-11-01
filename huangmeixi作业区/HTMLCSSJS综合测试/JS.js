24922d7b80e945b8d0ecfb6e1826a9d1d51203fe
huangmeixiqi <1600582963@qq.com>
2018-11-01 13:38:55 +0800

    HTMLCSSJS综合作业

--- huangmeixi作业区/HTMLCSSJS综合测试/JS.js
+++ huangmeixi作业区/HTMLCSSJS综合测试/JS.js
@@ -1,67 +1,58 @@
-window.onload=function(){
-    var wrap=document.getElementById('wrap'),
-        pic=document.getElementById('pic'),
-        list=document.getElementById('list').getElementsByTagName('li'),
-        index=0,
-        timer=null;
+window.onload = function () {
+    //获取各元素
+    var slider=document.getElementById("wrap");
+    var ul = document.getElementById("qx_ul");
+    var ol = document.getElementById("qx_ol").getElementsByTagName("li")
+    var timer=null;
+    var index=0;
+    var leader = 0;
+    var target = 0;
+    ul.appendChild(ul.children[0].cloneNode(true));
 
-   //计时器
-   timer = setInterval(autoPlay, 10);
-   
-   function autoPlay() {
-       target--;
-
-       target <= -1200 ? target = 0 : target;
-       leader = leader + (target - leader) / 1000;
-       //console.log(leader);
-       ul.style.left = leader + "px";
-   }
-      // 定义并调用自动播放函数
-      if(timer){
-          
-          clearInterval(timer);
-          timer=null;
-      }
-     timer=setInterval(autoplay,2000);
-      // 定义图片切换函数
-      function autoplay(){
-          index++;
-          if(index>=list.length){
-              index=0;
-          }
-         changeoptions(index);
-             
-      }
-      ulObj.appendChild(ulObj.children[0].cloneNode(true));
-     
-     // 鼠标划过时停止自动播放
-    wrap.onmouseover=function()
-    {
-        clearInterval(timer);
-	
+    timer =setInterval(autoPlay,2000);
+ 
+    function autoPlay() {
+        index++;
+        if (index >= ol.length) {
+            index = 0;
+        }
+        cutimages(index);
+        console.log(index);
     }
-     // 鼠标离开时继续播放至下一张
+   
+    function cutimages(cuindex) {
+        for (var i = 0; i < ol.length; i++) {
+
+            for (var i = 0; i < ol.length; i++) {
+                ol[i].className = '';
+            }
+            ol[cuindex].className = "current";
+            target = ol[cuindex].index * -500;
+        }
+    }
+
+    setInterval(function () {
+        
+        leader = leader + (target - leader) / 10;
+        ul.style.left = leader + 'px';
+    }, 20)
+      //鼠标经过事件
+    wrap.onmouseover=function(){
+        clearInterval(timer);
+    }
+    //鼠标离开事件
     wrap.onmouseout=function(){
+        timer=setInterval(autoPlay,2000);
+    }
     
-    timer=setInterval(autoplay,2000);
+    for (var i = 0; i < ol.length; i++) {
+        ol[i].index = i;
+        ol[i].onmouseover = function () {
+            clearInterval(timer);
+            cutimages(this.index);
+            index = this.index;
+            console.log(index);
+        }
+    }
 }
-     // 遍历所有数字导航实现划过切换至对应的图片
-	 for(var i=0;i<list.length;i++){
-		 list[i].id=i;
-		 list[i].onmouseover=function(){
-			 clearInterval(timer);
-			 changeoptions(this.id);
-			 
-			 }
-		 }
-		function changeoptions(curindex){
-			for(var j=0;j<list.length;j++){
-              list[j].className='';
-              pic.style.top=0;
-              
-          }
-          list[curindex].className='active';
-          pic.style.top=-curindex*170+'px';
-		  index=curindex;
-			} 
-}
+
