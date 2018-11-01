d1a8f63b139d4f00ac2860f67896e49354f0424c
kawayi1727 <1163695520@qq.com>
2018-11-01 10:15:08 +0800

    JD秒杀页面

--- chenyuxiu/京东秒杀JD/script.js
+++ chenyuxiu/京东秒杀JD/script.js
@@ -1,18 +1,17 @@
-// 刷新时间
 function updateTime() {
-    //   秒杀结束时间
-    var endTime = new Date('2018/10/25,12:00:00');
-    // 取当前时间
+    //秒杀结束的时间
+    var endTime = new Date('2018/11/1,12:00:00');
+    //取当前时间
     var currentTime = new Date();
-    //  计算剩余时间,用秒作为单位
+    //计算剩余的时间,用秒作为单位
     var leftSecond = parseInt((endTime.getTime() - currentTime.getTime()) / 1000);
-    //计算剩余的小时
+    //计算剩余小时
     h = parseInt(leftSecond / 3600);
-    //计算剩余的分钟
+    //计算剩余分钟
     m = parseInt(leftSecond / 60 % 60);
-    //计算剩余的秒
+    //计算剩余秒
     s = parseInt(leftSecond % 60);
-    // 调整格式
+    //调整格式
     if (h < 10)
         h = "0" + h;
 
@@ -30,13 +29,12 @@ function updateTime() {
     document.getElementById('minute').innerHTML = m;
     document.getElementById('second').innerHTML = s;
 
-
     //秒杀结束的操作
     if (leftSecond <= 0) {
-        // 显示相对应信息“秒杀已经结束”
-        document.getElementById('end-box').style.background = 'url(images/flash_end.png)on-repeat';
+        //显示相应信息  "秒杀已经结束"
+        document.getElementById('end-box').style.background = 'url(images/flash_end.png) no-repeat';
         document.getElementById('end-box').style.display = 'block';
-        document.getElementById('end-box').innerHTML = "秒杀结束"
+        document.getElementById('end-box').innerHTML = '秒杀已结束';
 
         clearInterval(countDown);
     }
