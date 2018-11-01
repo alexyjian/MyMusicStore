7a810989bb90d6d3c1fa28097f0ca89f9680df41
luyusheng7 <2278814961@qq.com>
2018-11-01 11:46:49 +0800

    JS轮播1

--- luyusheng/JS划轮播1/script.js
+++ luyusheng/JS划轮播1/script.js
@@ -8,27 +8,27 @@ var pic_top=0;
 var timer=null;
 
 //查询up元素 定义onmouseover事件时执行的方法
-$('picUp').onmouseover=function(){
+$("picUp").onmouseover=function(){
 //打开计时器，每秒修改top属性，图片底部时停止
 clearInterval(timer);
 timer=setInterval(function(){
-    pic_top--;
-    pic_top >= -1070 ? $('pic').style.top=pic_top+ 'px':clearInterval(timer);
-},20);
+    pic_top --;
+    pic_top >= -1070 ? $("pic").style.top= pic_top+ "px":clearInterval(timer);
+},100);
 }
 
 //down onmouseover
-$('picDown').onmouseover=function(){
+$("picDown").onmouseover=function(){
 //打开计时器，每秒修改top属性，图片底部时停止
 clearInterval(timer);
 timer=setInterval(function(){
-    pic_top++;
-    pic_top < 0 ? $('pic').style.top=pic_top+ 'px':clearInterval(timer);
-},20);
+    pic_top ++;
+    pic_top < 0 ? $("pic").style.top= pic_top + "px":clearInterval(timer);
+},100);
 }
 
 //onmouseover 计时器停
-$('xiaomi').onmouseout=function(){
+$("xiaomi").onmouseout=function(){
     clearInterval(timer);
 }
 }
