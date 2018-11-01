9063a314f03f274d57a084bbdbf83fca607cf8d2
luyusheng7 <2278814961@qq.com>
2018-11-01 12:12:40 +0800

    JS自动轮播

--- luyusheng/JS自动轮播/sropt.js
+++ luyusheng/JS自动轮播/sropt.js
@@ -1,11 +1,19 @@
 window.onload=function(){
     var naoll=document.getElementById('naoll');
     var ul=document.getElementById('on_ul');
-    var ol=document.getElementById('on_ol');
-    var ollis=ol.children;
+    var ol=document.getElementById('on_ol').getElementsByTagName("li");
+    var timer=null;
     var leader=0;
     var target=0;
+    
+    var 
 
+    //自动播放设置
+    if(timer){
+       clearInterval(timer);
+       timer=null;
+   }  
+    var ollis=ol.children;
     for(var i=0; i<ollis.length; i++){
         //每个li的索引号
         ollis[i].index=i;
@@ -16,19 +24,12 @@ window.onload=function(){
             }
             this.className='arroet';
             //目标位置=当前index乘以图片宽度
-            target= -this.index * 490;
+            Animation(ul) -this.index * 500;
         }
     }
     //动画，每20秒 left的值如何变化
     setInterval(function(){
         leader=leader + (target-leader)/10;
         ul.style.left=leader + 'px';
-    },20)
-     //鼠标悬停事件
-     naoll.onmouseover=function(){
-        clearInterval(timer);
-    }
-    naoll.onmouseout=function(){
-        timer=setInterval(autoPlay,10);
-    }
+    },100)
 }
