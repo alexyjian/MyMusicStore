930b94fb6729b958868fd87eff6860b952286f1a
2876416732 <2876416732@qq.com>
2018-11-01 11:42:20 +0800

    2018.11.01 HTMLCSSJS综合测试

--- 我的作业区/HTMLCSSJS综合测试/js.js
+++ 我的作业区/HTMLCSSJS综合测试/js.js
@@ -1,10 +1,9 @@
 window.onload=function(){
-
-    var wrap=document.getElementById('slider'),
         ul=document.getElementById('ul'),
         ol=document.getElementById('ol').getElementsByTagName('li'),
         index=0,
         timer=null;
+
  
       // 定义并调用自动播放函数
       if(timer){
@@ -19,22 +18,18 @@ timer=setInterval(autoplay,2000);
           if(index>=ol.length){
               index=0;
           }
-         changeoptions(index);
-          
-          
+         changeoptions(index);   
       }
+
+    //鼠标移到图片时暂停
+      slider.onmouseover = function(){
+            clearInterval(timer);
+        }
+    //鼠标离开时继续切换下一张
+        slider.onmouseout = function(){
+            timer = setInterval(autoplay,2000);
+        }
      
-     // 鼠标划过整个容器时停止自动播放
-wrap.onmouseover=function(){
-    
-    clearInterval(timer);
-	
-}
-     // 鼠标离开整个容器时继续播放至下一张
-    wrap.onmouseout=function(){
-    
-    timer=setInterval(autoplay,2000);
-}
      // 遍历所有数字导航实现划过切换至对应的图片
 	 for(var i=0;i<ol.length;i++){
 		 ol[i].id=i;
@@ -42,17 +37,24 @@ wrap.onmouseover=function(){
 			 clearInterval(timer);
 			 changeoptions(this.id);
 			 
-			 }
-		 }
+             }
+         }
+         
 		function changeoptions(curindex){
 			for(var j=0;j<ol.length;j++){
               ol[j].className='';
-              ul.style.top=0;
-              
+              ul.style.top=0;    
           }
+    
           ol[curindex].className='active';
-          ul.style.top=-curindex*170+'px';
-		  index=curindex;
-            }
+          ul.style.top=-curindex*200+'px';
+          index=curindex;
+  //自动切换和点击切换
+          if(timer){
+            clearInterval(timer);
+            timer=null;
+        }
+  timer=setInterval(autoplay,2000);
+            }   
    }
    
