c9bb1caae7aeb40e13c82d243e8eb4e5a26fc39c
kawayi1727 <1163695520@qq.com>
2018-11-01 11:48:56 +0800

    JS左右轮播

--- chenyuxiu/JS轮播_鼠标焦点/srcip.js
+++ chenyuxiu/JS轮播_鼠标焦点/srcip.js
@@ -1,29 +1,28 @@
 window.onload = function() {
-    var slider = document.getElementById("slider");
-    var ul = document.getElementById("ad_ul");
-    var ol = document.getElementById("ad_ol");
+    var ul = document.getElementById('ad_ul');
+    var ol = document.getElementById('ad_ol');
 
     var ollis = ol.children;
-
 
     var leader = 0;
     var target = 0;
 
     for (var i = 0; i < ollis.length; i++) {
-        // 每个li的索引号
+        //每个li的索引号
         ollis[i].index = i;
         ollis[i].onmouseover = function() {
             for (var j = 0; j < ollis.length; j++) {
-                ollis[j].className = ''; // 去掉所有的class='current'
+                ollis[j].className = ''; //去掉所有的class='current'
             }
             this.className = 'current';
 
-            target = -this.index * 490; //目标位置=当前的index乘以图片的宽度
+            target = -this.index * 490; //目标位置=当前index乘以图片宽度
         }
     }
-    // 动画 每20秒 left 的值如何变化
+
+    //动画 每20ms left的值如何变化
     setInterval(function() {
         leader = leader + (target - leader) / 10;
-        ul.style.left = leader = leder + 'px';
+        ul.style.left = leader + 'px';
     }, 20)
 }
