ef59b49d24742b009dce7120f4f353dca0be1aad
suweixi <1985748697@qq.com>
2018-11-01 09:06:12 +0800

    HTMLCSSJS综合测试

--- Dashan/HTMLCSSJS综合测试/script.js
+++ Dashan/HTMLCSSJS综合测试/script.js
@@ -4,26 +4,26 @@ window.onload = function () {
     var buttons = document.getElementById('buttons').getElementsByTagName('span');
     var prev = document.getElementById('prev');
     var next = document.getElementById('next');
-    var index = 1; //显示第几个小圆点
+    var index =1; //显示第几个
     var changed = false; //切换状态值无切换
     var timer;
 
-    function showButton() { //点亮小圆点
+    function showButton() { 
         for (var i = 0; i < buttons.length; i++) {
             if (buttons[i].className == 'on') {
                 buttons[i].className = '';
                 break;
             }
         }
-        buttons[index - 1].className = 'on'; 
+        buttons[index - 1].className = 'on';
     }
 
     function change(offset) { //切换函数
         changed = true;
         var newleft = parseInt(list.style.left) + offset;
 
-        var time = 300; //位移总时间（ms）
-        var interval = 10; //位移间隔时间
+        var time = 10; //位移总时间（ms）
+        var interval = 5; //位移间隔时间
         var speed = offset / (time / interval); //每次的位移量   总偏移/次数
 
         function go() {
@@ -35,11 +35,11 @@ window.onload = function () {
                 changed = false;
                 list.style.left = newleft + 'px';
 
-                if (newleft > -500) { //归位
-                    list.style.left = -4000 + 'px';
+                if (newleft > -480) { //归位
+                    list.style.left = -2400 + 'px';
                 }
-                if (newleft < -4000) {
-                    list.style.left = -500 + 'px';
+                if (newleft < -2400) {
+                    list.style.left = -480 + 'px';
                 }
             }
         }
@@ -49,7 +49,7 @@ window.onload = function () {
     function play() { //自动切换函数
         timer = setInterval(function () {
             next.onclick();
-        }, 3000);
+        }, 2400);
     }
 
     function stop() { //自动切换停止函数
@@ -80,7 +80,7 @@ window.onload = function () {
         }
     }
 
-    //为小圆点添加点击事件
+    //添加点击事件
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].onclick = function () {
             //判断如果点击对应以打开图片，退出函数
@@ -98,8 +98,7 @@ window.onload = function () {
             }
         }
     }
-    container.onmouseover = stop; //鼠标移动到容器内停止自动切换
+    container.onmouseover = stop; //鼠标移动到容器内停止  移开自动切换
     container.onmouseout = play; //鼠标在容器外执行自动切换
-
     play();
 }
