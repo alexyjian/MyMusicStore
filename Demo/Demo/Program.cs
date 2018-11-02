de9025d71816ce9541e33daae70b82a867aca1d8
2876416732 <2876416732@qq.com>
2018-11-02 12:53:10 +0800

    2018.11.02测试

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1008611");
+            Console.WriteLine("1008611");
+            Console.WriteLine("1008611");
+            Console.WriteLine("1008611");
+            Console.WriteLine("1008611");
+        }
+    }
+}
