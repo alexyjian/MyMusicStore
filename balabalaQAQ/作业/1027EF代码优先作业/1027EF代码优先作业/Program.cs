f571754713f8aaa6ff77d04fa889abed55a92647
balabalaQAQ <1124048070@qq.com>
2018-11-04 11:34:04 +0800

    .

--- balabalaQAQ/作业/1027EF代码优先作业/1027EF代码优先作业/Program.cs
+++ balabalaQAQ/作业/1027EF代码优先作业/1027EF代码优先作业/Program.cs
@@ -10,17 +10,44 @@ namespace _1027EF代码优先作业
     {
         static void Main(string[] args)
         {
-            using (var CourseDB = new CourseDBEntities())
+
+
+            //添加
+            var context = new CourseDBEntities();
+            var courseDB = context.Courses.ToList();
+            var DB = new Courses()
             {
-                var DB = new Courses
-                {
-                    ID = Guid.NewGuid(),
-                    Title = "做作业",
-                    Credit = "009",
-                    Department_ID = Guid.Parse("a664af03-adcf-4311-b2d5-44c98264a5a0")
+                ID = Guid.NewGuid(),
+                Title = "做作业",
+                Credit = "009",
+                Department_ID = context.Departments.SingleOrDefault(x => x.Name == "电子信息工程学院").ID
                 };
-                CourseDB.Courses.Add(DB);
+            context.Courses.Add(DB);
+            context.SaveChanges();
+
+            //修改
+            var editCourese = context.Courses.SingleOrDefault(n => n.Title == "做作业");
+            if (editCourese != null)
+            {
+                editCourese.Title = "做作业啦";
+                editCourese.Credit = "00J";
+                context.SaveChanges();
             }
+            else
+            {
+                Console.Write("没有该记录，无法修改！");
+            }
+
+            //删除
+            var delCourese = context.Courses.Find(Guid.Parse(""));
+            context.Courses.Remove(DB);
+            context.SaveChanges();
+
+
+            foreach (var c in courseDB)
+                    Console.WriteLine("课程名称：{0} 课程学分：{0} 课程ID：{0}", c.Title, c.Credit, c.Department_ID);
+                Console.ReadKey();
+            
         }
     }
 }
