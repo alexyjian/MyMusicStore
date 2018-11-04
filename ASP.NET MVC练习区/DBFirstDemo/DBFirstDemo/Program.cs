27f510d340fd6e183c3f4804bbb04d1344505167
2876416732 <2876416732@qq.com>
2018-11-04 11:26:13 +0800

    DBFirstDemo，数据优先示例

--- ASP.NET MVC练习区/DBFirstDemo/DBFirstDemo/Program.cs
+++ ASP.NET MVC练习区/DBFirstDemo/DBFirstDemo/Program.cs
@@ -19,18 +19,46 @@ namespace DBFirstDemo
                 foreach (var d in departments)
                     Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
 
-                Console.WriteLine("修改记录");
+
+                //Console.WriteLine("修改记录");
+                //Console.WriteLine("================================");
+                ////修改
+                //var editDepartment = context.Departments.SingleOrDefault(x => x.Name == "环境与食品检测学院");
+                //if (editDepartment != null)
+                //{
+                //    editDepartment.Name = "环境与食品检测学院";
+                //    editDepartment.SortCode = "007";
+                //    context.SaveChanges();
+                //}
+                //else
+                //    Console.WriteLine("未找到该记录，不能修改");
+
+                //Console.WriteLine("删除记录");
+                //Console.WriteLine("================================");
+                ////find--用主键查询实体
+                ////var delDept = context.Departments.Find(Guid.Parse("1fc54ff7-b598-459a-be5c-47effd4b9454"));
+                //var id = Guid.Parse("1794b626-b88c-453f-8ea8-e221fd47e341");
+                //var delDept = context.Departments.SingleOrDefault(x => x.ID == id);
+                //context.Departments.Remove(delDept);
+                //context.SaveChanges();
+
+
+                //var departments1 = context.Departments.OrderBy(n => n.SortCode).ToList();
+                //foreach (var d in departments1)
+                //    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+
+
+                Console.WriteLine("添加记录");
                 Console.WriteLine("================================");
-                //修改
-                var editDepartment = context.Departments.SingleOrDefault(x => x.Name == "环境与食品检测学院");
-                if (editDepartment != null)
+                var newDept = new Departments
                 {
-                    editDepartment.Name = "环境与食品检测学院";
-                    editDepartment.SortCode = "007";
-                    context.SaveChanges();
-                }
-                else
-                    Console.WriteLine("未找到该记录，不能修改");
+                    ID = Guid.NewGuid(),
+                    Name = "测试学院",
+                    Dscn = "不可乱来",
+                    SortCode = "888"
+                };
+                context.Departments.Add(newDept);
+                context.SaveChanges();
 
                 var departments1 = context.Departments.OrderBy(n => n.SortCode).ToList();
                 foreach (var d in departments1)
--- ASP.NET MVC练习区/Demo/Demo/Program.cs
+++ ASP.NET MVC练习区/Demo/Demo/Program.cs
@@ -19,42 +19,42 @@ namespace Demo
                 foreach (var d in departments)
                     Console.WriteLine("编号{0},部门名称{1},说明{2}",d.SortCode,d.Name,d.Dscn);
 
-                //Console.WriteLine("添加一条新的部门记录");
+                Console.WriteLine("添加一条新的部门记录");
                 //添加一条记录
-                // var newDepartment = new Departments
-                // {
-                // ID = Guid.NewGuid(),
-                // Name = "环境与食品学院",
-                // Dscn = "环境与食品检测",
-                //SortCode = "007"
-                // };
+                 var newDepartment = new Departments
+                 {
+                     ID = Guid.NewGuid(),
+                     Name = "环境与食品学院",
+                     Dscn = "环境与食品检测",
+                     SortCode = "007"
+                 };
                 //把新的对象添加到上下文中
-                //context.Departments.Add(newDepartment);
+                context.Departments.Add(newDepartment);
                 //更新上下文，把新的实体保存到数据库中
-                //context.SaveChanges();
+                context.SaveChanges();
 
                 //显示新的记录
-                // var departments1 = context.Departments.OrderBy(n => n.SortCode).ToList();
-                // foreach (var d in departments1)
-                // Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
-
-
-                Console.WriteLine("修改记录");
-                Console.WriteLine("================================");
-                //修改
-                var editDepartment = context.Departments.SingleOrDefault(x =>x.Name== "环境与食品检测学院");
-                if (editDepartment != null)
-                {
-                    editDepartment.Name = "环境与食品检测学院";
-                    editDepartment.SortCode = "007";
-                    context.SaveChanges();
-                }
-                else
-                    Console.WriteLine("未找到该记录，不能修改");
-
-                var departments1 = context.Departments.OrderBy(n => n.SortCode).ToList();
+                 var departments1 = context.Departments.OrderBy(n => n.SortCode).ToList();
                 foreach (var d in departments1)
-                Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+                    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+
+
+                //Console.WriteLine("修改记录");
+                //Console.WriteLine("================================");
+                ////修改
+                //var editDepartment = context.Departments.SingleOrDefault(x =>x.Name== "环境与食品检测学院");
+                //if (editDepartment != null)
+                //{
+                //    editDepartment.Name = "环境与食品检测学院";
+                //    editDepartment.SortCode = "007";
+                //    context.SaveChanges();
+                //}
+                //else
+                //    Console.WriteLine("未找到该记录，不能修改");
+
+                //var departments1 = context.Departments.OrderBy(n => n.SortCode).ToList();
+                //foreach (var d in departments1)
+                //Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
 
                 Console.ReadKey();
             }
