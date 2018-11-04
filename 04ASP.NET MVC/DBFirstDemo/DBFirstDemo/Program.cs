a939558c3bd6cc673d028600c8b7cd86a6c856d5
alexyjian <alex.yjian@gmail.com>
2018-11-04 10:21:08 +0800

    DB first的实体框架的增删查改Demo

--- 04ASP.NET MVC/DBFirstDemo/DBFirstDemo/Program.cs
+++ 04ASP.NET MVC/DBFirstDemo/DBFirstDemo/Program.cs
@@ -17,18 +17,18 @@ namespace DBFirstDemo
                 foreach (var d in departments)
                     Console.WriteLine("编号:{0},部门名称:{1},说明:{2}", d.SortCode, d.Name, d.Dscn);
 
-                Console.WriteLine("修改记录");
-                Console.WriteLine("========================================");
-                //修改
-                var editDepartment = context.Departments.SingleOrDefault(x => x.Name == "环境与食品检测学院");
-                if (editDepartment != null)
-                {
-                    editDepartment.Name = "环境与食品检测学院";
-                    editDepartment.SortCode = "007";
-                    context.SaveChanges();
-                }
-                else
-                    Console.WriteLine("未找到该记录，不能修改.");
+                //Console.WriteLine("修改记录");
+                //Console.WriteLine("========================================");
+                ////修改
+                //var editDepartment = context.Departments.SingleOrDefault(x => x.Name == "环境与食品检测学院");
+                //if (editDepartment != null)
+                //{
+                //    editDepartment.Name = "环境与食品检测学院";
+                //    editDepartment.SortCode = "007";
+                //    context.SaveChanges();
+                //}
+                //else
+                //    Console.WriteLine("未找到该记录，不能修改.");
 
                 //Console.WriteLine("删除记录");
                 //Console.WriteLine("========================================");
@@ -39,6 +39,17 @@ namespace DBFirstDemo
                 //context.Departments.Remove(delDept);
                 //context.SaveChanges();
 
+                Console.WriteLine("添加记录");
+                Console.WriteLine("========================================");
+                var newDept = new Departments
+                {
+                    ID = Guid.NewGuid(),
+                    Name = "测试学院",
+                    Dscn = "不可描述",
+                    SortCode = "999"
+                };
+                context.Departments.Add(newDept);
+                context.SaveChanges();
 
                 var departments1 = context.Departments.OrderBy(n => n.SortCode).ToList();
                 foreach (var d in departments1)
