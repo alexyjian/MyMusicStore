3185ea5e4ad27381c476ff16b2eaffb16b92ee36
Areart <15778250484@163.com>
2018-11-04 10:57:53 +0800

    2018-11-4

--- 我的作业和练习区/我的作业区/ASP.NET MVC/_1027EF代码优先作业/_1027EF代码优先作业/Program.cs
+++ 我的作业和练习区/我的作业区/ASP.NET MVC/_1027EF代码优先作业/_1027EF代码优先作业/Program.cs
@@ -10,6 +10,23 @@ namespace _1027EF代码优先作业
     {
         static void Main(string[] args)
         {
+            using(var context=new CuresContext())
+            {
+                foreach(var dep in context.Departments.OrderBy(x => x.SortCode).ToList())
+                {
+                    Console.WriteLine("{0}   {1}    {2}",dep.Name,dep.SortCode,dep.Dscn);
+                }
+                //添加一条纪录
+                Console.WriteLine("添加一条纪录");
+                var adddep = new Departments() {
+                    ID = Guid.Parse("08693647-b635-4a8d-a734-b6ba8f9da479"),
+                    Name="电竞与战略学院",
+                    Dscn="就是专门打游戏的学校",
+                    SortCode="010"
+                };
+                context.Departments.Add();
+                Console.ReadKey();
+            }
         }
     }
 }
