d19cf7f5b891812e44dab9b8aac6e5f8645609f2
hxm193514 <1935197914@qq.com>
2018-11-04 10:14:22 +0800

    增删改查

--- HandsomeBoy/04ASP.NET MVC/Denol/Denol/Program.cs
+++ HandsomeBoy/04ASP.NET MVC/Denol/Denol/Program.cs
@@ -22,6 +22,7 @@ namespace Denol
 
             //Console.ReadKey();
 
+            //查询记录
             using (var context = new CourseDBEntities())
             {
                 // .where .orderby .tolist() 注意顺序
@@ -32,9 +33,60 @@ namespace Denol
                 {
                     Console.WriteLine("编号{0},部门{1},说明{2}", d.SortCode, d.Name, d.Dscn);
                 }
-                //aasd
-                Console.ReadKey();
+
+
+                //Console.WriteLine("添加一条新纪录");
+                //Console.WriteLine("----------------------------------------");
+                //var newDepartment = new Department
+                //{
+                //    ID = Guid.NewGuid(),
+                //    Name = "环境与食品学院",
+                //    Dscn = "环境与食品检测",
+                //    SortCode = "005"
+                //};
+                //context.Departments.Add(newDepartment);
+                //context.SaveChanges();
+                //var departments1 = context.Departments.OrderBy(n => n.SortCode).ToList();
+                //foreach (var d in departments1)
+                //{
+                //    Console.WriteLine("编号{0},部门{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+                //}
+
+                //修改
+                //Console.Write("修改记录");
+                //Console.WriteLine("----------------------------------------");
+                
+                //var editDcpartment = context.Departments.SingleOrDefault(x => x.Name == "环境与食品学院");
+                //if (editDcpartment != null)
+                //{
+                //    editDcpartment.Name = "环境与食品检测学院";
+                //    editDcpartment.SortCode = "007";
+                //    context.SaveChanges();
+                //}
+                //else
+                //{
+                //    Console.Write("未找到该记录，不能不能修改");
+                // }
+                //    foreach (var d in departments1)
+                //    Console.WriteLine("编号{0},部门{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+
+                //删除
+                Console.Write("删除记录");
+                Console.WriteLine("----------------------------------------");
+                //find--用主键查询实体
+                var delDept = context.Departments.Find(Guid.Parse("e54fbe5d-026f-46d5-8455-bd981e6b39c5"));
+                context.Departments.Remove(delDept);
+                context.SaveChanges();
+
+                foreach (var d in departments)
+                {
+                    Console.WriteLine("编号{0},部门{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+                }
+
             }
+            Console.ReadLine();
+         
+            
         }
     }
 }
