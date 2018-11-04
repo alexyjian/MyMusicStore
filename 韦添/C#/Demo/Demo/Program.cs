6175ebc98f971538af7500901829d5677238ac3c
weitian666 <1935836331@qq.com>
2018-11-04 12:02:10 +0800

    333

--- 韦添/C#/Demo/Demo/Program.cs
+++ 韦添/C#/Demo/Demo/Program.cs
@@ -13,7 +13,7 @@ namespace Demo
             //使用数据上下文进行数据操作，using不是上下文代码的范围，执行完后内存就会自动释放
             using (var context = new CourseDBEntities())
             { 
-                var departments = context.Departments.OrderBy(n => n.SortCode).ToList();
+                var departments = context.Departments.Where(n=>n.Name.Contains("工程")).OrderBy(n => n.SortCode).ToList();
                 foreach (var d in departments)
                     Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
                 Console.ReadKey();
