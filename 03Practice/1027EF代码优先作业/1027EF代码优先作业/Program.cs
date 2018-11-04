101fa8820e75477e5324acfdaf431dbc8721002b
alexyjian <alex.yjian@gmail.com>
2018-11-04 11:23:13 +0800

    EF代码优先作业1,查询与添加

--- 03Practice/1027EF代码优先作业/1027EF代码优先作业/Program.cs
+++ 03Practice/1027EF代码优先作业/1027EF代码优先作业/Program.cs
@@ -10,6 +10,46 @@ namespace _1027EF代码优先作业
     {
         static void Main(string[] args)
         {
+            var context = new DBContext();
+            var courses = context.Courses.ToList();
+            foreach (var c in courses)
+                Console.WriteLine("课程名称：{0}  课程学分:{1}  所属学院:{2}", c.Title, c.Credit, c.Departments.Name);
+
+            Console.WriteLine("==================添加三门课程====================");
+            //添加课程
+            var c1 = new Courses()
+            {
+                ID = Guid.NewGuid(),
+                Title = "软件工程项目组织管理",
+                Credit = 17,
+                //外键的对象 一定要从同一个上下文中查询
+                Departments = context.Departments.SingleOrDefault(x => x.Name == "电子信息工程学院")
+            };
+            var c2= new Courses()
+            {
+                ID = Guid.NewGuid(),
+                Title = "算法与数据结构",
+                Credit = 2,
+                //外键的对象 一定要从同一个上下文中查询
+                Departments = context.Departments.SingleOrDefault(x => x.Name == "电子信息工程学院")
+            };
+            var c3 = new Courses()
+            {
+                ID = Guid.NewGuid(),
+                Title = "C#图像处理",
+                Credit = 2,
+                //外键的对象 一定要从同一个上下文中查询
+                Departments = context.Departments.SingleOrDefault(x => x.Name == "电子信息工程学院")
+            };
+            context.Courses.Add(c1);
+            context.Courses.Add(c2);
+            context.Courses.Add(c3);
+            context.SaveChanges();
+
+            foreach (var c in context.Courses.ToList())
+                Console.WriteLine("课程名称：{0}  课程学分:{1}  所属学院:{2}", c.Title, c.Credit, c.Departments.Name);
+
+            Console.ReadLine();
         }
     }
 }
