16e7ad556b4d1fa21c7b22f29d5807b3c121d05a
balabalaQAQ <1124048070@qq.com>
2018-11-04 10:55:13 +0800

    .

---
+++ balabalaQAQ/作业/1027EF代码优先作业/1027EF代码优先作业/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1027EF代码优先作业
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var CourseDB = new CourseDBEntities())
+            {
+                var DB = new Courses
+                {
+                    ID = Guid.NewGuid(),
+                    Title = "做作业",
+                    Credit = "009",
+                    Department_ID = Guid.Parse("a664af03-adcf-4311-b2d5-44c98264a5a0")
+                };
+                CourseDB.Courses.Add(DB);
+            }
+        }
+    }
+}
