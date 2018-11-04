042544ff114a57aff3b985149691854c467651b1
WaxdM <1575908620@qq.com>
2018-11-04 08:45:29 +0800

    465

--- vs/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ vs/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,8 +10,16 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console .Write ("黄学名是傻子 ");
-            Console.Read();
+            //使用数据上下文进行数据操作，using表示上下文代码的范围，执行完后内存会自动释放
+            using (var context = new CourseDBEntities())
+            {
+                var departments = context.dbo_Departnents.OrderBy(n => n.SortCode).ToList();
+                foreach (var d in departments)
+                {
+                    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+                    Console.ReadKey();
+                }
+            }
         }
     }
 }
