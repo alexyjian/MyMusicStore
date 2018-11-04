0e71bdc0c0d1dc726b7e96dca02972ca6c47c5e6
ChaoPiPi <1806616500@qq.com>
2018-11-04 11:32:29 +0800

    Update

---
+++ Class/C#/EF代码优先作业/Demo/Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new OneLibraryEntities())
+            {
+
+            }
+        }
+    }
+}
