f7f62ed0b1e1b10a95b6020ba765615293b6ec16
HappyWindManS <3066004354@qq.com>
2018-11-04 16:02:01 +0800

    MyComputer

---
+++ MR/MVC学习/Demo/Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //使用数据上下文进行数据操作，using表示上下文代码的范围，执行完后内存会自动释放
+            using (var context = new CourseContext())
+            {
+                var departments = context.Departments.OrderBy(n => n.SortCode).ToList();
+                foreach(var d in departments)
+                    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
