f3938f85fc9ae28ec4e7600b161981e9919fa3d5
1123781921 <1123781921@qq.com>
2018-11-04 11:46:19 +0800

    11.4作业

--- lql139/ASP.NET mvc/11.4/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ lql139/ASP.NET mvc/11.4/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,48 +10,50 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            using (var context = new CourseDBEntities())
-            {
-                var departments = context.Departments.OrderBy(n => n.Sortcode).ToList();
-                foreach (var d in departments)
-                    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.Sortcode, d.Name, d.Dscn);
-                //添加--------------
-                //var newDepartment = new Departments
-                //{
-                //    ID = Guid.NewGuid(),
-                //    Name = "环境与食品学院",
-                //    Dscn = "环境食品检测",
-                //    Sortcode = "005"
-                //};
-                //context.Departments.Add(newDepartment);
-                //context.SaveChanges();
-                //var departments1 = context.Departments.OrderBy(n => n.Sortcode).ToList();
-                //foreach (var d in departments1)
-                //    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.Sortcode, d.Name, d.Dscn);
+            var context = new CourseDBEntities();
+            var course = context.Courses.ToList();
+            foreach (var c in course)
+                Console.WriteLine("课程名称：{0},课程学分：{1},所属学院：{2}",c.Title,c.Credit,c.Departments.Name);
+            ////添加--------------------
+            //var newcour = new Courses()
+            //{
+            //    ID = Guid.NewGuid(),
+            //    Title = "软件工程项目",
+            //    Credit = 17,
+            //    Departments = context.Departments.SingleOrDefault(x => x.Name == "电子信息工程学院")
+            //};
+            //context.Courses.Add(newcour);
+            //context.SaveChanges();
+            //var course1 = context.Courses.ToList();
+            //foreach (var c in course1)
+            //    Console.WriteLine("课程名称：{0},课程学分：{1},所属学院：{2}", c.Title, c.Credit, c.Departments.Name);
+            //修改--------------------
+            //var updetecourse = context.Courses.SingleOrDefault(x => x.Title == "软件工程项目");
+            //if (updetecourse != null)
+            //{
+            //    updetecourse.Title = "软件工程大大大项目";
+            //    context.SaveChanges();     
+            //}
+            //else
+            //{ Console.WriteLine("未找到记录"); }
 
-                //修改--------------
-                //var editDepartment = context.Departments.SingleOrDefault(x => x.Name == "环境与食品学院");
-                //if (editDepartment != null)
-                //{
-                //    editDepartment.Name = "环境与食品检测学院";
-                //    context.SaveChanges();////更新数据库
-                //}
-                //else { Console.WriteLine("未找到记录"); }
-                //var departments1 = context.Departments.OrderBy(n => n.Sortcode).ToList();
-                //foreach (var d in departments1)
-                //    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.Sortcode, d.Name, d.Dscn);
+            //var course2 = context.Courses.ToList();
+            //foreach (var c in course2)
+            //    Console.WriteLine("课程名称：{0},课程学分：{1},所属学院：{2}", c.Title, c.Credit, c.Departments.Name);
 
-                //删除--------------
-                //var detdep = context.Departments.Find(Guid.Parse("c6c88ddb-8636-4d02-ba3c-7ca42818ec0b"));
-                //context.Departments.Remove(detdep);
-                //context.SaveChanges();
+            ////删除
+            //var deletecourse=context.Courses.SingleOrDefault(x => x.Title == "软件工程大大大项目");
+            //context.Courses.Remove(deletecourse);
+            //context.SaveChanges();
+            //var course3 = context.Courses.ToList();
+            //foreach (var c in course3)
+            //    Console.WriteLine("课程名称：{0},课程学分：{1},所属学院：{2}", c.Title, c.Credit, c.Departments.Name);
 
-                //var departments1 = context.Departments.OrderBy(n => n.Sortcode).ToList();
-                //foreach (var d in departments1)
-                //    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.Sortcode, d.Name, d.Dscn);
+            Console.ReadKey();
+         
 
-                Console.ReadKey();
-            }
+
         }
     }
-}
+    }
+
