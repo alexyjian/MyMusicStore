0583f3ea59fe8110276e08ab4848426187ea6c6b
alexyjian <alex.yjian@gmail.com>
2018-11-04 10:05:54 +0800

    DBFirstDemo,数据优先示例

--- 04ASP.NET MVC/DBFirstDemo/DBFirstDemo/Program.cs
+++ 04ASP.NET MVC/DBFirstDemo/DBFirstDemo/Program.cs
@@ -10,6 +10,32 @@ namespace DBFirstDemo
     {
         static void Main(string[] args)
         {
+            //使用数据上下文进行数据操作，using表示上下文代码的范围，执行完后内存会自动释放
+            using (var context = new CourseContext())
+            {
+                var departments = context.Departments.OrderBy(n => n.SortCode).ToList();
+                foreach (var d in departments)
+                    Console.WriteLine("编号:{0},部门名称:{1},说明:{2}", d.SortCode, d.Name, d.Dscn);
+
+                Console.WriteLine("修改记录");
+                Console.WriteLine("========================================");
+                //修改
+                var editDepartment = context.Departments.SingleOrDefault(x => x.Name == "环境与食品检测学院");
+                if (editDepartment != null)
+                {
+                    editDepartment.Name = "环境与食品检测学院";
+                    editDepartment.SortCode = "007";
+                    context.SaveChanges();
+                }
+                else
+                    Console.WriteLine("未找到该记录，不能修改.");
+
+                var departments1 = context.Departments.OrderBy(n => n.SortCode).ToList();
+                foreach (var d in departments1)
+                    Console.WriteLine("编号:{0},部门名称:{1},说明:{2}", d.SortCode, d.Name, d.Dscn);
+
+                Console.ReadKey();
+            }
         }
     }
 }
