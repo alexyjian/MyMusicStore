104c104bef1efb1d8d355b6524bde93197abe86e
luyusheng7 <2278814961@qq.com>
2018-11-04 12:46:48 +0800

    代码优先作业

--- luyusheng/Demol/Demol/Program.cs
+++ luyusheng/Demol/Demol/Program.cs
@@ -12,10 +12,27 @@ namespace Demol
         {
             using (var context = new CourseDBEntities1())
             {
-                //使用数据上下文进行数据操作，using表示上下文代码的范围，执行完后内存会自动释放
+                //.where .orderby .tolis()注意调用的顺序
                 var departments = context.Department.OrderBy(n => n.SortCode).ToList();
                 foreach (var d in departments)
                     Console.WriteLine("编号:{0},部门名称:{1},说明{2}",d.SortCode,d.Name,d.Dscn);
+                Console.WriteLine("添加一条新的部门记录");
+                //添加一条记录
+                var newDepartment = new Department
+                {
+                    ID = Guid.NewGuid(),
+                    Name ="环境与食品学院",
+                    Dscn = "环境与食品检测",
+                    SortCode ="007"
+                };
+                //把新的对象添加到上下文中
+                context.Department.Add(newDepartment);
+                //更新上下文，把新的实体保存到数据库中
+                context.SaveChanges();
+                //显示新的记录
+                var departments1 = context.Department.OrderBy(n => n.SortCode).ToList();
+                foreach (var d in departments1)
+                    Console.WriteLine("编号:{0},部门名称:{1},说明{2}", d.SortCode, d.Name, d.Dscn);
                 Console.ReadKey();
             }
         }
