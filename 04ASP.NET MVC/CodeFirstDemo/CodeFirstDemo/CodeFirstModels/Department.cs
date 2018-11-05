0b67d428e16521702586088e768ee65ce99c27f6
alexyjian <alex.yjian@gmail.com>
2018-11-05 17:53:04 +0800

    代码优先示例

---
+++ 04ASP.NET MVC/CodeFirstDemo/CodeFirstDemo/CodeFirstModels/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo.CodeFirstModels
+{
+    /// <summary>
+    /// 二级学院的实体
+    /// </summary>
+    public class Department
+    {
+        //实体的主键，属性的命名规则:ID、Id、类名+ID、类名+Id
+        public Guid ID { get; set; }
+        //学院名称
+        public string Name { get; set; } 
+        //说明
+        public string Description { get; set; }
+        //排序码
+        public string SortCode { get; set; }
+
+        public Department()
+        {
+            ID = Guid.NewGuid();
+        }
+    }
+}
