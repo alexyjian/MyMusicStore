d2498eb235e67b4778d188feaf1bfd9bb165fc55
weitian666 <1935836331@qq.com>
2018-11-05 17:04:50 +0800

    删除记录

--- 韦添/C#/Demo2/Demo2/Program.cs
+++ 韦添/C#/Demo2/Demo2/Program.cs
@@ -16,18 +16,30 @@ namespace Demo2
                 var departments = context.Departments.OrderBy(n => n.SortCode).ToList();
                 foreach(var d in departments)
                     Console.WriteLine("编号:{0},部门名称:{1},说明:{2}",d.SortCode,d.Name,d.Dscn);
-                Console.WriteLine("修改记录");
+
+                //Console.WriteLine("修改记录");
+                //Console.WriteLine("=======================================================================");
+                ////修改
+                //var editDepartment = context.Departments.SingleOrDefault(x => x.Name == "环境与食品检测学院");
+                //if(editDepartment!=null)
+                //{
+                //    editDepartment.Name = "环境与食品检测学院";
+                //    editDepartment.SortCode = "008";
+                //    context.SaveChanges();
+                //}
+                //else
+                //    Console.WriteLine("未找到该记录，不能修改");
+
+
+                Console.WriteLine("删除记录");
                 Console.WriteLine("=======================================================================");
-                //修改
-                var editDepartment = context.Departments.SingleOrDefault(x => x.Name == "环境与食品检测学院");
-                if(editDepartment!=null)
-                {
-                    editDepartment.Name = "环境与食品检测学院";
-                    editDepartment.SortCode = "008";
-                    context.SaveChanges();
-                }
-                else
-                    Console.WriteLine("未找到该记录，不能修改");
+                var delDept = context.Departments.Find(Guid.Parse("7771ce06-278c-4bda-9c9e-30824490460f"));
+                //var id = Guid.Parse("7771ce06-278c-4bda-9c9e-30824490460f");
+                //var delDept = context.Departments.SingleOrDefault(x => x.ID == id);
+                context.Departments.Remove(delDept);
+                context.SaveChanges();
+
+
 
                 var departments1 = context.Departments.OrderBy(n => n.SortCode).ToList();
                 foreach(var d in departments1)
