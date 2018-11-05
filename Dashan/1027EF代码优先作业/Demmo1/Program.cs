4444fe50c2e18116b6b55bb676bfe83efcf361a7
suweixi <1985748697@qq.com>
2018-11-05 12:27:22 +0800

    EF代码优先作业

--- Dashan/1027EF代码优先作业/Demmo1/Program.cs
+++ Dashan/1027EF代码优先作业/Demmo1/Program.cs
@@ -19,41 +19,41 @@ namespace Demmo1
                     Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
 
 
-                //Console.WriteLine("添加一条新的部门记录");
-                ////添加一条新的记录
-                //var newDepartment = new Departmnt
-                //{
-                //    ID = Guid.NewGuid(),
-                //    Name = "机电学院",
-                //    Dscn = "机电部",
-                //    SortCode = "008"
+                Console.WriteLine("添加一条新的部门记录");
+                //添加一条新的记录
+                var newDepartment = new Departmnt
+                {
+                    ID = Guid.NewGuid(),
+                    Name = "机电学院",
+                    Dscn = "机电部",
+                    SortCode = "008"
 
 
-                //};
+                };
 
-                ////
-                //context.Departmnts.Add(newDepartment);
-                ////
-                //context.SaveChanges();
+                //
+                context.Departmnts.Add(newDepartment);
+                //
+                context.SaveChanges();
                 //显示新的记录
 
 
 
 
 
-                //var department1 = context.Departmnts.OrderBy(n => n.SortCode).ToList();
-                //foreach (var d in department1)
-                //    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+                var department1 = context.Departmnts.OrderBy(n => n.SortCode).ToList();
+                foreach (var d in department1)
+                    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
 
 
-                //Console.WriteLine("修改记录");
-                //Console.WriteLine("=============================================");
+                Console.WriteLine("修改记录");
+                Console.WriteLine("=============================================");
 
-                ////修改
-                //var editDpartment = context.Departmnts.SingleOrDefault(x => x.Name == "艺术学院");
-                //editDpartment.Name = "艺术学院";
-                //editDpartment.SortCode = "005";
-                //context.SaveChanges();
+                //修改
+                var editDpartment = context.Departmnts.SingleOrDefault(x => x.Name == "艺术学院");
+                editDpartment.Name = "艺术学院";
+                editDpartment.SortCode = "005";
+                context.SaveChanges();
 
 
                 //else
@@ -66,15 +66,15 @@ namespace Demmo1
                 Console.WriteLine("删除记录");
                 Console.WriteLine("=============================================");
 
-                var id = Guid.Parse("a77dc643-da6f-4895-b674-dde647b42273");
-                var deIDept = context.Departmnts.SingleOrDefault(x => x.ID == id);
-                context.Departmnts.Remove(deIDept);
-                context.SaveChanges();
+                //var id = Guid.Parse("a77dc643-da6f-4895-b674-dde647b42273");
+                //var deIDept = context.Departmnts.SingleOrDefault(x => x.ID == id);
+                //context.Departmnts.Remove(deIDept);
+                //context.SaveChanges();
 
-                var department1 = context.Departmnts.OrderBy(n => n.SortCode).ToList();
-                foreach (var d in department1)
-                    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
-                Console.ReadKey();
+                //var department1 = context.Departmnts.OrderBy(n => n.SortCode).ToList();
+                //foreach (var d in department1)
+                //    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+                //Console.ReadKey();
 
 
 
