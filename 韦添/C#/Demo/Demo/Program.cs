27ddff998472ffb707c4f80671be654aee01cca6
weitian666 <1935836331@qq.com>
2018-11-05 15:52:36 +0800

    11.5

--- 韦添/C#/Demo/Demo/Program.cs
+++ 韦添/C#/Demo/Demo/Program.cs
@@ -13,9 +13,31 @@ namespace Demo
             //使用数据上下文进行数据操作，using不是上下文代码的范围，执行完后内存就会自动释放
             using (var context = new CourseDBEntities())
             { 
-                var departments = context.Departments.Where(n=>n.Name.Contains("工程")).OrderBy(n => n.SortCode).ToList();
+                var departments = context.Departments.OrderBy(n => n.SortCode).ToList();
                 foreach (var d in departments)
                     Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+                Console.WriteLine("添加一条新的部门的记录");
+                //添加一条记录
+                var newDepartment = new Departments
+                {
+                    ID = Guid.NewGuid(),
+                    Name = "环境与食品学院",
+                    Dscn = "环境与食品检测",
+                    SortCode="008"
+                  
+
+                };
+
+                //把新的对象添加到上下文
+                context.Departments.Add(newDepartment);
+                //更新上下文，把新的实体保存到数据库中
+                context.SaveChanges();
+
+                //显示新的记录
+                var departments1 = context.Departments.OrderBy(n => n.SortCode).ToList();
+                foreach (var d in departments1)
+                    Console.WriteLine("编号{0},部门{1},说明{2}", d.SortCode, d.Name, d.Dscn);
+
                 Console.ReadKey();
             }
         }
---
+++ 韦添/C#/EF代码作业1/EF代码作业1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF代码作业1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new CourseContext();
+            var courses = context.Courses.ToList();
+            foreach (var c in courses)
+                Console.WriteLine("课程名称：{0}  课程学分:{1}  所属学院:{2}", c.Title, c.Credit, c.Departments.Name);
+
+            Console.WriteLine("==================添加三门课程====================");
+            //添加课程
+            var c1 = new Courses()
+            {
+                ID = Guid.NewGuid(),
+                Title = "软件工程项目组织管理",
+                Credit = 17,
+                //外键的对象 一定要从同一个上下文中查询
+                Departments = context.Departments.SingleOrDefault(x => x.Name == "电子信息工程学院")
+            };
+            var c2 = new Courses()
+            {
+                ID = Guid.NewGuid(),
+                Title = "算法与数据结构",
+                Credit = 2,
+                Departments = context.Departments.SingleOrDefault(x => x.Name == "电子信息工程学院")
+            };
+            var c3 = new Courses()
+            {
+                ID = Guid.NewGuid(),
+                Title = "C#图像处理",
+                Credit = 2,
+                Departments = context.Departments.SingleOrDefault(x => x.Name == "电子信息工程学院")
+            };
+            context.Courses.Add(c1);
+            context.Courses.Add(c2);
+            context.Courses.Add(c3);
+            context.SaveChanges();
+
+            foreach (var c in context.Courses.ToList())
+                Console.WriteLine("课程名称：{0}  课程学分:{1}  所属学院:{2}", c.Title, c.Credit, c.Departments.Name);
+
+            Console.WriteLine("================修改一门课程======================");
+            var obj = context.Courses.SingleOrDefault(x => x.Title == "商务英语");
+            if (obj != null)
+            {
+                obj.Title = "计算机英语";
+                obj.Credit = 4;
+                obj.Departments = context.Departments.SingleOrDefault(x => x.Name == "电子信息工程学院");
+                context.SaveChanges();
+            }
+            else
+                Console.WriteLine("未找到改课程，无法修改！");
+            foreach (var c in context.Courses.ToList())
+                Console.WriteLine("课程名称：{0} 课程学分{1} 所属学院{2}", c.Title, c.Credit, c.Departments.Name);
+
+            //Console.WriteLine("================删除一门课程======================");
+            //var delobj = context.Courses.Find(Guid.Parse("eddc1f62-013d-4324-9b7f-e52994d338d5"));
+            //context.Courses.Remove(delobj);
+            //context.SaveChanges();
+            //foreach (var c in context.Courses.ToList())
+            //    Console.WriteLine("课程名称：{0} 课程学分{1} 所属学院{2}", c.Title, c.Credit, c.Departments.Name);
+
+            Console.ReadLine();
+
+        }
+    }
+}
