9a5da84804a8985220a5350f29566d31a26a09ea
Areart <15778250484@163.com>
2018-11-05 18:01:13 +0800

    代码优先实例

--- CodeFirstDemo/CodeFirstDemo/Program.cs
+++ CodeFirstDemo/CodeFirstDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CodeFirstDemo.CodeFirstModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,10 @@ namespace CodeFirstDemo
     {
         static void Main(string[] args)
         {
-
+            var stu= new CourseContext().Students.OrderBy(x => x.StudentCode).ToList();
+            foreach (var s in stu)
+                Console.WriteLine("学号：{0}",s.StudentCode);
+            Console.ReadKey();
         }
     }
 }
