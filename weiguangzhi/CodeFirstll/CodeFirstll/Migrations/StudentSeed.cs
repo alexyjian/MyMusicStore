5fac4110468d90bc7686fd71cf7a6bbb7af6bbfa
wqeasd <281437024@qq.com>
2018-11-05 18:11:32 +0800

    11-5

---
+++ weiguangzhi/CodeFirstll/CodeFirstll/Migrations/StudentSeed.cs
@@ -0,0 +1,28 @@
+﻿using CodeFirstll.CodeFirstModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace CodeFirstll.Migrations
+{
+    public class StudentSeed
+    {
+        public static void Seed(CourseContext context)
+        {
+            var stu1 = new Student()
+            {
+                StudentCode = "0001",
+                Name = "无言组",
+                Address = "武当山",
+                Birthday = DateTime.Parse("2018-01-01"),
+                Phone = "1111111",
+                Department = context.Departments.SingleOrDefault(x => x.Name == "电子信息工程学院")
+
+            };
+        }
+    }
+}
