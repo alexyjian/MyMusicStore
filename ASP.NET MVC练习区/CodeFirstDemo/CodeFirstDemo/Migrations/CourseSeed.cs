e3942250484bf755e1aab52c1d35741819ffbb96
2876416732 <2876416732@qq.com>
2018-11-05 18:29:41 +0800

    2018.11.5

---
+++ ASP.NET MVC练习区/CodeFirstDemo/CodeFirstDemo/Migrations/CourseSeed.cs
@@ -0,0 +1,17 @@
+﻿using CodeFirstDemo.CodeFirstModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo.Migrations
+{
+    public class CourseSeed
+    {
+        public static void Seed(CourseContext context)
+        {
+
+        }
+    }
+}
