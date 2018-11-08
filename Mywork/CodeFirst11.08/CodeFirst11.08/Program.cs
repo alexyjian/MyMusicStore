d80cc9f0b3979b10d71b30e278d3b0812764406f
Baiguia <1942152252@qq.com>
2018-11-08 08:09:17 +0800

    11.08

---
+++ Mywork/CodeFirst11.08/CodeFirst11.08/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst11._08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
