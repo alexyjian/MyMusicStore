69d7be72aee168d14a062c0b6ae58c019b916d74
hxm193514 <1935197914@qq.com>
2018-11-08 12:17:20 +0800

    11/8

---
+++ HandsomeBoy/03Practice/11-8/CodeFirt1108/CodeFirt1108/DataContext/StuDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using CodeFirt1108.Entities;
+
+namespace CodeFirt1108.DataContext
+{
+   public class StuDBContext:DbContext
+    {
+        public DbSet<DepartMents> DepartMent { get; set; }
+        public DbSet<Student> Studetnt { get; set; }
+    }
+}
---
+++ HandsomeBoy/03Practice/11-8/CodeFirt1108/CodeFirt1108/Entities/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirt1108.Entities
+{
+   public class Student
+    {
+        public Guid ID { get; set; }
+  
+        public string StudentNo { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public DateTime BirhDay { get; set; } = DateTime.Parse("1999-11-24");
+
+        public string Address { get; set; }
+        public bool Sex { get; set; }
+        public string phone { get; set; }
+
+        public Student() {
+            this.ID = Guid.NewGuid();
+        }
+    }
+}
