006a99002595907556b4a24c32968786bf63c830
weitian666 <1935836331@qq.com>
2018-11-08 10:39:55 +0800

    练习

---
+++ 韦添/C#/CodeFirstDemo/CodeFirstDemo/CodeFirstModels/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo.CodeFirstModels
+{
+   public class Course
+    {
+
+        public Guid ID { get; set; }
+
+
+        public string Title { get; set; }
+
+        public int Credit { get; set; } = 1;
+
+        public virtual Department Department { get; set; }
+
+        public Course()
+        {
+            ID = Guid.NewGuid();
+        }
+    }
+}
