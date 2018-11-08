ca595220439bbe9baab9268a4efc3bf526f7d008
huangmeixiqi <1600582963@qq.com>
2018-11-08 12:45:41 +0800

    随机创建1000名学生，并去掉重复

---
+++ huangmeixi作业区/SAP.NET MVC 作业区/1108EF作业_随机生成1000名同学入库/CodeFirst/CodeFirst/Entities/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst.Entities
+{
+  public  class Student
+    {
+        public Guid ID { get; set; }
+        public string StudentNO { get; set; }
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public bool Sex { get; set; } = true;
+        public DateTime BirthDay { get; set; }=DateTime.Parse("1998-12-30");
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public virtual DepartMent  Department{ get; set; }
+    public Student()
+    {
+        this.ID = Guid.NewGuid();
+    }
+        public string GetFullName()
+        {
+            this.FullName = this.FristName + this.LastName;
+            return this.FullName;
+        }
+}
+}
