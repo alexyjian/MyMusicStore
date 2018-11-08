b6df5c62473ab3312b8bdabe8ef0b1b8c6aac435
WaxdM <1575908620@qq.com>
2018-11-08 08:44:27 +0800

    测试

---
+++ 185/CodeFirst1108/CodeFirst1108/Entities/DeparMent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst1108.Entities
+{
+  public   class DeparMent
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string SortCode { get; set; }
+        public string Description { get; set; }
+        public DeparMent()
+        {
+            this.ID = Guid.NewGuid();
+        }
+    }
+}
---
+++ 185/CodeFirst1108/CodeFirst1108/Entities/Studetnt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst1108.Entities
+{
+    public  class Studetnt
+    {
+        public Guid ID { get; set; }
+        public string StudentNo { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+        public bool Sex { get; set; } = true;
+        public DateTime BirthDay { get; set; } = DateTime.Parse("1990-01-01");
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public virtual DeparMent Department {get;set;}
+        public Studetnt()
+        {
+            this.ID = Guid.NewGuid();
+        }
+        public string GetFullName()
+        {
+            this.FullName = this.FullName + this.LastName;
+            return this.FullName;
+        }
+    }
+}
