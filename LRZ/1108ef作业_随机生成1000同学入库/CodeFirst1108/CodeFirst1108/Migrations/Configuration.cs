62be988d38d0b26beeee9d87ad446c973f253924
lrz991222 <353226337@qq.com>
2018-11-08 12:10:36 +0800

    1108EF作业

---
+++ LRZ/1108ef作业_随机生成1000同学入库/CodeFirst1108/CodeFirst1108/Migrations/Configuration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst1108.Migrations
+{
+    internal sealed class Configuration: DbMigrationsConfiguration<CodeFirst1108.DataContext.StuDBcontext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(CodeFirst1108.DataContext.StuDBcontext context)
+        {
+            DepartmentSeed.Seed(context);
+        }
+    }
+}
