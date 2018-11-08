315fe46b8bfc0a931e403dacfd98e782c9566018
2876416732 <2876416732@qq.com>
2018-11-08 12:40:18 +0800

    CodeFirst1108 随机1000学生信息入库

--- 我的作业区/1108随机生成1000位同学入库/CodeFirst1108/CodeFirst1108/Entities/DepartMent.cs
+++ 我的作业区/1108随机生成1000位同学入库/CodeFirst1108/CodeFirst1108/Entities/DepartMent.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace CodeFirst1108.Entities
 {
-    class DepartMent
+    public class DepartMent
     {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string SortCode { get; set; }
+
+        public string Description { get; set; }
+        public DepartMent()
+        {
+            this.ID = Guid.NewGuid();
+        }
     }
 }
