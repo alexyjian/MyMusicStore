6f1b654bee41d8a347df5c3294a4e165156ad215
HappyWindManS <3066004354@qq.com>
2018-11-08 08:44:42 +0800

    708studen;11/8,8:44

---
+++ 1000Student/1000Student/Entities/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1000Student.Entities
+{
+    public class Student
+    {
+        public Guid ID { get; set; }
+        public string StudentNo { get; set; }
+        public string FirstName { get; set; }
+        public string lastName { get; set; }
+        public string FullName { get; set; }
+        public bool Sex { get; set; } = true;
+        public DateTime BirthDay { get; set; } = DateTime.Parse("1998-01-01");
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public virtual DepartMent DepartMent { get; set; }
+        public Student()
+        {
+            this.ID = Guid.NewGuid();
+        }
+    }
+}
