28826cfb6f96076754bf206994d12fca7d0daa8c
luyusheng7 <2278814961@qq.com>
2018-11-08 12:16:35 +0800

    1108作业_随机

--- luyusheng/03Practice作业文件夹/CodeFirst1108/CodeFirst1108/Program.cs
+++ luyusheng/03Practice作业文件夹/CodeFirst1108/CodeFirst1108/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using CodeFirst1108.Migrations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CodeFirst1108
@@ -10,6 +12,13 @@ namespace CodeFirst1108
     {
         static void Main(string[] args)
         {
+            for (var i=0; i<10; i++)
+            {
+                var fname = "";
+                var lname = "";
+                Console.WriteLine(StudentSeed._GetRandomChineseFullName(ref fname, ref lname));
+                Thread.Sleep(3);
+            }
         }
     }
 }
