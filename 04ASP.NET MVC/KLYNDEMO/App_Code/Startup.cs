394d242150eb4b3a4bc14eeeb78101396fa3a524
mwailxx <1650098394@qq.com>
2018-11-09 12:16:52 +0800

    11-9上午未完成的作业

---
+++ 04ASP.NET MVC/KLYNDEMO/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KLYNDEMO.Startup))]
+namespace KLYNDEMO
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
