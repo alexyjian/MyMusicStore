e1c836f8e301968f877712fe776aa2174cb54668
Pretty33 <321838579@qq.com>
2018-11-09 08:41:21 +0800

    2018.11.9  APS.NETWebForm示例

---
+++ WAnnie/04ASP.NET MVC/APS.NETWebForm示例/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(APS.NETWebForm示例.Startup))]
+namespace APS.NETWebForm示例
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
