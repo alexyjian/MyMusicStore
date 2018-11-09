3d6c5f8e87d9a54b19076e62807f4d1e7ffc065b
alexyjian <alex.yjian@gmail.com>
2018-11-09 11:52:49 +0800

    下拉菜单

--- 04ASP.NET MVC/WebFormDemo/ASPNETWEB/Default.aspx.cs
+++ 04ASP.NET MVC/WebFormDemo/ASPNETWEB/Default.aspx.cs
@@ -9,6 +9,10 @@ public partial class _Default : Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        var depts = new StuContext.StuDBContext().DepartMents.ToList();
+        DropDownList1.DataSource = depts;
+        DropDownList1.DataTextField = "Name";
+        DropDownList1.DataValueField = "ID";
+        DropDownList1.DataBind();
     }
 }
