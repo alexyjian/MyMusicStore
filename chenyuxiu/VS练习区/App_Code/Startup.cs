f94f1b0091e8bf60148733d068c6dc32c07fa6ce
kawayi1727 <1163695520@qq.com>
2018-11-09 09:14:44 +0800

    Webform示例

---
+++ chenyuxiu/VS练习区/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VS练习区.Startup))]
+namespace VS练习区
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
