1df3b6c0df928a391bbb474a47ac23f6430358ac
Seezhonghuai <1875680955@qq.com>
2018-11-11 18:01:08 +0800

    11-11

---
+++ zhonghuai/WebFormDemo/DataContext/StuDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContext
+{
+    //用来挂实体
+    public class StuDBContext:DbContext
+    {
+        public DbSet<DepartMent> DepartMents { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ zhonghuai/WebFormDemo/Entities/DepartMent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class DepartMent
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string SortCode { get; set; }
+        public DepartMent()
+        {
+            ID = Guid.NewGuid();
+        }
+    }
+}
