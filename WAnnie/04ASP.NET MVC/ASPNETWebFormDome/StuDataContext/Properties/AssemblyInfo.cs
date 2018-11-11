e8d9b00c9fcdffaba31555a2e387b3f473364441
Pretty33 <321838579@qq.com>
2018-11-11 18:37:10 +0800

    2018.11.11 增加实体框架EF到Webform项目

--- WAnnie/04ASP.NET MVC/ASPNETWebFormDome/StuDataContext/Properties/AssemblyInfo.cs
+++ WAnnie/04ASP.NET MVC/ASPNETWebFormDome/StuDataContext/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("StuContext")]
+[assembly: AssemblyTitle("StuDataContext")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("StuContext")]
+[assembly: AssemblyProduct("StuDataContext")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("6cc1a20e-f7d7-4a3d-a8f2-b93a8266a253")]
+[assembly: Guid("79fffd11-427d-41ef-8cad-637235974977")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
