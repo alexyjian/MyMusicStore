54662e031a05816a864f677b0034b7851244ddfa
JIEJIEJIEJI <1105658417@qq.com>
2018-11-11 18:06:29 +0800

    11.11

---
+++ Lianxi/ASPNETP/DataContext/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Entities;
+namespace DataContext
+{
+    public class DbContext:System.Data.Entity.DbContext
+        {
+        public DbSet<category> Categories { get; set; }
+        public DbSet<Prpduct> Puoducts { get; set; }
+   
+    }
+}
---
+++ Lianxi/ASPNETP/Entities/Prpduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+   public class Prpduct
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string SN { get; set; }
+        public string DSCN { get; set; }
+        public virtual  category Categoty { get; set; }
+        public Prpduct()
+        {
+            this.ID = Guid.NewGuid();
+        }
+    }
+}
