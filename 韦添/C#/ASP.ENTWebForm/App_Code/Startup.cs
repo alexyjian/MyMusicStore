d1d5bdcbc088b7f1240300e22ae48ab0f9215e4f
weitian666 <1935836331@qq.com>
2018-11-11 17:08:44 +0800

    Webform

---
+++ 韦添/C#/ASP.ENTWebForm/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.ENTWebForm.Startup))]
+namespace ASP.ENTWebForm
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ 韦添/C#/ASP.ENTWebForm/Default.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">    
+    Hello   班长！！！！    
+</asp:Content>
