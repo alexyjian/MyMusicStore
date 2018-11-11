66c1ffee87e672f96d233f1d2ae9514627e69fd9
suweixi <1985748697@qq.com>
2018-11-11 13:52:53 +0800

    ASP.netWebForm

---
+++ Dashan/ASPnetWebForm1/DataContext/Migrations/Configuration.cs
@@ -0,0 +1,22 @@
+namespace DataContext.Migrations
+{
+    using global::Migrations;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DataContext.StuDBCotext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(DataContext.StuDBCotext context)
+        {
+            DepartmentSeed.Seed(context);
+            StudentSeed.Seed(context);
+        }
+    }
+}
---
+++ Dashan/ASPnetWebForm1/DataContext/Migrations/DepartmentSeed.cs
@@ -0,0 +1,46 @@
+﻿using DataContext;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migrations
+{
+   public class DepartmentSeed
+    {
+        public static void Seed(StuDBCotext context)
+        {
+            var d1 = new DepartMent()
+            {
+                Name = "电子信息工程学院",
+                Description = "电子信息工程学院",
+                SortCode = "001",
+            };
+            var d2 = new DepartMent()
+            {
+                Name = "贸易与旅游工程学院",
+                Description = "贸易与旅游工程学院",
+                SortCode = "002",
+            };
+            var d3 = new DepartMent()
+            {
+                Name = "汽修工程学院",
+                Description = "汽修工程学院",
+                SortCode = "003",
+            };
+            var d4 = new DepartMent()
+            {
+                Name = "艺术学院",
+                Description = "艺术学院",
+                SortCode = "004",
+            };
+            context.DepartMents.Add(d1);
+            context.DepartMents.Add(d2);
+            context.DepartMents.Add(d3);
+            context.DepartMents.Add(d4);
+            context.SaveChanges();
+        }
+    }
+}
