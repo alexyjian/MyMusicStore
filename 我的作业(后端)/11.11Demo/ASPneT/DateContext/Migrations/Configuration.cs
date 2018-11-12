03e20b2776bcd43576b0348078027df8be638659
Administrator <Administrator@ts70842>
2018-11-12 18:11:06 +0800

    11.12Demo测试2

---
+++ 我的作业(后端)/11.11Demo/ASPneT/DateContext/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace DateContext.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DateContext.ProductDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(DateContext.ProductDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
