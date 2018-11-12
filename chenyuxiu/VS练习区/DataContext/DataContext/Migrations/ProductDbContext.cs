3720f9ca1b5b5d3bd3215173eaa67417172c5065
kawayi1727 <1163695520@qq.com>
2018-11-12 22:51:07 +0800

    注释种子

---
+++ chenyuxiu/VS练习区/DataContext/DataContext/Migrations/ProductDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DataContext.Migrations;
+using Entities;
+using System.Runtime.Remoting.Contexts;
+
+namespace DataContext.Migrations
+{
+    public class ProductDbContext : Context
+    {
+        public DbSst<Product> Categories { get; set; }
+        public DbSst<Product> Products { get; set; }
+
+    }
+
+}
