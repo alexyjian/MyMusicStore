b9dc9aaf633638ab92b5accc5326cf457438be3a
mwailxx <1650098394@qq.com>
2018-11-12 18:20:56 +0800

    11-12xw加个微信2

---
+++ 04ASP.NET MVC/WebSite3/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSite3.Startup))]
+namespace WebSite3
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
