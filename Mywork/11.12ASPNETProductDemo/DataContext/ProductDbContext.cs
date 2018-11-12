80ace1fa9a9dbeeafa0c371cd29b7af78d8396af
Baiguia <1942152252@qq.com>
2018-11-12 17:31:26 +0800

    11.12

---
+++ Mywork/11.12ASPNETProductDemo/DataContext/ProductDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace DataContext
+{
+    public  class ProductDbContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
