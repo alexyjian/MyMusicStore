20c5da3c4628fb62367e3817c9339b6b51a83279
suweixi <1985748697@qq.com>
2018-11-12 22:15:46 +0800

    删除修改

---
+++ Dashan/ASPnetWebForm/Web/Web/ProductList.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ProductList.aspx.cs" Inherits="Web.ProductList" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h4>商品列表</h4>
+    <h4>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" Caption="商品报表" DataKeyNames="ID" OnPageIndexChanging="GridView1_PageIndexChanging" OnRowDeleting="GridView1_RowDeleting" PageSize="18" Width="100%" AllowPaging="True" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating">
+            <Columns>
+                <asp:BoundField DataField="SN" HeaderText="商品编号" />
+                <asp:BoundField DataField="Name" HeaderText="商品名称" />
+                <asp:BoundField DataField="DSCN" HeaderText="商品说明">
+                <ItemStyle Width="300px" />
+                </asp:BoundField>
+                <asp:TemplateField HeaderText="维护操作" ShowHeader="False">
+                    <EditItemTemplate>
+                        <asp:LinkButton ID="LinkButton1" runat="server" CausesValidation="True" CommandName="Update" Text="更新"></asp:LinkButton>
+                        &nbsp;<asp:LinkButton ID="LinkButton2" runat="server" CausesValidation="False" CommandName="Cancel" Text="取消"></asp:LinkButton>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="LinkButton1" runat="server" CausesValidation="False" CommandName="Edit" Text="编辑"></asp:LinkButton>
+                        &nbsp;<asp:LinkButton ID="LinkButton2" runat="server" CausesValidation="False" CommandName="Delete" Text="删除"  OnClientClick="return confirm('你真的要如此狠心。。。删了我么？');" ></asp:LinkButton>
+                    </ItemTemplate>
+                    <ItemStyle Width="150px" />
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </h4>
+</asp:Content>
---
+++ Dashan/ASPnetWebForm/Web/Web/ProductList.aspx.cs
@@ -0,0 +1,77 @@
+﻿using DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web
+{
+    public partial class ProductList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //页面初次加载
+            if (!IsPostBack)
+                _getData();
+        }
+        private void _getData()
+        {
+            using (var context = new ProductDbContext())
+            {
+                //查询出Product数据
+                var productList = context.Products.OrderBy(x => x.SN).ToList();
+                GridView1.DataSource = productList;
+                GridView1.DataBind();
+            }
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            _getData();
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            var id = Guid.Parse(GridView1.DataKeys[e.RowIndex].Value.ToString());
+            using (var context =new ProductDbContext())
+            {
+                //删除记录
+                var delProduct = context.Products.Find(id);
+                context.Products.Remove(delProduct);
+                context.SaveChanges();
+            }
+            _getData();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            _getData();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            var id = Guid.Parse(GridView1.DataKeys[e.RowIndex].Value.ToString());
+            using (var context = new ProductDbContext())
+
+            {
+                var p = context.Products.Find(id);
+                var row = GridView1.Rows[e.RowIndex];
+                var sn = (row.Cells[0].Controls[0] as TextBox).Text.Trim();
+                var name = (row.Cells[1].Controls[0] as TextBox).Text.Trim();
+                var dscn = (row.Cells[2].Controls[0] as TextBox).Text.Trim();
+
+                p.SN = sn;
+                p.Name = name;
+                p.DSCN = dscn;
+                context.SaveChanges();
+
+            }
+            GridView1.EditIndex = -1;
+            _getData();
+        }
+    }
+}
