9fab7e832abcc38575a0a0cf70c1d1416ffdaaa5
mwailxx <1650098394@qq.com>
2018-11-14 16:21:30 +0800

    11-14下午打招呼

---
+++ 04ASP.NET MVC/WebSite5/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSite5.Startup))]
+namespace WebSite5
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
