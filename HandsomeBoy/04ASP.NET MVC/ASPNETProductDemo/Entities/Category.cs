813a8a2b14b974a1ec14df5079d40a82c27809d7
hxm193514 <1935197914@qq.com>
2018-11-14 18:12:27 +0800

    11/14

---
+++ HandsomeBoy/04ASP.NET MVC/ASPNETProductDemo/Entities/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+   public class Category
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string SortCode { get; set; }
+
+        public Category()
+        {
+            this.ID = Guid.NewGuid();
+        }
+    }
+}
---
+++ HandsomeBoy/04ASP.NET MVC/ASPNETProductDemo/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+   public class Product
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string SN { get; set; }
+        public string DSCN { get; set; }
+        public virtual Category Categoty { get; set; }
+        public Product()
+        {
+            this.ID = Guid.NewGuid();
+        }
+    }
+}
