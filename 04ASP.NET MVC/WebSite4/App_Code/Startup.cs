7b8c9307e3f77f9b2dd49e41d33ba9caad5ca41c
mwailxx <1650098394@qq.com>
2018-11-14 15:52:16 +0800

    11-14下午加个微信的修改

---
+++ 04ASP.NET MVC/WebSite4/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSite4.Startup))]
+namespace WebSite4
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ 04ASP.NET MVC/WebSite4/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Label1.Text = "你好，你叫什么名字，加个微信";
+    }
+}
