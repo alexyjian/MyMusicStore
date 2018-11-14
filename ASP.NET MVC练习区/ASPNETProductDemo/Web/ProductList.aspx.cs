78851cb0d348f0e82ea0b3d50710c67e2eea5992
2876416732 <2876416732@qq.com>
2018-11-14 15:27:14 +0800

    显示所有产品信息（2018.11.14）

---
+++ ASP.NET MVC练习区/ASPNETProductDemo/Web/ProductList.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataContext;
+
+
+public partial class ProductList : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //页面初次加载时，加载数据
+        if (!IsPostBack)
+            _getData();
+    }
+
+    private void _getData()
+    {
+        using (var context = new ProductDbContext())
+        {
+            //查询出Product数据
+            var productList = context.Products.OrderBy(x=>x.SN).ToList();
+            GridView1.DataSource = productList;
+            GridView1.DataBind();
+        }
+    }
+}
