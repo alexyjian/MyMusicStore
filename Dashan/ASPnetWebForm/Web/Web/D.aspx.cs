e500654790d044ba7475564858d15583c7a403b4
suweixi <1985748697@qq.com>
2018-11-15 10:37:13 +0800

    ID传值影藏敏感信息

---
+++ Dashan/ASPnetWebForm/Web/Web/D.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web
+{
+    public partial class D : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Session["str"] = TextBox1.Text;
+            Response.Redirect("~/E.aspx");
+        }
+    }
+}
---
+++ Dashan/ASPnetWebForm/Web/Web/E.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web
+{
+    public partial class C : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["str"] != null)
+                Label1.Text = Session["str"].ToString();       
+        }
+    }
+}
