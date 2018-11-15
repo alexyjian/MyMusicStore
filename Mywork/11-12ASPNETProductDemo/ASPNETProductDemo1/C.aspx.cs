3fdf7157406e9c30966d0ed9a7d5022a43c56ef2
Baiguia <1942152252@qq.com>
2018-11-15 11:01:58 +0800

    11-15

---
+++ Mywork/11-12ASPNETProductDemo/ASPNETProductDemo1/C.aspx.cs
@@ -0,0 +1,22 @@
+﻿using DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class C : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        var category = new ProductDbContext().Categories.First();
+        Session["C"] = category;
+        Response.Redirect("~/D.aspx");
+    }
+}
---
+++ Mywork/11-12ASPNETProductDemo/ASPNETProductDemo1/D.aspx.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class D : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(Session["C"]!=null)
+        {
+            var c = Session["C"] as Category;
+            Label1.Text = c.SortCode + "," + c.Name;
+        }
+    }
+}
