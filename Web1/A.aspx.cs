53226d429ffe17793d503ac7b3c3de73101d63fa
JIEJIEJIEJI <1105658417@qq.com>
2018-11-15 12:10:34 +0800

    11.15

---
+++ Web1/A.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class A : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var str = textBox1.Text;
+        Response.Redirect("b.aspx?s=" + str);
+    }
+}
