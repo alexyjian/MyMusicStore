d041c35f08174d95aaaa5b75ae35373d5c96cc2e
Pretty33 <321838579@qq.com>
2018-11-15 10:24:56 +0800

    2018.11.15  明细页的制作

---
+++ WAnnie/04ASP.NET MVC/ASPNETProtuctDemo/Web/A.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class A : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        var str1 = TextBox1.Text;
+        var str2 = TextBox2.Text;
+
+        //域名？参数1=值+$参数2=值.....
+        Response.Redirect("~/b.aspx?s1="+str1 + "&s2=" + str2);  
+    }
+}
