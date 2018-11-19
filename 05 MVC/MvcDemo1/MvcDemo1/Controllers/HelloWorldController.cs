a92b9318163dc6a59db1b4daa17b3842792b438b
ljiancheng <1130520078@qq.com>
2018-11-19 17:12:24 +0800

    15/19

---
+++ 05 MVC/MvcDemo1/MvcDemo1/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            // return View("../../Views/Text/text1");
+            return View();
+        }
+        public string Welcome(string name,int id=1)
+        {
+            return "你好," + name + ",欢迎次数:" + id;
+        }
+    }
+}
