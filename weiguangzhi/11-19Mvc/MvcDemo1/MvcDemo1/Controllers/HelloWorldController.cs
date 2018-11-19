c40f37717bcd707a8f917987b46a61b341f7f086
wqeasd <281437024@qq.com>
2018-11-19 16:06:08 +0800

    11-19

---
+++ weiguangzhi/11-19Mvc/MvcDemo1/MvcDemo1/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "这是我的<b>默认</b>控制器(action)";
+        }
+
+        public string WelCome()
+        {
+            return "这是我的<b>欢迎</b>控制器(action)";
+        }
+    }
+}
