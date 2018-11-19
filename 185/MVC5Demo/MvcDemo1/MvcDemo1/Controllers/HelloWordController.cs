8367b70cb76bd29591b97542566b9e7ac556dfde
WaxdM <1575908620@qq.com>
2018-11-19 17:46:22 +0800

    mvd

---
+++ 185/MVC5Demo/MvcDemo1/MvcDemo1/Controllers/HelloWordController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo1.Controllers
+{
+    public class HelloWordController : Controller
+    {
+        // GET: HelloWord
+        public ActionResult  Index()
+        {
+            return View("../../Views/Test/test1");
+        }
+        public ActionResult  Welcome(string name,int id=1)
+        {
+            ViewBag.ID = id;
+            ViewBag.Name = name+",您好！";
+            return View();
+        }
+    }
+}
