ffc89a1de335168811f1d445fdfc3504546f1143
huangmeixiqi <1600582963@qq.com>
2018-11-19 17:53:01 +0800

    MVC简介

---
+++ hmx练习区/VS练习区/MVC5Demo/MVCDemo1/MVCDemo1/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "这是我的<b>默认</b>控制器(action)";
+        }
+        public string Welcome(string name, int numtimes = 1)
+        {
+            //return "这是我的<b>欢迎</b>控制器方法(action)";
+            return "你好，" + name + "，欢迎次数：" + numtimes;
+        }
+    }
+}
---
+++ huangmeixi作业区/SAP.NET MVC 作业区/MVC5Demo/MvcDemo1/MvcDemo1/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View("../../Views/Test/Test");
+
+            //return View();
+
+        }
+        public ActionResult Welcome(string name,int id=1)
+        {
+            //return "这是我的<b>欢迎</b>控制器方法(action)";
+            //return "你好，" + name + "，欢迎次数：" + numtimes;
+            ViewBag.ID = id;
+            ViewBag.name = name+",您好!";
+            Session["message"] = "明天会更好！";
+            return View();
+        }
+    }
+}
