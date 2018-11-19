b3073cfb06f29c16d474d4f1c028c955e574ad05
Baiguia <1942152252@qq.com>
2018-11-19 17:45:52 +0800

    MVC 简介

---
+++ Mywork/11-19 MvcDemo/11-19 MvcDemo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _11_19_MvcDemo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
