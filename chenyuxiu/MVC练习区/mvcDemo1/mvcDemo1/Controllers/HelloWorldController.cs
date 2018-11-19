abd000e630df18f2cc6c327a6948e16b0f2612ce
kawayi1727 <1163695520@qq.com>
2018-11-19 18:06:25 +0800

    MVC简介

---
+++ chenyuxiu/MVC练习区/mvcDemo1/mvcDemo1/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcDemo1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View("../../Views/Test/Test");
+        }
+
+        public ActionResult Welcome(string name,int id=1)
+        {
+            return View();
+        }
+        public ActionResult Welcome1(string name, int id = 1)
+        {
+            ViewBag.ID = id;
+            ViewBag.Name = name +",您好！";
+            Session["message"] = "今天天气不错!!!!";
+            return View();
+        }
+    }
+}
