9b6545acb97530ced626a8a4d72d2f56aedefe3f
JIEJIEJIEJI <1105658417@qq.com>
2018-11-19 18:05:38 +0800

    11.19web

--- Lianxi/Mvc5Demo/MycDeno1/MycDeno1/Controllers/HelloWoldController.cs
+++ Lianxi/Mvc5Demo/MycDeno1/MycDeno1/Controllers/HelloWoldController.cs
@@ -9,13 +9,23 @@ namespace MycDeno1.Controllers
     public class HelloWoldController : Controller
     {
         // GET: HelloWold
-        public string Index()
+        public ActionResult Index()
         {
-            return "这是我的<b>默认</b>控制器方法(action)";
+            return View();
         }
-        public string Welcome( string name,int id)
+        //public string Index()
+        //{
+        //    return "这是我的<b>默认</b>控制器方法(action)";
+        ////}
+        //public string Welcome( string name,int id)
+        //{
+        //    return "您好"+name+"欢迎次数:"+id;
+        //}
+        public ActionResult Welcome(string name, int id = 1)
         {
-            return "您好"+name+"欢迎次数:"+id;
+            ViewBag.ID = id;
+            ViewBag.Name = name + ",爱您";
+            return View();
         }
     }
 }
