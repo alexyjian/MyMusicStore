94b8983e6de44ea9a0600860964abd388d4f519d
Seezhonghuai <1875680955@qq.com>
2018-11-19 16:39:15 +0800

    测试

---
+++ zhonghuai/mvc/MvcDemo1/MvcDemo1/Controllers/HelloWordController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo1.Controllers
+{
+    public class HelloWordController : Controller
+    {
+        // GET: HelloWord
+        public string Index()
+        {
+            return "这是我的<b>默认的</b>控制器方法(action)";
+        }
+
+        public string Welcome(string name, int id = 1)
+        {
+            return "您好" + name + "欢迎次数" + id;
+
+        }
+    }
+}
