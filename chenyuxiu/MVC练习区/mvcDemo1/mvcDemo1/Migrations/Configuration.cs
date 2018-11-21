a0ec74fc3b88cd5d056436c071d77c1bed79970f
kawayi1727 <1163695520@qq.com>
2018-11-21 16:34:35 +0800

    MVC EF 增删查改

---
+++ chenyuxiu/MVC练习区/mvcDemo1/mvcDemo1/Migrations/Configuration.cs
@@ -0,0 +1,63 @@
+﻿namespace mvcDemo1.Migrations
+{
+    using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<mvcDemo1.Models.MovieDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(mvcDemo1.Models.MovieDBContext context)
+        {
+            context.Database.ExecuteSqlCommand("delete movies");
+            var m1 = new Movie()
+            {
+                Title = "复仇者联盟IV",
+                ReleaseDate = DateTime.Parse("2019-2-2"),
+                Genre = "3D 动作 好莱坞",
+                Prce = 50.00M,
+            };
+            var m2 = new Movie()
+            {
+                Title = "怪物猎人",
+                ReleaseDate = DateTime.Parse("2018-12-12"),
+                Genre = "3D 动作 好莱坞",
+                Prce = 30.00M,
+            };
+            var m3 = new Movie()
+            {
+                Title = "极限特工",
+                ReleaseDate = DateTime.Parse("2019-2-2"),
+                Genre = "2D 动作 好莱坞",
+                Prce = 20.00M,
+            };
+            var m4 = new Movie()
+            {
+                Title = "银河护卫队",
+                ReleaseDate = DateTime.Parse("2019-2-2"),
+                Genre = "3D 动作 好莱坞",
+                Prce = 40.00M,
+            };
+            var m5 = new Movie()
+            {
+                Title = "毒液：致命守护者",
+                ReleaseDate = DateTime.Parse("2019-2-2"),
+                Genre = " 动作 好莱坞",
+                Prce = 50.00M,
+            };
+            context.Moves.Add(m1);
+            context.Moves.Add(m2);
+            context.Moves.Add(m3);
+            context.Moves.Add(m4);
+            context.Moves.Add(m5);
+            context.SaveChanges();
+
+        }
+    }
+}
---
+++ chenyuxiu/MVC练习区/mvcDemo1/mvcDemo1/Models/MovieDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mvcDemo1.Models
+{
+    public class MovieDBContext:DbContext
+    {
+        public DbSet<Movie> Moves { get; set; }
+    }
+}
