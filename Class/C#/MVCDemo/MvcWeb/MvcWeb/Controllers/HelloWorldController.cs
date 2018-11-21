61a9af8e6174ec65b47723fde5987445b85faf0d
ChaoPiPi <1806616500@qq.com>
2018-11-21 15:14:34 +0800

    MvcDemo

--- Class/C#/MVCDemo/MvcWeb/MvcWeb/Controllers/HelloWorldController.cs
+++ Class/C#/MVCDemo/MvcWeb/MvcWeb/Controllers/HelloWorldController.cs
@@ -11,8 +11,8 @@ namespace MvcWeb.Controllers
         // GET: HelloWorld
         public ViewResult Index()
         {
-            string[] Names = { "黄武健", "韦安妮", "银浩然" };
-            string[] Powers = { "班长", "副班长", "团支书" };
+            string[] Names = { "黄武健", "韦安妮", "银浩然","覃萱娜","黄学明" };
+            string[] Powers = { "班长", "副班长", "团支书","宣传委员","心理委员" };
             ViewBag.Name = Names;
             ViewBag.Power = Powers;
             return View();
