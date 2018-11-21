6492d16901095aee08024cae1e0a9686fd618e06
Baiguia <1942152252@qq.com>
2018-11-21 16:29:12 +0800

    11-21

--- Mywork/11-19 MvcDemo/11-19 MvcDemo/Migrations/Configuration.cs
+++ Mywork/11-19 MvcDemo/11-19 MvcDemo/Migrations/Configuration.cs
@@ -37,9 +37,41 @@
                 Genre = "3D 动作 好莱坞",
                 Price = 30.00M
             };
+            var m4 = new Movie()
+            {
+                Tite = "城市猎人",
+                ReLeaseDate = DateTime.Parse("2018-2-1"),
+                Genre = "2D 动作 剧情",
+                Price = 30.00M
+            };
+            var m5 = new Movie()
+            {
+                Tite = "反贪风暴3",
+                ReLeaseDate = DateTime.Parse("2018-2-1"),
+                Genre = "3D 动作 悬疑",
+                Price = 30.00M
+            };
+            var m6 = new Movie()
+            {
+                Tite = "我不是药神",
+                ReLeaseDate = DateTime.Parse("2018-5-25"),
+                Genre = "2D 动作 情感 剧情",
+                Price = 28.00M
+            };
+            var m7 = new Movie()
+            {
+                Tite = "小偷家族",
+                ReLeaseDate = DateTime.Parse("2018-2-1"),
+                Genre = "2D 情感 剧情 犯罪",
+                Price = 30.00M
+            };
             context.Movie.Add(m1);
             context.Movie.Add(m2);
             context.Movie.Add(m3);
+            context.Movie.Add(m4);
+            context.Movie.Add(m5);
+            context.Movie.Add(m6);
+            context.Movie.Add(m7);
             context.SaveChanges();
         }
     }
