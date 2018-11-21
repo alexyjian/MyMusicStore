f82614a9c933bd235cbb18f2a4f416a140eceb06
mwailxx <1650098394@qq.com>
2018-11-21 15:51:45 +0800

    11-21下午Mvc1

---
+++ 04ASP.NET MVC/WebSite6/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSite6.Startup))]
+namespace WebSite6
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
