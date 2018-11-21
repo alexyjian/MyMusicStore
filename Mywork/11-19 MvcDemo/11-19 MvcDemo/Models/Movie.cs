7625ad095e1ccbfca2d747e4eeb6c4bb5393387d
Baiguia <1942152252@qq.com>
2018-11-21 15:27:24 +0800

    11-21

---
+++ Mywork/11-19 MvcDemo/11-19 MvcDemo/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _11_19_MvcDemo.Models
+{
+    public class Movie
+    {
+        public Guid ID { get; set; }
+        public string Tite { get; set; }
+        public DateTime ReLeaseDate { get; set;}
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+
+        public Movie()
+        {
+            ID = Guid.NewGuid();
+            ReLeaseDate = DateTime.Now;
+        }
+    }
+}
