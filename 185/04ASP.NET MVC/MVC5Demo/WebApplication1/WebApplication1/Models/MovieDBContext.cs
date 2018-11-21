7d19baacda5a675b820b74f6d36f24ad738f4441
WaxdM <1575908620@qq.com>
2018-11-21 17:05:24 +0800

    11.21.更改

---
+++ 185/04ASP.NET MVC/MVC5Demo/WebApplication1/WebApplication1/Models/MovieDBContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace WebApplication1.Models
+{
+    public class MovieDBContext:DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
