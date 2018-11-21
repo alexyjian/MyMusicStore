2097332669c1e1e18e6c0aa953fb6295eb329f27
ljiancheng <1130520078@qq.com>
2018-11-21 16:48:44 +0800

    MVC 增删查改，21号

--- 05 MVC/MvcDemo1/MvcDemo1/Controllers/HomeController.cs
+++ 05 MVC/MvcDemo1/MvcDemo1/Controllers/HomeController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcDemo1.Models;
 
 namespace MvcDemo1.Controllers
 {
     public class HomeController : Controller
     {
+        private MovieDBContext db = new MovieDBContext();
+
+        //GET:Movies
         public ActionResult Index()
         {
-            return View();
+            //强类型模型传入数据到试图
+            return View(db.Movies.ToList());
         }
 
         public ActionResult About()
