8bd71eaa53b3c997927d1c2ed29550b58ade43df
Pretty33 <321838579@qq.com>
2018-11-22 15:21:40 +0800

    MVC EF 增删查改

---
+++ WAnnie/04ASP.NET MVC/MVC5Demo/MVCDemo1/MVCDemo1/Models/MovieDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace MVCDemo1.Models
+{
+    public class MovieDBContext:DbContext
+    {
+        public DbSet<Moive> Moives { get; set; }
+        
+    }
+}
