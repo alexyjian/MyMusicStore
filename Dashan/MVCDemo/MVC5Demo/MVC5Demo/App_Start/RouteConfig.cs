39eb7c7e1c1e6846639063d730d57cf577b918ea
苏子晴 <@DESKTOP-K52UG7L>
2018-11-22 20:15:38 +0800

    MVC ef 强类型模型

---
+++ Dashan/MVCDemo/MVC5Demo/MVC5Demo/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcDemo
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)//默认路由表
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "HelloWorld", action = "Index", id = UrlParameter.Optional }//可手动更改控制
+            );
+
+            //添加一个新的路由
+            routes.MapRoute(
+                name: "Hello",
+                url: "{controller}/{action}/{name}/{id}"
+                );
+        }
+
+    }
+}
---
+++ Dashan/MVCDemo/MVC5Demo/MVC5Demo/Controllers/HelloWorldController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class HelloWorldController : Controller //控制类
+    {
+        // GET: HelloWorld
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    //return View();//默认返回View 前端
+        //    return "这是我的 <b> 默认 </b>控制器(action)";
+        //}
+        ////public string Welcome()
+        ////{
+        ////    //强调所选字体成粗体 <b> 欢迎 </b> 
+        ////    return "这是我的 <b> 欢迎 </b>控制器方法(action)";
+        ////}
+
+
+
+
+             //带参数
+        public ActionResult Welcome(string  name,int id=1)
+        {
+
+            //return "您好,"+ name + "欢迎次数："+ id;
+
+            ViewBag.ID = id;
+            ViewBag.Name = name + ",您好！";
+            Session["message"] = "你们很棒棒的！";
+            return View();
+        }
+    }
+}
