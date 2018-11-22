2072ced775000690b19f477e481002459738d8a3
Pretty33 <321838579@qq.com>
2018-11-22 15:12:34 +0800

    MVC EF 增删查改

---
+++ WAnnie/04ASP.NET MVC/MVC5Demo/MVCDemo1/MVCDemo1/Models/Moive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo1.Models
+{
+    public class Moive
+    {
+        public Guid ID { get; set; }
+        public string Titile { get; set; }
+        //上映时间
+        public DateTime ReleaseDate { get; set; }
+        //电影类别
+        public string Genre { get; set; }
+        //价格
+        public decimal Price { get; set; }
+
+        public Moive()
+        {
+            ID = Guid.NewGuid();
+            ReleaseDate = DateTime.Now;
+        }
+    }
+}
