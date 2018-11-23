97719ae89fbabd18341583035e6712bd27329ba1
2876416732 <723817914@qq.com>
2018-11-23 12:40:22 +0800

    测试

--- 03Practice/1027EF代码优先作业/1027EF代码优先作业/Properties/AssemblyInfo.cs
+++ 03Practice/1027EF代码优先作业/1027EF代码优先作业/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 //请将此类型的 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
 
-// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+// 如果0此项目向 COM 公开，则下列 GUID 用于类型库的  ID
 [assembly: Guid("fb579c1f-424f-4790-9a5d-6e1d64b0a4cd")]
 
 // 程序集的版本信息由下列四个值组成: 
