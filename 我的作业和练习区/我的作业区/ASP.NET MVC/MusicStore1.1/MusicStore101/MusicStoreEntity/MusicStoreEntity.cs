c5f354af9cce27f554b4b065215b81a26c738ea1
Areart <15778250484@163.com>
2018-11-26 16:31:49 +0800

    音乐商店

--- 我的作业和练习区/我的作业区/ASP.NET MVC/MusicStore1.1/MusicStore101/MusicStoreEntity/MusicStoreEntity.cs
+++ 我的作业和练习区/我的作业区/ASP.NET MVC/MusicStore1.1/MusicStore101/MusicStoreEntity/MusicStoreEntity.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MusicStoreEntity
 {
-    public class Class1
+    public class MusicStoreEntity:DbContext
     {
     }
 }
