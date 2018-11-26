40a1baa6b967332b6dc5ad6eeb9a2e091e5e30ce
ShroudzZ <1833549764@qq.com>
2018-11-26 16:38:41 +0800

    11.26

--- Shroud/MusicStore/MusicStore/MusicStoreEntity/MusicContext.cs
+++ Shroud/MusicStore/MusicStore/MusicStoreEntity/MusicContext.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using MusicStoreEntity.UserAndRole;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace MusicStoreEntity
 {
-    public class MusicContext:DbContext
+    //使用带用户认证权限机制的实体框架
+    public class MusicContext : IdentityDbContext<ApplicationUser>
     {
+        //调用基类的构造函数
+        public MusicContext() : base("EntityDbContext") { }
+        public static MusicContext Create()
+        {
+            return new MusicContext();
+        }
 
+        #region 用户与角色的实体
+
+        public IDbSet<ApplicationInformation> ApplicationInformations { get; set; }
+        public IDbSet<ApplicationBusinessType> ApplicationBusinessTypes { get; set; }
+        public IDbSet<ApplicaitionUserInApplication> ApplicaitionUserInApplications { get; set; }
+        public IDbSet<Person> Persons { get; set; }
+
+        #endregion
     }
 }
