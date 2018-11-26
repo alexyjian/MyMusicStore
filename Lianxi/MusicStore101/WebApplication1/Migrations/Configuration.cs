e919e7044d495d8998b686bdcd804a9e53b24d86
JIEJIEJIEJI <1105658417@qq.com>
2018-11-26 18:09:37 +0800

    音乐商店

---
+++ Lianxi/MusicStore101/WebApplication1/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace WebApplication1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MusicStorEntity.EntityDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MusicStorEntity.EntityDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
