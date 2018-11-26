7a380608d30a83df6398ee17c62862d3780e49b2
Baiguia <1942152252@qq.com>
2018-11-26 16:06:53 +0800

    音乐商店解决方案搭建11-26

---
+++ Mywork/MusicStore1-1/MusicStoreEntity/MusicContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStoreEntity
+{
+    public class MusicContext:DbContext
+    {
+    }
+}
