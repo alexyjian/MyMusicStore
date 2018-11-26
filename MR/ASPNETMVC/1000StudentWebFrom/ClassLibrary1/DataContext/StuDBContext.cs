0cd2b09e1bd0a6a8cc87dcebc43838978573d192
HappyWindManS <3066004354@qq.com>
2018-11-26 13:43:07 +0800

    MyComputer

---
+++ MR/ASPNETMVC/1000StudentWebFrom/ClassLibrary1/DataContext/StuDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using _1000Student.Entities;
+
+namespace _1000Student.DataContext
+{
+    public class StuDBContext:DbContext
+    {
+		public DbSet<DepartMent> DepartMents { get; set; }
+		public DbSet<Student> Students { get; set; }
+    }
+}
