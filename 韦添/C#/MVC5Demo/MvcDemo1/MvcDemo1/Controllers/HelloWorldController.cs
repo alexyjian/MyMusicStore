57f2d495b74b8496094de78e0f35b538dc2fe82c
weitian666 <1935836331@qq.com>
2018-11-26 18:11:03 +0800

    练习

---
+++ 韦添/C#/MVC5Demo/MvcDemo1/MvcDemo1/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "这是我的<b>默认</b>控制方法(action)";
+        }
+
+        public string Welcome(string name,int id=1)
+        {
+            return "您好，" + name + ",欢迎次数:"+id;
+        }
+    }
+}
