a187d4884450e3db56350029c9671a51e9a23418
ljiancheng <1130520078@qq.com>
2018-11-26 17:57:53 +0800

    ASPNETMVC认证机制

--- 06MusicStore1.1/MusicStore101/MusicStoreEntity/EntityDbContext.cs
+++ 06MusicStore1.1/MusicStore101/MusicStoreEntity/EntityDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using MusicStoreEntity.UserAndRole;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -7,7 +9,23 @@ using System.Threading.Tasks;
 
 namespace MusicStoreEntity
 {
-    public class EntityDbContext:DbContext
+    //使用带用户认证权限机制的实体框架
+    public class EntityDbContext : IdentityDbContext<ApplicationUser>
     {
+        //调用基类的构造函数
+        public EntityDbContext() : base("EntityDbContext") { }
+        public static EntityDbContext Create()
+        {
+            return new EntityDbContext();
+        }
+
+        #region 用户与角色的实体
+
+        public IDbSet<ApplicationInformation> ApplicationInformations { get; set; }
+        public IDbSet<ApplicationBusinessType> ApplicationBusinessTypes { get; set; }
+        public IDbSet<ApplicaitionUserInApplication> ApplicaitionUserInApplications { get; set; }
+        public IDbSet<Person> Persons { get; set; }
+
+        #endregion
     }
 }
