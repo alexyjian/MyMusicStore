6277a0ca9406fa5d700addaef3c864903e974df8
Pretty33 <321838579@qq.com>
2018-11-27 22:45:21 +0800

    2018.11.26  添加用户角色认证实体

---
+++ WAnnie/05MusiceStore1.1/MusicStore101/MusicStoreEntity/MusicContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MusicStoreEntity.UserAndRole;
+
+namespace MusicStoreEntity
+{
+  public   class MusicContext: IdentityDbContext<ApplicationUser>
+    {
+        //调用基类的构造函数
+        public MusicContext() : base("MusicContext") { }
+        public static MusicContext Create()
+        {
+            return new MusicContext();
+        }
+
+        #region 用户与角色的实体
+
+        public DbSet<ApplicationInformation> ApplicationInformations { get; set; }
+        public DbSet<ApplicationBusinessType> ApplicationBusinessTypes { get; set; }
+        public DbSet<ApplicaitionUserInApplication> ApplicaitionUserInApplications { get; set; }
+        public DbSet<Person> Persons { get; set; }
+
+        #endregion
+}
+}
