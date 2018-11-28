dd4fbf79895bd77e6166b927f578882d5865a1f4
lrz991222 <353226337@qq.com>
2018-11-28 17:30:53 +0800

    添加用户角色认证实体

---
+++ LRZ/MusicStore1.1/MusicStore101/MusicStoreEntity/UserAndRole/ApplicationUserManager.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity;
+
+namespace MusicStoreEntity.UserAndRole
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        {
+        }
+    }
+}
