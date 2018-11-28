6b8918ad4769e4a676988c7ed1ed5adf70396377
weitian666 <1935836331@qq.com>
2018-11-28 16:43:09 +0800

    建立EF

---
+++ 韦添/C#/MVC5Demo/MvcDemo1/MvcDemo1/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemo1.Models
+{
+    public class Movie
+    {
+        public Guid ID { get; set; }
+        public string Titile { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
