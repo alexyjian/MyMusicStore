145a98531921770783e446f061bb645d5f4311b1
QAQhaoran <1317898963@qq.com>
2018-11-28 15:24:08 +0800

    ...

---
+++ MvcDemol/MvcDemol/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemol.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            return "这是我的<b>默认</b>控制器方法（action）";
+        }
+        public string Welcome()
+        {
+            return "这是我的<b>默认</b>控制器方法（action）";
+        }
+    }
+}
---
+++ MvcDemol/MvcDemol/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemol.Models
+{
+    public class Movie
+    {
+        public Guid ID { get; set; }
+        public string Titile { get; set; }
+        public DateTime ReleaseDate { get; set; }  //上映时间
+        public string Gebre { get; set; }  //电影类别
+        public decimal price { get; set; }  //价格
+    }
+}
