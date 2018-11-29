fe9e5f520215b9942d0004e43dede79f05d1c4df
ljiancheng <1130520078@qq.com>
2018-11-29 11:08:54 +0800

    完成公共布局页

--- 06MusicStore1.1/MusicStore101/MusicStore/Migrations/GenreSeed.cs
+++ 06MusicStore1.1/MusicStore101/MusicStore/Migrations/GenreSeed.cs
@@ -13,16 +13,16 @@ namespace MusicStore.Migrations
         {
             var genres = new List<Genre>()
             {
-                new Genre() {Name="摇滚",Description="Rock"},
-                new Genre() {Name="爵士",Description="Jazz"},
-                new Genre() {Name="重金属",Description="Meta"},
-                new Genre() {Name="慢摇",Description="DownTempo"},
-                new Genre() {Name="蓝调",Description="Blue"},
-                new Genre() {Name="拉丁",Description="Latin"},
-                new Genre() {Name="流行",Description="Pop"},
-                new Genre() {Name="古典",Description="Classical"},
-                new Genre() {Name="DJ",Description="DJ"},
-                new Genre() {Name="嘻哈",Description="HiHop"},
+                new Genre(){Name="摇  滚",Description="Rock"     },
+                new Genre(){Name="爵  士",Description="Jazz"     },
+                new Genre(){Name="重金属",Description="Meta"     },
+                new Genre(){Name="慢  摇",Description="DownTempo"},
+                new Genre(){Name="蓝  调",Description="Blue"     },
+                new Genre(){Name="拉  丁",Description="Latin"    },
+                new Genre(){Name="流  行",Description="Pop"      },
+                new Genre(){Name="古  典",Description="Classical"},
+                new Genre(){Name="D    J",Description="DJ"       },
+                new Genre(){Name="嘻  哈",Description="HiHop"    },
             };
             foreach (var g in genres)
                 _dbContext.Genres.Add(g);
