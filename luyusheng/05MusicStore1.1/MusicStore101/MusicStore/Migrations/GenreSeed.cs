12fa88ee8b90fa05625246899ebe6556e8ad7a6a
luyusheng7 <2278814961@qq.com>
2018-11-29 12:12:30 +0800

    11-29 音乐商店

--- luyusheng/05MusicStore1.1/MusicStore101/MusicStore/Migrations/GenreSeed.cs
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStore/Migrations/GenreSeed.cs
@@ -12,23 +12,23 @@ namespace MusicStore.Migrations
         public static void Seed()
 
         {
-        //    var genres = new List<Genre>()
-        //    {
-        //    new Genre() { Name = "摇滚" },
-        //    new Genre() { Name = "爵士" },
-        //    new Genre() { Name = "伤感" },
-        //    new Genre() { Name = "慢摇" },
-        //    new Genre() { Name = "抖音" },
-        //    new Genre() { Name = "火山视频" },
-        //    new Genre() { Name = "古风" },
-        //    new Genre() { Name = "DJ" },
-        //    new Genre() { Name = "流行" },
-        //    new Genre() { Name = "青春" },
-        //};
-        //    foreach (var g in genres)
-        //        _dbContext.Genres.Add(g);
+            var genres = new List<Genre>()
+            {
+            new Genre() { Name = "摇滚" },
+            new Genre() { Name = "爵士" },
+            new Genre() { Name = "伤感" },
+            new Genre() { Name = "慢摇" },
+            new Genre() { Name = "抖音" },
+            new Genre() { Name = "火山视频" },
+            new Genre() { Name = "古风" },
+            new Genre() { Name = "DJ" },
+            new Genre() { Name = "流行" },
+            new Genre() { Name = "青春" },
+        };
+            foreach (var g in genres)
+                _dbContext.Genres.Add(g);
 
-        //    _dbContext.SaveChanges();
-       }
+            _dbContext.SaveChanges();
+        }
     }
 }
---
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStoreEntity/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStoreEntity
+{
+   public class Genre
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Genre()
+        {
+            ID = Guid.NewGuid();
+        }
+    }
+}
