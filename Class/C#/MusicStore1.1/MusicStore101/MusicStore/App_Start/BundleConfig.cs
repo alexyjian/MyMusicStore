602c72e7c4e14b29766bbecb6a0d06b8609fc93b
ChaoPiPi <1806616500@qq.com>
2018-11-29 08:16:16 +0800

    update

--- Class/C#/MusicStore1.1/MusicStore101/MusicStore/App_Start/BundleConfig.cs
+++ Class/C#/MusicStore1.1/MusicStore101/MusicStore/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace MusicStore
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/style.css"));
         }
     }
 }
