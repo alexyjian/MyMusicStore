e5062b7d75ec2c5953ebb3ab2b497cf10563249b
ShroudzZ <1833549764@qq.com>
2018-11-29 08:49:06 +0800

    11.29

--- Shroud/MusicStore/MusicStore/MusicStore/App_Start/BundleConfig.cs
+++ Shroud/MusicStore/MusicStore/MusicStore/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace MusicStore
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                       "~/Scripts/jquery-ui-{version}.min.js"));
+
             // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
             // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,6 +29,9 @@ namespace MusicStore
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/JqueryUI").Include(
+                     "~/Content/themes/base/jquery-ui.min.css"));
+            
         }
     }
 }
