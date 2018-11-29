690ea6e8db29d072c1b8a43913056c5fdf454dfa
luyusheng7 <2278814961@qq.com>
2018-11-30 01:40:18 +0800

    完成左侧导航

---
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStore/Migrations/GenreSeed.cs
@@ -0,0 +1,34 @@
+﻿using MusicStoreEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.Migrations
+{
+    public class GenreSeed
+    {
+        private static readonly MusicStoreEntity.EntityDbContext _dbContext = new MusicStoreEntity.EntityDbContext();
+        public static void Seed()
+
+        {
+            var genres = new List<Genre>()
+            {
+            new Genre() { Name = "摇滚",Description ="Rock" },
+            new Genre() { Name = "爵士" ,Description ="Jazz"},
+            new Genre() { Name = "伤感" ,Description ="ShKa"},
+            new Genre() { Name = "慢摇",Description ="DownTempo" },
+            new Genre() { Name = "抖音",Description ="Yidou" },
+            new Genre() { Name = "火山视频" ,Description ="HoShan"},
+            new Genre() { Name = "古风" ,Description ="Classical"},
+            new Genre() { Name = "DJ" ,Description ="DJ"},
+            new Genre() { Name = "流行" ,Description ="Pop"},
+            new Genre() { Name = "青春" ,Description ="HiHop"},
+        };
+            foreach (var g in genres)
+                _dbContext.Genres.Add(g);
+
+            _dbContext.SaveChanges();
+        }
+    }
+}
