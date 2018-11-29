65b102312c4863dffab1eefd44f6011e25975420
1123781921 <1123781921@qq.com>
2018-11-29 12:05:44 +0800

    11.29

--- lql139/ASP.NET mvc/MusicStore1.1/MusicStore101/MusicStore/Migrations/Configuration.cs
+++ lql139/ASP.NET mvc/MusicStore1.1/MusicStore101/MusicStore/Migrations/Configuration.cs
@@ -16,7 +16,7 @@ namespace MusicStore.Migrations
         {
             //UserAndRoleSeed.AddRoles();
             //UserAndRoleSeed.AddUsers();
-            GenreSeed.Seed();
+            //GenreSeed.Seed();
         }
     }
 }
