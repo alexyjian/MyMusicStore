de28b657402fe21ad654905bb2b2a752075271b6
ShroudzZ <1833549764@qq.com>
2018-11-29 09:33:20 +0800

    11.29

--- Shroud/MusicStore/MusicStore/MusicStore/App_Start/BundleConfig.cs
+++ Shroud/MusicStore/MusicStore/MusicStore/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace MusicStore
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
