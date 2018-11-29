156257d9b70a978663f848b28ac979e471f86668
WaxdM <1575908620@qq.com>
2018-11-29 11:05:22 +0800

    尾栏完成

--- 185/05Musi/MusicStore101/MusicStore/Migrations/Configuration.cs
+++ 185/05Musi/MusicStore101/MusicStore/Migrations/Configuration.cs
@@ -14,8 +14,9 @@ namespace MusicStore.Migrations
 
         protected override void Seed(MusicStoreEntity.EntityDbContext context)
         {
-            UserAndRoleSeed.AddRoles();
-            UserAndRoleSeed.AddUsers();
+            //UserAndRoleSeed.AddRoles();
+            //UserAndRoleSeed.AddUsers();
+            GenreSeed.Seed();
         }
     }
 }
