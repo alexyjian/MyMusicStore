e52e3c060d74dd8e5acae2912d1e8bc69787c665
WaxdM <1575908620@qq.com>
2018-11-29 08:39:28 +0800

    11.29音乐商店更新

--- 185/05Musi/MusicStore101/MusicStore/App_Start/BundleConfig.cs
+++ 185/05Musi/MusicStore101/MusicStore/App_Start/BundleConfig.cs
@@ -26,6 +26,11 @@ namespace MusicStore
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //绑定JqueryUI的样式库
+            bundles.Add(new StyleBundle("~/Content/JqueryUI").Include("~/Content/themes/base/jquery-ui.css"));
+            //绑定JqueryUI 的脚本库
+            bundles.Add(new ScriptBundle("~/bundles/JqueryUI").Include("~/Content/themes/base/jquery-ui-{version}.js"));
         }
     }
 }
