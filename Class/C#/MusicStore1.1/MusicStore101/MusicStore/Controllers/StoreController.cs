e35313240efaef47a4d65ede92fbfffa8c3163a6
ChaoPiPi <1806616500@qq.com>
2018-11-30 12:03:50 +0800

    专辑明细

---
+++ Class/C#/MusicStore1.1/MusicStore101/MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,20 @@
+﻿using MusicStoreEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        private static readonly EntityDbContext _dbContext = new EntityDbContext();
+        // GET: Store
+        public ActionResult Detail(Guid ID)
+        {
+            var detail = _dbContext.Albums.Find(ID);
+            return View(detail);
+        }
+    }
+}
