d31783fed5e2cb29a455d6c9e21758fa864c3105
Areart <15778250484@163.com>
2018-11-30 11:52:53 +0800

    2018-11-30

--- 我的作业和练习区/我的作业区/ASP.NET MVC/MusicStore1.1/MusicStore101/Music/Controllers/HomeController.cs
+++ 我的作业和练习区/我的作业区/ASP.NET MVC/MusicStore1.1/MusicStore101/Music/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using MusicStoreEntity;
 using MusicStoreEntity.UserAndRole;
 using System;
 using System.Collections.Generic;
@@ -13,22 +14,11 @@ namespace Music.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var context = new EntityDbContext();
+            var list = context.Albums.OrderByDescending(x => x.PublisherDate).Take(20).ToList();
+            return View(list);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
         public string TestLogin(string username="messi",string pwd = "123.abc")
         {
             var userManage = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new MusicStoreEntity.EntityDbContext()));
