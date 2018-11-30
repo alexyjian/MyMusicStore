289a41c7c0f7c1144e6a3021a0ecca9fb42903f5
weitian666 <1935836331@qq.com>
2018-11-30 12:00:48 +0800

    音乐商店

--- 韦添/C#/05MusicStore1.1/MusicStore101/MusicStore/Controllers/HomeController.cs
+++ 韦添/C#/05MusicStore1.1/MusicStore101/MusicStore/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace MusicStore.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page."; 
 
             return View();
         }
