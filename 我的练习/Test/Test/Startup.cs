bd1622a989567c9b8b5344dccc9b021ea1f61812
Administrator <Administrator@ts70842>
2018-11-30 08:53:53 +0800

    Test

---
+++ 我的练习/Test/Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test.Startup))]
+namespace Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
