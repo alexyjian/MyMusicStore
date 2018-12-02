f05fc47f1a569e1489c26b5991e4df43666502a6
WaxdM <1575908620@qq.com>
2018-12-02 17:35:33 +0800

    12/2登陆操作完成

---
+++ 185/05Musi/MusicStore101/MusicStore/Controllers/AccountController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MusicStore.ViewModels;
+using MusicStoreEntities.UserAndRole;
+using MusicStoreEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 登陆方法
+        /// </summary>
+        /// <param name="returnUrl">登陆成功后跳转地址</param>
+        /// <returns></returns>
+
+        public ActionResult Login(string returnUrl=null )
+        {
+            if (string.IsNullOrEmpty(returnUrl))
+                ViewBag.ReturnUrl = Url.Action("index", "home");
+            else
+                ViewBag.ReturnUrl = returnUrl;
+
+            return View();
+        }
+
+        [HttpPost]//此Action用来接收用户提交
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var loginStatus = new LoginUserStatus()
+                {
+                    IsLogin = false,
+                    Message = "用户或密码错误"
+                };
+                var userManage =
+                    new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new EntityDbContext()));
+                var user = userManage.Find(model.UserName, model. PassWord);
+                if (user != null)
+                {
+                    var roleName = "";
+                    var context = new EntityDbContext();
+                    foreach (var role in user.Roles)
+                    {
+                        roleName += (context.Roles.Find(role.RoleId) as ApplicationRole).DisplayName + "";
+
+                    }
+
+                    loginStatus.IsLogin = true;
+                    loginStatus.Message = "登陆成功！用户的角色：" + roleName;
+                    loginStatus.GotoController = "home";
+                    loginStatus.GotoAction = "index";
+
+                    Session["loginStatus"] = loginStatus;
+
+                    var loginUserSessionModel = new LoginUserSessionModel()
+                    {
+                        User = user,
+                        Person = user.Person,
+                        RoleName = roleName,
+                    };
+
+                    Session["LoginUserSessionModel"] = loginUserSessionModel;
+
+                    var identity = userManage.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(returnUrl))
+                        ViewBag.ReturnUrl = Url.Action("index", "home");
+                    else
+                        ViewBag.ReturnUrl = returnUrl;
+                    ViewBag.LoginUserStatus = loginStatus;
+                    return View();
+                }
+            }
+            if (string.IsNullOrEmpty(returnUrl))
+                ViewBag.ReturnUrl = Url.Action("index", "home");
+            else
+                ViewBag.RetumUrl = returnUrl;
+            return View();
+        }
+
+       
+    }
+}
---
+++ 185/05Musi/MusicStore101/MusicStore/ViewModels/LoginUserStatus.cs
@@ -0,0 +1,31 @@
+﻿using MusicStoreEntities.UserAndRole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.ViewModels
+{
+    public class LoginUserStatus
+    {
+        //登陆成否成功
+        public bool IsLogin { get; set; }
+        //登陆成功失败的信息
+        public string Message { get; set;}
+        //登陆成功后跳转的控制器
+        public string GotoController { get; set;}
+        //登陆成功后跳转的方法
+        public string GotoAction { get; set; }
+
+    }
+    /// <summary>
+    /// 用户处理登陆成功后的用户会话
+    /// </summary>
+    public class LoginUserSessionModel
+    {
+        public ApplicationUser User { get; set; }//账户对象
+        public Person Person { get; set; }//个人信息的对象
+        public string RoleName { get; set; }
+        //此外还可以定义其他业务对象 购物车 订单.....
+    }
+}
