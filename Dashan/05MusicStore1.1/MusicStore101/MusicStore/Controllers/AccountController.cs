105921b6710b061f7e33d6a03cbc19b09b7a1cf8
suweixi <1985748697@qq.com>
2018-12-02 18:04:36 +0800

    用户登录

--- Dashan/05MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
+++ Dashan/05MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using MusicStore.ViewMoldels;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MusicStoreEntity;
+using MusicStoreEntity.UserAndRole;
 
 namespace MusicStore.Controllers
 {
@@ -21,8 +26,73 @@ namespace MusicStore.Controllers
 
         public ActionResult Login(string returnUrl = null)
         {
+            //没有地址就跳转回首页
+            if (string.IsNullOrEmpty(returnUrl))
+                ViewBag.ReturnUrl = Url.Action("index", "home");
+            else
+                ViewBag.ReturnUrl = returnUrl;
             return View();
+        }
+        [HttpPost] //此Action用来接收用户提交
+        public ActionResult Login (LoginViewModel model,string returnUrl)
+        {
+            //判断实体是否校验通过
+            if(ModelState.IsValid)
+            {
+                var loginStatus = new LoginUserStatus()
+                {
+                    IsLogin = false,
+                    Message = "用户或密码错误",
+                };
+                //登录处理
+                var userManage = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new EntityDbContext()));
+                var user = userManage.Find(model.UserName, model.PassWord);
+                if(user != null)
+                {
+                    var roleName = "";
+                    var context = new EntityDbContext();
+                    foreach (var role in user.Roles)
+                    {
+                        roleName += (context.Roles.Find(role.RoleId) as ApplicationRole).DisplayName + ",";
+                    }
 
+                    loginStatus.IsLogin = true;
+                    loginStatus.Message = "登录成功！用户的角色：" + roleName;
+                    loginStatus.GotoController = "home";
+                    loginStatus.GotoAction = "index";
+                    //登录状态保存到回话
+                    Session["loginStatus"] = loginStatus;
+                    var loginUserSessionModel = new LoginUserSessionModel()
+                    {
+                        User = user,
+                        Person = user.Person,
+                        RoleName = roleName,
+                    };
+                    //登录成功后用户信息保存到回话
+                    Session["LoginUserSessionModel"] = loginUserSessionModel;
+
+                    //identity 登录处理，登录令牌Tkken
+                    var identity = userManage.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    return Redirect(returnUrl);
+
+
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(returnUrl))
+                        ViewBag.ReturnUrl = Url.Action("index", "home");
+                    else
+                        ViewBag.ReturnUrl = returnUrl;
+                    ViewBag.LoginUserStatus = loginStatus;
+                    return View();
+                }
+            }
+            if (string.IsNullOrEmpty(returnUrl))
+                ViewBag.ReturnUrl = Url.Action("index", "home");
+            else
+                ViewBag.ReturnUrl = returnUrl;
+            return View();
         }
     }
+
 }
---
+++ Dashan/05MusicStore1.1/MusicStore101/MusicStore/ViewMoldels/LoginUserStatus.cs
@@ -0,0 +1,25 @@
+﻿using MusicStoreEntity.UserAndRole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.ViewMoldels
+{
+    /// <summary>
+    /// 用户登录的状态
+    /// </summary>
+    public class LoginUserStatus
+    {
+        public bool IsLogin { get; set; }//登录是否成功
+        public string Message { get; set; }//登录成功或失败的信息
+        public string GotoController { get; set; }
+        public string GotoAction { get; set; }
+    }
+    public class LoginUserSessionModel
+    {
+        public ApplicationUser User { get; set; }
+        public Person Person { get; set; }
+        public string RoleName { get; set; }
+    }
+}
