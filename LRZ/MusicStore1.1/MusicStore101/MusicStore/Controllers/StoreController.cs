c35fcfa0798ab5ef427272755afcb795ece9ac93
lrz991222 <353226337@qq.com>
2018-12-02 18:36:49 +0800

    1

---
+++ LRZ/MusicStore1.1/MusicStore101/MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,35 @@
+﻿using MusicStoreEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        private static readonly EntityDbContext _context = new EntityDbContext();
+        /// <summary>
+        /// 显示专辑的明细
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Detail(Guid id)
+        {
+            var detail = _context.Albums.Find(id);
+            return View(detail);
+        }
+
+        /// <summary>
+        /// 按分类显示专辑
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            var genres = _context.Genres.OrderBy(x => x.Name).ToList();
+            return View(genres);
+        }
+    }
+}
