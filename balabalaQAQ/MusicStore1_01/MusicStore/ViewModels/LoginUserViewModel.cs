15e56fc788d56ec3978ee973b399bc60238c808d
balabalaQAQ <1124048070@qq.com>
2018-12-02 16:44:03 +0800

    new

---
+++ balabalaQAQ/MusicStore1_01/MusicStore/ViewModels/LoginUserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using MusicStoreEntity.UserAndRole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.ViewModels
+{
+    /// <summary>
+    /// 用户登录时的状态
+    /// </summary>
+    public class LoginUserViewModel
+    {
+        //是否登录成功
+        public bool IsLogin { get; set; }
+        //登录成功或失败的信息
+        public string Message { get; set; }
+        //登录成功后跳转的控制器
+        public string GotoContrller { get; set; }
+        //登录成功后跳转的方法
+        public string GotoAction { get; set; }
+    }
+    public class LoginUserSessionModel
+    {
+        public ApplicationUser User { get; set; }
+        public Person Person{ get; set; }
+        public string RoleName { get; set; }
+    }
+
+}
