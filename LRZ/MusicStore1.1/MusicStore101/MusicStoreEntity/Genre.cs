b203ad66b9543df4e2edd7ddc9789e284257a322
lrz991222 <353226337@qq.com>
2018-12-02 12:01:38 +0800

    fl

---
+++ LRZ/MusicStore1.1/MusicStore101/MusicStoreEntity/Genre.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStoreEntity
+{
+    /// <summary>
+    /// 音乐分类
+    /// </summary>
+    public class Genre
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Genre()
+        {
+            ID = Guid.NewGuid();
+        }
+    }
+}
