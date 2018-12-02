bac4cff6c51dd554f3c98bc7e26cad99bf94a754
ljiancheng <1130520078@qq.com>
2018-12-02 17:43:32 +0800

    修改登录操作的一些bug

---
+++ 06MusicStore1.1/MusicStore101/MusicStore/Controllers/LoginUserSessionModel.cs
@@ -0,0 +1,15 @@
+﻿using MusicStoreEntity.UserAndRole;
+
+namespace MusicStore.Controllers
+{
+    internal class LoginUserSessionModel
+    {
+        public LoginUserSessionModel()
+        {
+        }
+
+        public Person Person { get; set; }
+        public string RoleName { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
