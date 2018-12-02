46d5dd107aa0af714b9b1229882919e1315fe271
Seezhonghuai <1875680955@qq.com>
2018-12-02 17:46:27 +0800

    12-2Login

---
+++ zhonghuai/MusicStore1.1/MusicStore101/MusicStore101/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicStore101.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="用户不能为空")]//此字段是必填
+        [Display(Name ="用户名")]//定义字段的呈现名称
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]//前端按此数据类型显示
+        [Required(ErrorMessage ="密码不能为空")]//此字段是必填
+        [Display(Name ="密码")]//定义字段的呈现名称
+        public string PassWord { get; set; }
+    }
+}
