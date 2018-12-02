79a2160ad1df925a65634713987bfa13aa9b46b6
JIEJIEJIEJI <1105658417@qq.com>
2018-12-02 09:14:01 +0800

    .

--- Lianxi/MusicStore101/WebApplication1/Controllers/StorController.cs
+++ Lianxi/MusicStore101/WebApplication1/Controllers/StorController.cs
@@ -7,14 +7,25 @@ using System.Web.Mvc;
 namespace WebApplication1.Controllers
 {
     public class StorController : Controller
-    {/// <summary>
-    /// 显示专辑明细
-    /// </summary>
-    /// <returns></returns>
+    {
+        private static readonly MusicStorEntity.EntityDbContext _context = new MusicStorEntity.EntityDbContext();
+
+        /// <summary>
+        /// 显示专辑明细
+        /// </summary>
+        /// <returns></returns>
         // GET: Stor
-        public ActionResult Index()
+        public ActionResult Detail(Guid id)
         {
+            var detail = _context.Albums.Find(id);
             return View();
+        }
+        public ActionResult Browser(Guid id)
+        {
+            var list = _context.Albums.Where(x => x.Genre.ID == id)
+                .OrderByDescending(x => x.PublisherDate).ToList();
+            return View(list);
+
         }
     }
 }
