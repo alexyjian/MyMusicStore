7b9c02ceb6c6081ecb608bd9fbf64d52a781f626
HappyWindManS <3066004354@qq.com>
2018-12-02 17:11:09 +0800

    708Study;12/2,17:11

--- MR/ASPNETMVC/MusicStore01/MusicStore/Controllers/AccountController.cs
+++ MR/ASPNETMVC/MusicStore01/MusicStore/Controllers/AccountController.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using MusicStore.ViewModels;
+using MusicStoreEntity;
+using MusicStoreEntity.UserAndRole;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UserAndRole;
 
 namespace MusicStore.Controllers
 {
@@ -21,6 +27,63 @@ namespace MusicStore.Controllers
         /// <returns></returns>
         public ActionResult Login(string returnUrl=null)
         {
+            if(string.IsNullOrEmpty(returnUrl))
+            {
+                ViewBag.ReturnUrl = Url.Action("index", "home");
+            }
+            else
+            {
+                ViewBag.ReturnUrl = returnUrl;
+            }
+            return View();
+        }
+
+
+        [HttpPost] //此Action用来接收用户提交
+        public ActionResult Login(LoginViewModel model,string returnUrl)
+        {
+            //判断实体是否校验通过
+            if(ModelState.IsValid)
+            {
+                var loginStatus = new LoginUserStatus()
+                {
+                    IsLogin = false,
+                    Message = "用户或密码输入错误",
+                };
+                //登录处理
+                var userManage =
+                    new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new EntityDbContext()));
+                var user = userManage.Find(model.UserName, model.PassWord);
+                if(user!=null)
+                {
+                    var roleName = "";
+                    var context = new EntityDbContext();
+                    foreach(var role in user.Roles)
+                    {
+                        roleName += (context.Roles.Find(role.RoleId) as ApplicationRole).DisplayName + ",";
+                    }
+
+                    loginStatus.IsLogin = true;
+                    loginStatus.Message = "登陆成功！用户的角色：" + roleName;
+                    loginStatus.GotoController = "home";
+                    loginStatus.GotoAction = "index";
+                    //把登录状态保存到会话
+                    Session["logonStatus"] = loginStatus;
+
+                    var loginUserSessionModel = new LoginUserSessionModel()
+                    {
+                        User = user,
+                        Person = user.Person,
+                        RoleName = roleName,
+                    };
+                    //把登录成功后用户信息保存到会话
+                    Session["LosinUsrSessionModel"] = loginUserSessionModel;
+
+                    //identity登录处理,创造aspnet的登录令牌Token
+                    var identity = userManage.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    return Redirect(returnUrl);
+                }
+            }
             return View();
         }
     }
---
+++ MR/ASPNETMVC/MusicStore01/MusicStore/ViewModels/LoginUserStatus.cs
@@ -0,0 +1,37 @@
+﻿using MusicStoreEntity.UserAndRole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UserAndRole;
+
+namespace MusicStore.ViewModels
+{
+    /// <summary>
+    /// 用户登录时的状态
+    /// </summary>
+    public class LoginUserStatus
+    {
+        //登录是否成功
+        public bool IsLogin { get; set; }
+        //登录成功或失败的信息
+        public string Message { get; set; }
+        //登陆成功后跳转的控制器
+        public string GotoController { get; set; }
+        //登陆成功后跳转的信息
+        public string GotoAction { get; set; }
+    }
+
+
+    /// <summary>
+    /// 用于处理登录成功后的用户会话
+    /// </summary>
+    public class LoginUserSessionModel
+    {
+        public ApplicationUser User { get; set; }//账户对象
+        public Person Person { get; set; }//个人信息的对象
+        public string RoleName { get; set; }//用户角色
+
+        //此外还可以定义其他业务对象：购物车、订单......
+    }
+}
