b500c60d0a20b4758f0279c5cc16e05b2ab206e6
ljiancheng <1130520078@qq.com>
2018-12-02 17:58:26 +0800

    修改登录操作的一些bug

--- 06MusicStore1.1/MusicStore101/MusicStore/ViewModels/LoginViewModel.cs
+++ 06MusicStore1.1/MusicStore101/MusicStore/ViewModels/LoginViewModel.cs
@@ -16,8 +16,8 @@ namespace MusicStore.ViewModels
         public string UserName { get; set; }
 
         [DataType(DataType.Password)]//前端按此数据类型显示
-        [Required(ErrorMessage = "用户名不能为空")]//此字段是必填
-        [Display(Name = "用户名")]//定义字段的呈现名称
+        [Required(ErrorMessage = "密码不能为空")]//此字段是必填
+        [Display(Name = "密码")]//定义字段的呈现名称
         public string PassWord { get; set; }
     }
 }
