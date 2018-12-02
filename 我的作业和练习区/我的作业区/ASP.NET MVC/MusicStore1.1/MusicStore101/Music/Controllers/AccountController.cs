a31c1f1f18c20d242303b2a7d65073793508d526
Areart <15778250484@163.com>
2018-12-02 17:05:58 +0800

    2018-12-2

---
+++ 我的作业和练习区/我的作业区/ASP.NET MVC/MusicStore1.1/MusicStore101/Music/Controllers/AccountController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Music.ViewModels;
+using MusicStoreEntity;
+using MusicStoreEntity.UserAndRole;
+
+namespace Music.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+        /// <summary>
+        ///登陆方法
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login(string retunUrl=null)
+        {
+            if (string.IsNullOrEmpty(retunUrl))
+            {
+                ViewBag.ReturnUrl = Url.Action("index", "Home");
+            }
+            else
+            {
+                ViewBag.ReturnUrl = retunUrl;
+            }
+            return View();
+        }
+        [HttpPost]//此Action用来接收用户提交
+        public ActionResult Login(LoginViewModel model,string returnUrl)
+        {
+            //判断实体是否校验通过
+            if (ModelState.IsValid)
+            {
+                var loginStatus = new LoginUserStatus()
+                {
+                    IsLogin = false,
+                    Message = "用户或密码错误"
+                };
+                //登陆处理
+                var userManage =
+                    new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new EntityDbContext()));
+                var user = userManage.Find(model.Username, model.PassWord);
+                if (user != null)
+                {
+                    var roleName = "";
+                    var context=new EntityDbContext();
+                    foreach (var role in user.Roles)
+                    {
+                        roleName += (context.Roles.Find(role.RoleId) as ApplicationRole).DisplayName + ",";
+                    }
+
+                    loginStatus.IsLogin = true;
+                    loginStatus.Message = "登陆成功!用户的角色:" + roleName;
+                    loginStatus.GotoController = "home";
+                    loginStatus.GotoAction = "index";
+                    //把登陆状态保存到会话
+                    Session["loginStatus"] = loginStatus;
+                    var loginUserSessionModel=new LoginUserSessionModel()
+                    {
+                        User = user,
+                        Person = user.Person,
+                        Rolename=roleName
+                    };
+                    //把登陆成功后用户信息保存到会话
+                    Session["loginUserSessionModel"] = loginUserSessionModel;
+                    var identity = userManage.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    return Redirect(returnUrl);
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ 我的作业和练习区/我的作业区/ASP.NET MVC/MusicStore1.1/MusicStore101/Music/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Music.ViewModels
+{
+    /// <summary>
+    /// 登陆的视图模型，用于用户输入的验证以及视图的呈现
+    /// </summary>
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "用户名不能为空")]//此字段是必填的
+        [Display(Name = "用户名")]//定义字段的呈现名称
+        public string Username { get; set; }
+        [DataType(DataType.Password)]//前段按此数据类型显示
+        [Required(ErrorMessage = "用户名不能为空")]//此字段是必填的
+        [Display(Name = "密码")]//定义字段的呈现名称
+        public string PassWord { get; set; }
+    }
+}
