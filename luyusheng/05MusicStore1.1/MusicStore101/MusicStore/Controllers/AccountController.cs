3ae246d8cb6f78ab77dc902006878bca05f57489
luyusheng7 <2278814961@qq.com>
2018-12-03 01:06:17 +0800

    登录的视图

---
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 登录方法
+        /// </summary>
+        /// <param name="returnUrl">登录成功后跳转地址</param>
+        /// <returns></returns>
+        public ActionResult Login(string returnUrl=null)
+        {
+            return View();
+        }
+    }
+}
---
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStore/ViewModels/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicStore.ViewModels
+{
+    /// <summary>
+    /// 登录的视图模型，用于用户输入的验证以及视图的呈现
+    /// </summary>
+    public class LoginViewModel
+    {
+        //此字段是必填
+        [Required(ErrorMessage ="用户名不能为空")]
+        //定义字段的呈现名称
+        [Display(Name ="用户名")]
+        public string UserName { get; set; }
+
+        //前端按此数据类型显示
+        [DataType(DataType.Password)]
+        //此字段是必填
+        [Required(ErrorMessage ="密码不能为空")]
+        //定义字段的呈现名称
+        [Display(Name ="密码")]
+        public string PassWord { get; set; }
+    }
+}
