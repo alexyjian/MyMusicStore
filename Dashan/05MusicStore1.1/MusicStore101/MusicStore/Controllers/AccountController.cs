d69079f1411f5db9fe179762bac6d08affd800fb
suweixi <1985748697@qq.com>
2018-12-02 15:54:54 +0800

    密码和用户名登录

---
+++ Dashan/05MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 登录页面
+        /// </summary>
+        /// <param name="returnUrl">登录成功后跳转地址</param>
+        /// <returns></returns>
+
+        public ActionResult Login(string returnUrl = null)
+        {
+            return View();
+
+        }
+    }
+}
---
+++ Dashan/05MusicStore1.1/MusicStore101/MusicStore/ViewMoldels/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.ViewMoldels
+{
+
+    /// <summary>
+    /// 登录的视图模型，用于用户输入的验证及视图的呈现
+    /// </summary>
+    public class LoginViewModel
+    {
+        //特性 Required  此字段必填
+        [Required(ErrorMessage ="用户名不能为空")]
+        [Display(Name ="用户名")]//定义字段呈现名称
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]//前端按此数据类型显示
+        [Required(ErrorMessage ="密码不能为空")]
+        [Display(Name ="密码")]//定义字段呈现名称
+        public string PassWord { get; set; }
+
+    }
+}
