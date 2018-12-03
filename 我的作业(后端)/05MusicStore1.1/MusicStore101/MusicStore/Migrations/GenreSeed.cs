f68ce28e33ac94c4e07ee92944bcdf47d83dfb31
Administrator <Administrator@ts70842>
2018-12-03 15:01:13 +0800

    首页

--- 我的作业(后端)/05MusicStore1.1/MusicStore101/MusicStore/Migrations/GenreSeed.cs
+++ 我的作业(后端)/05MusicStore1.1/MusicStore101/MusicStore/Migrations/GenreSeed.cs
@@ -25,7 +25,7 @@ namespace MusicStore.Migrations
                 new Genre() {Name="拉丁",Description="Latin" },
                 new Genre() {Name="流行",Description="Pop" },
                 new Genre() {Name="古典",Description="Classical" },
-                new Genre() {Name="DJ" ,Description="DJ"},
+                new Genre() {Name="t土嗨" ,Description="DJ"},
                 new Genre() {Name="嘻哈",Description="Hip-Hop" },
             };
             foreach (var g in genres)
