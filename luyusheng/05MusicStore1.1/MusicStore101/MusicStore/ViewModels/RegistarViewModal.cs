250faef2e410baee52c505757f5ff00e4a857ac8
luyusheng7 <2278814961@qq.com>
2018-12-05 18:14:48 +0800

    注册视图模型定义

--- luyusheng/05MusicStore1.1/MusicStore101/MusicStore/ViewModels/RegistarViewModal.cs
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStore/ViewModels/RegistarViewModal.cs
@@ -26,11 +26,11 @@ namespace MusicStore.ViewModels
 
         [Required(ErrorMessage = "密码不能为空")]
         [Display(Name = "密码")]
-        [DataType(DataType.EmailAddress)]
+        [DataType(DataType.Password)]
         [StringLength(20,ErrorMessage ="{0}长度不能小于{2}位",MinimumLength =6)]
         public string PassWord { get; set; }
 
-        [Required(ErrorMessage = "确认密码")]
+        [Display(Name = "确认密码")]
         [DataType(DataType.Password)]
         [Compare("PassWord",ErrorMessage ="密码两次输入要一致")]
         public string ConfirmPassWord { get; set; }
