f0733b0f35e2c0cf716e8b08de67e41bb76c8d13
luyusheng7 <2278814961@qq.com>
2018-12-06 08:53:46 +0800

    完成注册页

--- luyusheng/05MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
@@ -26,6 +26,43 @@ namespace MusicStore.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Register(RegistarViewModal model)
         {
+            if (ModelState.IsValid)
+            {
+                var person = new Person()
+                {
+                    FirstName = model.FullName.Substring(0, 1),
+                    LastName = model.FullName.Substring(1, model.FullName.Length - 1),
+                    Name = model.FullName,
+                    CredentialsCode = "",
+                    Birthday = DateTime.Now,
+                    Sex = true,
+                    MobileNumber = "15577623214",
+                    Email = model.Email,
+                    TelephoneNumber = "15577623214",
+                    Description = "",
+                    CreateDateTime = DateTime.Now,
+                    UpdateTime = DateTime.Now,
+                    InquiryPassword = "未设置",
+                };
+                var user = new ApplicationUser()
+                {
+                    UserName = model.UserName,
+                    FirstName = model.FullName.Substring(0, 1),
+                    LastName =model.FullName.Substring(1,model.FullName.Length -1),
+                    ChineseFullName =model.FullName,
+                    MobileNumber = "15577623214",
+                    Email =model.Email,
+                    Person =person,
+                };
+
+                //是否要验证Emil
+
+                var idManager = new IdentityManager();
+                idManager.CreateUser(user, model.PassWord);
+                idManager.AddUserToRole(user.Id, "RegisterUser");
+
+                return Content("<script>alert('恭喜注册成功!');location.herf='" + Url.Action("login", "Account") + "'</script>");
+            }
             //用户的保存 Person ApplicationUser
             return View();
         }
