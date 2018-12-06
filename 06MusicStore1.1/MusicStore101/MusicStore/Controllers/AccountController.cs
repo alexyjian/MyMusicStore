441c5ad3ed223cd0e8bdb3af02d5f8ecee72f60c
ljiancheng <1130520078@qq.com>
2018-12-06 09:12:52 +0800

    完成注册页和注销

--- 06MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
+++ 06MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
@@ -23,8 +23,45 @@ namespace MusicStore.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Register(RegisterViewModel mode)
+        public ActionResult Register(RegisterViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                var person=new Person()
+                {
+                    FirstName = model.FullName.Substring(0, 1),
+                    LastName = model.FullName.Substring(1, model.FullName.Length - 1),
+                    Name = model.FullName,
+                    CredentialsCode = "",
+                    Birthday = DateTime.Now,
+                    Sex = true,
+                    MobileNumber = "18866668888",
+                    Email = model.Email,
+                    TelephoneNumber = "18866668888",
+                    Description = "",
+                    CreateDateTime = DateTime.Now,
+                    UpdateTime = DateTime.Now,
+                    InquiryPassword = "未设置",
+                };
+                var user = new ApplicationUser()
+                {
+                    UserName = model.UserName,
+                    FirstName = model.FullName.Substring(0, 1),
+                    LastName = model.FullName.Substring(1, model.FullName.Length - 1),
+                    ChineseFullName = model.FullName,
+                    MobileNumber = "18866668888",
+                    Email = model.Email,
+                    Person = person,
+                };
+
+                //是否要验证Email
+
+                var idManager = new IdentityManager();
+                idManager.CreateUser(user, model.PassWord);
+                idManager.AddUserToRole(user.Id, "RegisterUser");
+
+                return Content("<script>alert('恭喜注册成功!');location.href='" + Url.Action("login", "Account") + "'</script>");
+            }
             //用户的保存 person ApplicationUser
             return View();
         }
@@ -103,6 +140,13 @@ namespace MusicStore.Controllers
             else
                 ViewBag.ReturnUrl = returnUrl;
             return View();
+        }
+
+        public ActionResult LoginOut()
+        {
+            Session.Remove("loginStatus");
+            Session.Remove("LoginUserSessionModel");
+            return RedirectToAction("index", "Home");
         }
     }
 }
