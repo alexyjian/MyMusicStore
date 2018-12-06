ae6461d752611b1a02ff274be6b092c50669711b
balabalaQAQ <1124048070@qq.com>
2018-12-06 18:15:32 +0800

    日常整理

--- balabalaQAQ/MusicStore1_01/MusicStore/Controllers/AccountController.cs
+++ balabalaQAQ/MusicStore1_01/MusicStore/Controllers/AccountController.cs
@@ -22,34 +22,42 @@ namespace MusicStore.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Register(RegisterViewModel model)
         {
-            var idManager = new IdentityManager();
-            var person = new Person()
+            if (ModelState.IsValid)
             {
-                FirstName = model.UserName,
-                LastName = model.UserName,
-                Name = model.UserName,
-                CredentialsCode = "452222198210090011",
-                Birthday = DateTime.Now,
-                Sex = false,
-                MobileNumber = "13899998888",
-                Email =model.Email,
-                CreateDateTime = DateTime.Now,
-                TelephoneNumber = "80861688",
-                Description = "用户",
-                UpdateTime = DateTime.Now,
-                InquiryPassword = model.PassWord,
-            };
-            var newUser = new ApplicationUser()
-            {
-                FirstName = model.FullName,
-                UserName = model.UserName,
-                ChineseFullName = model.UserName,
-                Email = model.Email,
-                Person = person,
-            };
-            idManager.CreateUser(newUser, model.PassWord);
-            idManager.AddUserToRole(newUser.Id, "RegisterUser");
-            return View();
+                var idManager = new IdentityManager();
+                var person = new Person()
+                {
+                    FirstName = model.FullName.Substring(0, 1),
+                    LastName = model.FullName.Substring(1, model.FullName.Length - 1),
+                    Name = model.FullName,
+                    CredentialsCode = "",
+                    Birthday = DateTime.Now,
+                    Sex = false,
+                    MobileNumber = "13899998888",
+                    Email = model.Email,
+                    CreateDateTime = DateTime.Now,
+                    TelephoneNumber = "80861688",
+                    Description = "用户",
+                    UpdateTime = DateTime.Now,
+                    InquiryPassword = "未设置",
+                };
+                var newUser = new ApplicationUser()
+                {
+                    FirstName = model.FullName.Substring(0, 1),
+                    LastName = model.FullName.Substring(1, model.FullName.Length - 1),
+                    UserName = model.UserName,
+                    ChineseFullName = model.FullName,
+                    MobileNumber = "13899998888",
+                    Email = model.Email,
+                    Person = person,
+                };
+                //是否要验证Email
+
+                idManager.CreateUser(newUser, model.PassWord);
+                idManager.AddUserToRole(newUser.Id, "RegisterUser");
+                return Content("<script>alert('恭喜注册成功！');location.href='" + Url.Action("Account", "login") + "'+<script/>");
+            }
+            else { return View(); }
         }
 
 
@@ -72,6 +80,7 @@ namespace MusicStore.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Login(LoginViewModel model, string returnUrl)
         {
+
             //判断实体是否校验通过
             if (ModelState.IsValid)
             {
@@ -84,8 +93,10 @@ namespace MusicStore.Controllers
                 var userManage =
                     new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new EntityDbContext()));
                 var user = userManage.Find(model.UserName, model.PassWord);
+
                 if (user != null)
                 {
+
                     var roleName = "";
                     var context = new EntityDbContext();
                     foreach (var role in user.Roles)
@@ -113,8 +124,88 @@ namespace MusicStore.Controllers
                     var identity = userManage.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
                     return Redirect(returnUrl);
                 }
+                else
+                {
+                    if (string.IsNullOrEmpty(returnUrl))
+                        ViewBag.ReturnUrl = Url.Action("index", "home");
+                    else
+                        ViewBag.ReturnUrl = returnUrl;
+                    ViewBag.LoginUserStatus = loginStatus;
+                    return View();
+                }
             }
+            if (string.IsNullOrEmpty(returnUrl))
+                ViewBag.ReturnUrl = Url.Action("index", "home");
+            else
+                ViewBag.ReturnUrl = returnUrl;
             return View();
+        }
+        //修改密码
+        public ActionResult RevisePwd()
+        {
+            if (Session["LoginUserSessionModel"] == null)
+            { return RedirectToAction("Login"); }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RevisePwd(RevisePwdViewModelr model)
+        {
+            //用户先得登录才能修改
+
+            if (ModelState.IsValid)
+            {
+                //输入合法，进行修改
+                bool changePwdSuccessed;
+                try
+                {
+
+                    var userManage =
+                    new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new EntityDbContext()));
+                    var userName = (Session["LoginUserSessionModel"] as LoginUserSessionModel).User.UserName;
+                    //判断原密码是否输入正确
+                    var user = userManage.Find(userName, model.PassWord);
+                    if (user == null)
+                    {
+                        ModelState.AddModelError("", "原密码不正确");
+                        return View(model);
+                    }
+                    else
+                    {
+                        //修改密码
+                        changePwdSuccessed = userManage.ChangePassword(user.Id, model.PassWord, model.NewPassWord).Succeeded;
+                        if (changePwdSuccessed)
+                        {
+                            return Content("<script>alert('恭喜修改密码成功!');location.href='" + Url.Action("index", "home") +
+                                           "'</script>");
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "修改密码失败，请重试");
+                        }
+                    }
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "修改密码失败，请重试");
+                }
+            }
+
+            return View(model);
+        }
+        //个人信息
+        public ActionResult userInfo()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 用户注销
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult loginout()
+        {
+            Session.Remove("loginStatus");
+            Session.Remove("LoginUserSessionModel");
+            return RedirectToAction("index", "Home");
         }
     }
 }
