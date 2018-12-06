3e1d0f5eddd7dcda8e1e1f6ae70796bbcffad4e6
mwailxx <1650098394@qq.com>
2018-12-06 12:31:19 +0800

    12-6上午密码，注册，修改

---
+++ 05MusicStore1.1/MusicStore101/MusicStore/ViewModels/ChangePassWordViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.ViewModels
+{
+    public class ChangePassWordViewModel
+    {
+        [Required(ErrorMessage = "密码不能为空")]
+        [Display(Name = "密码")]
+        [DataType(DataType.Password)]
+        public string PassWord { get; set; }
+
+        [Required(ErrorMessage = "新密码不能为空")]
+        [Display(Name = "新密码")]
+        [DataType(DataType.Password)]
+        [StringLength(20, ErrorMessage = "{0}长度不能大于{1}小于{2}位", MinimumLength = 6)]
+        public string PassWord { get; set; }
+
+        [Display(Name = "密码")]
+        [DataType(DataType.Password)]
+        [Compare("PassWord", ErrorMessage = "密码两次输入要一致")]
+        public string ConfirmPassWord { get; set; }
+    }
+}
