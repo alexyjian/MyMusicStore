25a23fb63b9c71cc78698f1ada1f23b10107292f
HappyWindManS <3066004354@qq.com>
2018-12-06 12:02:01 +0800

    708Study,11/6,12:01[注册、注销与修改密码]

--- MR/ASPNETMVC/MusicStore01/MusicStore/Controllers/AccountController.cs
+++ MR/ASPNETMVC/MusicStore01/MusicStore/Controllers/AccountController.cs
@@ -28,6 +28,43 @@ namespace MusicStore.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Register(RegisterViewModel model)
         {
+            if(ModelState.IsValid)
+            {
+                var person = new Person()
+                {
+                    FirstName = model.FullName.Substring(0, 1),
+                    LastName = model.FullName.Substring(1, model.FullName.Length - 1),
+                    Name = model.FullName,
+                    CredentialsCode = "",
+                    Birthday = DateTime.Now,
+                    Sex = true,
+                    MobileNumber = "18000010001",
+                    Email = model.Email,
+                    TelephoneNumber = "18000010001",
+                    Description = "",
+                    CreateDateTime = DateTime.Now,
+                    UpdateTime = DateTime.Now,
+                    InquiryPassword = "未设置",
+                };
+                var user = new ApplicationUser()
+                {
+                    UserName = model.UserName,
+                    FirstName = model.FullName.Substring(0, 1),
+                    LastName = model.FullName.Substring(1, model.FullName.Length - 1),
+                    ChineseFullName = model.FullName,
+                    MobileNumber = "18000010001",
+                    Email = model.Email,
+                    Person = person,
+                };
+
+                var idManager = new IdentityManager();
+                idManager.CreateUser(user, model.PassWord);
+                idManager.AddUserToRole(user.Id, "RegisterUser");
+
+                //return RedirectToAction();
+                return Content("<script>alert('恭喜注册成功');location.href'" + Url.Action("login", "Account") + "</script>");
+            }
+
             //用户的保存Person ApplicationUser
             return View();
         }
@@ -108,5 +145,61 @@ namespace MusicStore.Controllers
                 ViewBag.ReturnUrl = returnUrl;
             return View();
         }
+
+        public ActionResult LoginOut()
+        {
+            Session.Remove("loginStatus");
+            Session.Remove("LoginUserSessionModel");
+            return RedirectToAction("index", "Home");
+        }
+
+        public ActionResult ChangePassWord()
+        {
+            //用户得先登录才能修改
+            if (Session["LoginUserSessionModel"]==null)
+            {
+                return RedirectToAction("login");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ChangePassWord(ChangePassWordViewModel model)
+        {
+            bool changePwdSuccessed;
+            try
+            {
+                var userManage =
+                 new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new EntityDbContext()));
+                var username=(Session["LoginUserSessionModel"]as LoginUserSessionModel).User.UserName;
+                var user = userManage.Find(username, model.PassWord);
+                if(user==null)
+                {
+                    ModelState.AddModelError("", "原密码不正确");
+                    return View();
+                }
+                else
+                {
+                    //修改密码
+                    changePwdSuccessed=userManage.ChangePassword(user.Id, model.PassWord, model.NewPassWord)
+                        .Succeeded;
+
+                    if(changePwdSuccessed)
+                    {
+                        return Content("<script>alert('恭喜修改密码成功');location.href='" + 
+                            Url.Action("Index", "Home") + "'</script>");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "修改密码失败，请重试");
+                    }
+                }
+            }
+            catch
+            {
+                ModelState.AddModelError("", "修改密码失败，请重试");
+            }
+            return View(model);
+        }      
     }
 }
