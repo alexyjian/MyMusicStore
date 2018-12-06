d666d5c1b7cdae814b3edca08e448809d1f845ce
suweixi <1985748697@qq.com>
2018-12-06 11:07:35 +0800

    用户登录、新用户注册、密码注销、密码修改

--- Dashan/05MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
+++ Dashan/05MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
@@ -11,8 +11,10 @@ using MusicStoreEntity.UserAndRole;
 
 namespace MusicStore.Controllers
 {
+
     public class AccountController : Controller
     {
+       
         /// <summary>
         /// 填写注册信息
         /// </summary>
@@ -155,6 +157,50 @@ namespace MusicStore.Controllers
             Session.Remove("LoginUserSessionModel");
             return RedirectToAction("index", "Home"); //返回首页
         }
-    }
 
+        public ActionResult ChangePassWord()
+        {
+
+            //登录状态下用户才能修改密码
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("Login");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ChangePassWord(ChangePassWordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //输入合法进行修改密码
+                bool changePwdSuccessed;
+                try
+                {
+                    var userManage =
+                        new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new EntityDbContext()));
+                    var userName = (Session["LoginUserSessionModel"] as LoginUserSessionModel).User.UserName;
+                    //判断原密码是否输入正确
+                    var user = userManage.Find(userName, model.PassWord);
+                    if(user == null )
+                    {
+                        ModelState.AddModelError("", "原密码不正确！！！");
+                        return View(model);
+                    }
+                    else
+                    {
+                        //修改密码
+                        changePwdSuccessed = userManage.ChangePassword(user.Id, model.PassWord, model.NewPassWord).Succeeded;
+                        if (changePwdSuccessed)
+                            return Content("<script>alert('修改密码成功啦！'); location.href='" + Url.Action("login", "Account") + "'</script >");//修改密码成功后弹出对话框
+                        else
+                            ModelState.AddModelError("", "修改密码失败！请重试！");
+                    }
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "修改密码失败！请重试！");
+                }
+            }
+            return View(model);
+        }
+    }
 }
