c9276da339ae7a283ef51f063932939da696b581
suweixi <1985748697@qq.com>
2018-12-06 09:08:04 +0800

    注册新用户完成

--- Dashan/05MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
+++ Dashan/05MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
@@ -19,8 +19,6 @@ namespace MusicStore.Controllers
         /// <returns></returns>
         public ActionResult Register()
         {
-          
-
             return View();
         }
 
@@ -30,6 +28,45 @@ namespace MusicStore.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Register(RegisterViewModel model )
         {
+            //新用户的数据引用
+            if(ModelState.IsValid)
+            {
+                var person = new Person()
+                {
+                    FirstName = model.FullName.Substring(0,1),
+                    LastName = model .FullName .Substring (1,model.FullName.Length-1),
+                    CredentialsCode = "452222199821566789",
+                    Name  = model.FullName,
+                    Birthday = DateTime.Now,
+                    Sex = false,
+                    MobileNumber = "18346464645",
+                    Email = model.Email,
+                    TelephoneNumber = "35346557787",
+                    CreateDateTime = DateTime.Now,
+                    Description = "注册用户",
+                    UpdateTime = DateTime.Now,
+                    InquiryPassword = "未设置 ",
+                };
+
+                var user = new ApplicationUser()
+                {
+                    UserName = model .UserName,
+                    FirstName = model.FullName.Substring(0, 1),
+                    LastName = model.FullName.Substring(1, model.FullName.Length-1),
+                    ChineseFullName = model .FullName,
+                    MobileNumber = "18346464645",
+                    Email = model .Email ,
+                    Person = person,
+
+                };
+
+                var idManage = new IdentityManager();
+                idManage.CreateUser(user, model.PassWord);
+                idManage.AddUserToRole(user.Id, "RegisterUser");
+
+                return Content("<script>alert('注册成功'); location.href='" +Url.Action("login","Account") +"'</script >");
+            }
+
             return View();
         }
 
@@ -109,6 +146,14 @@ namespace MusicStore.Controllers
             else
                 ViewBag.ReturnUrl = returnUrl;
             return View();
+        }
+
+        public ActionResult LoginOut()
+        {
+            //注销
+            Session.Remove("loginStatus");
+            Session.Remove("LoginUserSessionModel");
+            return RedirectToAction("index", "Home"); //返回首页
         }
     }
 
