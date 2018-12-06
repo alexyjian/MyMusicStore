492f518c9e5a7e0b2b3d50a771955034a8a34e64
lrz991222 <353226337@qq.com>
2018-12-06 09:06:16 +0800

    完成注册页

--- LRZ/MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
+++ LRZ/MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
@@ -26,7 +26,43 @@ namespace MusicStore.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Register(RegisterViewModel model)
         {
-            //用户保存 Person ApplicationUser
+            if (ModelState.IsValid)
+            {
+                var person = new Person()
+                {
+                    FirstName = model.FullName.Substring(0, 1),
+                    LastName = model.FullName.Substring(1, model.FullName.Length - 1),
+                    Name = model.FullName,
+                    CredentialsCode = "",
+                    Birthday = DateTime.Now,
+                    Sex = true,
+                    MobileNumber = "18070112222",
+                    Email = model.Email,
+                    TelephoneNumber = "18070112222",
+                    Description = "",
+                    CreateDateTime = DateTime.Now,
+                    UpdateTime = DateTime.Now,
+                    InquiryPassword = "未设置",
+                };
+                var user = new ApplicationUser()
+                {
+                    UserName = model.UserName,
+                    FirstName = model.FullName.Substring(0, 1),
+                    LastName = model.FullName.Substring(1, model.FullName.Length - 1),
+                    ChineseFullName = model.FullName,
+                    MobileNumber = "18070112222",
+                    Email = model.Email,
+                    Person = person,
+                };
+
+                //是否要验证Email
+                var idManager = new IdentityManager();
+                idManager.CreateUser(user, model.PassWord);
+                idManager.AddUserToRole(user.Id, "RegisterUser");
+
+                return Content("<script>alert('用户注册成功!');location.href='" + Url.Action("login", "Account") + "'</script>");
+            }
+
             return View();
         }
 
