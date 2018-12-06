bf633c8a1dd8401e0b018269b9c067faef9969a8
hxm193514 <1935197914@qq.com>
2018-12-07 01:11:15 +0800

    半夜乱提交

--- HandsomeBoy/02Css/轮播/轮播图/demo1.js
+++ HandsomeBoy/02Css/轮播/轮播图/demo1.js
@@ -1,27 +1,26 @@
-var oImg = $('img');
-var len = oImg.length;
-var flag = true;
-var curDisplay = 0;
-var nowIndex = 0;
-var timer;
-var wrap = $('.wrapper');
-var btn = $('.btn');
+var oImg = $('img');//选出图片标签
+var len = oImg.length;//保存有几张图片
+var flag = true;//判断是否翻页
+var nowIndex = 0; //保存图片索引
+var timer; //弄一个定时器
+var wrap = $('.wrapper');//保存最大的DIV
+var btn = $('.btn');//保存左右按钮
 
 function init() {
     carousel();
     bindEvent();
 }
 init();
-
+//代码旋转
 function carousel() {
-    var midLen = Math.floor(len / 2);
-    var lNum, rNum;
+    var midLen = Math.floor(len / 2); //每边一半的图片数量
+    var lNum, rNum; //保存左右图片的位置
     for (var i = 0; i < midLen; i++) {
-        lNum = curDisplay - i - 1;
+        lNum = nowIndex - i - 1; //从中间数 获取左边从第一张开始的图片
         oImg.eq(lNum).css({
             transform: 'translateX(' + (-150 * (i + 1)) + 'px) translateZ(' + (200 - i * 100) + 'px)'
         })
-        rNum = curDisplay + i + 1;
+        rNum = nowIndex + i + 1;//从中间数 获取从右边第一张开始的图片
         if (rNum > len - 1) {
             rNum -= len;
         }
@@ -29,7 +28,7 @@ function carousel() {
             transform: 'translateX(' + (150 * (i + 1)) + 'px) translateZ(' + (200 - i * 100) + 'px)'
         });
     };
-    oImg.eq(curDisplay).css({
+    oImg.eq(nowIndex).css({
         transform: 'translateZ(300px)'
     });
     wrap.on('transitionend', function () {
