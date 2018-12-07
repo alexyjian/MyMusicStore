60541ae6be60f1ef03823a1522ba803a8054211f
mwailxx <1650098394@qq.com>
2018-12-07 10:55:31 +0800

    12-7上午

---
+++ 05MusicStore1.1/MusicStore101/MusicStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading;
+using System.Web.Mvc;
+using System.Linq;
+
+namespace MusicStore.Controllers
+{
+    public class ShoppingCartController
+    {
+        ///<summary>
+        ///添加专辑到购物车
+        ///</summary>
+        ///<returns></returns>
+        [HttpPost]
+        public ActionResult AddCart(Guid id)
+        {
+            Thread.Sleep(300); //
+
+            if (Session["LoginUserSessionModel"] == null)
+                return Json("nologin");
+
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionMode).Person;
+            //
+            //
+            var cartItem = _context.Carts.SingleOrDefault(x => x.Person.ID == x.Person.ID && x.Album.ID == id);
+            var message = "";
+            if (cartItem == null)
+            {
+                //
+                cartItem = new Cart;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ 05MusicStore1.1/MusicStore101/MusicStoreEntity/Gart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStoreEntity
+{
+    /// <summary>
+    /// 购物车实体
+    /// </summary>
+    public class Gart
+    {
+        public Guid ID { get; set; }
+        public string CartID { get; set; }//
+        public int Count { get; set; }
+
+    }
+}
