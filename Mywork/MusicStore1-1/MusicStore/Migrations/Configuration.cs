58badc8b80898861215b7c085a9ed859bd33df0f
Baiguia <1942152252@qq.com>
2018-12-07 11:03:02 +0800

    12-7 添加购物车成功

--- Mywork/MusicStore1-1/MusicStore/Migrations/Configuration.cs
+++ Mywork/MusicStore1-1/MusicStore/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace MusicStoreEntity.Migrations
 {
+    using MusicStore.Migrations;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
--- Mywork/MusicStore1-1/MusicStoreEntity/EntityDbContext.cs
+++ Mywork/MusicStore1-1/MusicStoreEntity/EntityDbContext.cs
@@ -33,6 +33,8 @@ namespace MusicStoreEntity
         public IDbSet<Genre> Genres { get; set; }
         public IDbSet<Album> Albums { get; set; }
         public IDbSet<Artist> Artists { get; set; }
+
+        public IDbSet<Cart> Carts { get; set; }
         #endregion
     }
 }
