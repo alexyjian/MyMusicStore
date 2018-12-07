62dbba0e9863f33ea3a2425d2913852306801226
HappyWindManS <3066004354@qq.com>
2018-12-07 09:59:38 +0800

    708Study;12/7,10:00

---
+++ MR/ASPNETMVC/MusicStore01/MusicStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,31 @@
+﻿using MusicStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        /// <summary>
+        /// 添加专辑到购物车
+        /// </summary>
+        /// <returns></returns>
+        // GET: ShoppingCart
+        [HttpPost]
+        public ActionResult AddCart(Guid id)
+        {
+            if(Session["LoginUserSessionModel"]==null)
+            {
+                return Json("nologin");
+            }
+
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+            
+            var cartItem=_context.Carts.SingleOrDefault()
+            return View();
+        }
+    }
+}
---
+++ MR/ASPNETMVC/MusicStore01/MusicStoreEntity/Cart.cs
@@ -0,0 +1,28 @@
+﻿using MusicStoreEntity.UserAndRole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStoreEntity
+{
+    //购物车的实体
+    public class Cart
+    {
+        public Guid ID { get; set; }
+        public string CartID { get; set; }//购物车内部编号
+        public int Count { get; set; }//专辑的数量
+        public DateTime CreateDate { get; set; }
+
+        public virtual Album Album { get; set; }//关联专辑
+        public string AlbumID { get; set; }//便于前端编程
+        public virtual Person Person { get; set; }//关联用户
+
+        public Cart()
+        {
+            ID = Guid.NewGuid();
+            CreateDate = DateTime.Now;
+        }
+    }
+}
