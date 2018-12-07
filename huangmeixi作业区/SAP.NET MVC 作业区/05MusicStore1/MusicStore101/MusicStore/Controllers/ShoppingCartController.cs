5e3bee5a90c54091ef69154800fd70352042e994
huangmeixiqi <1600582963@qq.com>
2018-12-07 09:34:42 +0800

    添加购物车12.07

---
+++ huangmeixi作业区/SAP.NET MVC 作业区/05MusicStore1/MusicStore101/MusicStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        /// <summary>
+        /// 添加专辑到购物车
+        /// </summary>
+        /// <returns></returns>
+        // GET: ShoppingCart
+        [HttpPost]//处理提交
+        public ActionResult AddCart(Guid id)
+        {
+            
+            if (Session["LoginUserSessionModel"] == null)
+                return Json("nologin");
+            return View();
+        }
+    }
+}
---
+++ huangmeixi作业区/SAP.NET MVC 作业区/05MusicStore1/MusicStore101/MusicStoreEntity/Cart.cs
@@ -0,0 +1,31 @@
+﻿using MusicStoreEntity.UserAndRole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStoreEntity
+{
+    /// <summary>
+    /// 购物车的实体
+    /// </summary>
+  public  class Cart
+    {
+        public Guid ID { get; set; }
+        public string CartID { get; set; }
+
+        public  int Count { get; set; }
+        public  DateTime CreateDate { get; set; }
+
+        public virtual Album Album { get; set; }//关联专辑
+        public string AlbumID { get; set; }//便于前端编程
+        public virtual Person Person { get; set; }//关联用户
+
+        public Cart()
+        {
+            ID = Guid.NewGuid();
+            CreateDate = DateTime.Now;
+        }
+    }
+}
