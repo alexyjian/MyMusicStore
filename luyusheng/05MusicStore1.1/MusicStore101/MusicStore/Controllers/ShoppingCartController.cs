ce92754b2998951ad8ef7964ae25f7c6d1238136
luyusheng7 <2278814961@qq.com>
2018-12-07 11:59:24 +0800

    完成添加专辑到购物车

--- luyusheng/05MusicStore1.1/MusicStore101/MusicStore/Controllers/ShoppingCartController.cs
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStore/Controllers/ShoppingCartController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MusicStore.ViewModels;
+using MusicStoreEntity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -9,6 +11,7 @@ namespace MusicStore.Controllers
 {
     public class ShoppingCartController : Controller
     {
+        private static readonly EntityDbContext _context = new EntityDbContext();
         /// <summary>
         /// 添加专辑到购物车
         /// </summary>
@@ -17,12 +20,37 @@ namespace MusicStore.Controllers
         public ActionResult AddCart(Guid id)
         {
             //为了模仿真实网站环境,延时3秒，显示加载的艰苦
-            Thread.Sleep(3000);
-
+            Thread.Sleep(1000);
             if (Session["LoginUserSessionModel"] == null)
                 return Json("nologin");
 
-            return View();
+            var person = (Session["LoginUserSessionModel"]as LoginUserSessionModel).Person;
+            //添加购物车：如果购物车中没有当前专辑,直接添加,数量为1;如果购物车中存在此专辑,数量+1
+            //查询该用户的购物车记录是否包含此专辑
+            var cartItem = _context.Carts.SingleOrDefault(x => x.Person.ID == x.Person.ID && x.Album.ID == id);
+            var messge = "";
+                if(cartItem == null)
+            {
+                //
+                cartItem = new Cart()
+                {
+                    AlbumID = id.ToString(),
+                    Album = _context.Albums.Find(id),
+                    Person = _context.Persons.Find(person.ID),
+                    Count = 1,
+                    CartID = (_context.Carts.Where(x => x.Person.ID == person.ID).ToList().Count()+1).ToString()
+                };
+                _context.Carts.Add(cartItem);
+                _context.SaveChanges();
+                messge = "添加" + _context.Albums.Find(id).Title + "到购物车成功!";
+            }
+            else
+            {
+                cartItem.Count++;
+                _context.SaveChanges();
+                messge = _context.Albums.Find(id).Title + "原来就在购物车中,已为您数量+1!";
+            }
+            return Json(messge);         
         }
     }
 }
