2e28d6529a49af841a016b5909f60e0a43200dda
luyusheng7 <2278814961@qq.com>
2018-12-07 09:35:33 +0800

    局部刷新AJAX,添加购物车的操作

---
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        /// <summary>
+        /// 添加专辑到购物车
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult AddCart(Guid id)
+        {
+            //为了模仿真实网站环境,延时3秒，显示加载的艰苦
+            Thread.Sleep(3000);
+
+            if (Session["LoginUserSessionModel"] == null)
+                return Json("nologin");
+
+            return View();
+        }
+    }
+}
