7c78f3950679c03fab5dd678ed9e9cceb21a582f
balabalaQAQ <1124048070@qq.com>
2018-12-07 09:08:50 +0800

    .

---
+++ balabalaQAQ/作业/MusicStore1_01/MusicStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        /// <summary>
+        /// 添加专辑到购物车
+        /// </summary>
+        /// <returns></returns>
+        // GET: ShoppingCart
+        [HttpPost]
+        public ActionResult AddCart(Guid id)
+        {
+
+            //Thread.Sleep(3000);//延迟3S启用
+            if (Session["LoginUserSessionModel"] == null)
+                return Json("nologin");
+            return View();
+        }
+    }
+}
