256d754f47e9cd08188f1faf9ef01c74fbe6fe88
suweixi <1985748697@qq.com>
2018-12-09 09:13:09 +0800

    浏览购物车

--- Dashan/05MusicStore1.1/MusicStore101/MusicStore/Controllers/ShoppingCartController.cs
+++ Dashan/05MusicStore1.1/MusicStore101/MusicStore/Controllers/ShoppingCartController.cs
@@ -9,11 +9,37 @@ using System.Web.Mvc;
 
 namespace MusicStore.Controllers
 {
+    /// <summary>
+    /// 查询用户自己的购物车
+    /// </summary>
     public class ShoppingCartController : Controller
     {
         public ActionResult Index()
         {
-            return View();
+            //判断用户是否登录
+            if (Session["loginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Index", "ShoppingCart")});
+     
+
+            //查询出当前的登录用户
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+           
+            //查询出该用户的购物车项
+            var carts = _context.Carts.Where(x => x.Person.ID == x.Person.ID).ToList();
+
+            //计算购物车的总价
+            //linq 表达式
+            decimal? totalPricce = (from item in carts select item.Count * item.Album.Price).Sum();
+
+            //创建视图模型
+            //判断是否为空 为空则是它本身，则是所传的值
+            var cartVM = new ShoppingCartViewModel()
+            {
+                CartItems = carts,
+                CartTotaPrice = totalPricce ?? decimal.Zero
+
+            };
+            return View(cartVM);
         }
 
        
---
+++ Dashan/05MusicStore1.1/MusicStore101/MusicStore/ViewMoldels/ShoppingCartViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MusicStoreEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.ViewMoldels
+{
+    /// <summary>
+    /// 购物车的视图模型
+    /// </summary>
+    public class ShoppingCartViewModel
+    {
+        //所有的购物项
+        public List<Cart> CartItems { get; set; }
+        //总价格
+        public decimal CartTotaPrice { get; set; }
+    }
+}
