77ea445a4342a81b3d17146832d04729305651ee
HappyWindManS <3066004354@qq.com>
2018-12-09 11:46:30 +0800

    708Study;12/9,11:46

--- MR/ASPNETMVC/MusicStore01/MusicStore/Controllers/ShoppingCartController.cs
+++ MR/ASPNETMVC/MusicStore01/MusicStore/Controllers/ShoppingCartController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using MusicStore.ViewModels;
 using MusicStoreEntity;
 
+
 namespace MusicStore.Controllers
 {
     public class ShoppingCartController : Controller
@@ -20,7 +21,7 @@ namespace MusicStore.Controllers
         [HttpPost]
         public ActionResult AddCart(Guid id)
         {
-            Thread.Sleep(1000);   //为了模仿真实网站环境，延时3秒，显示加载的艰苦 
+            Thread.Sleep(1000);   
             if (Session["LoginUserSessionModel"] == null)
                 return Json("nologin");
             var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
@@ -57,7 +58,7 @@ namespace MusicStore.Controllers
             //判断用户是否登录
             if(Session["LoginUserSessionModel"]==null)
             {
-                return RedirectToAction("login","Accout",new {returnUrl=Url.Action("index","ShoppingCart")});
+                return RedirectToAction("login","Account",new {returnUrl=Url.Action("index","ShoppingCart")});
             }
             //查询出当前登录用户
             var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
@@ -83,6 +84,46 @@ namespace MusicStore.Controllers
             };
 
             return View(cartVM);
+        }
+
+        /// <summary>
+        /// 删除购物车项
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult RemoveCart(Guid id)
+        {
+            //判断用户是否登录
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("index", "ShoppingCart") });
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+
+            //查询出要处理删除的购物车项
+            var cartItem = _context.Carts.Find(id);
+            //如果购物项数量大于1则减1，如果为1则删除
+            if (cartItem.Count > 1)
+                cartItem.Count--;
+            else
+                _context.Carts.Remove(cartItem);
+            _context.SaveChanges();
+
+            //刷新局部视图  生成html元素注入到<tbody>中
+            var carts = _context.Carts.Where(x => x.Person.ID == person.ID).ToList();
+            var totalPrice = (from item in carts select item.Count * item.Album.Price).Sum();
+            var htmlString = "";
+            foreach (var item in carts)
+            {
+                htmlString += "<tr>";
+                htmlString += " <td><a href='../store/detail/" + item.ID + "'>" + item.Album.Title + "</a></td>";
+                htmlString += "<td>" + item.Album.Price.ToString("C") + "</td>";
+                htmlString += "<td>" + item.Count + "</td>";
+                htmlString += "<td><a href=\"#\" onclick=\"removeCart('" + item.ID + "');\"><i class=\"glyphicon glyphicon-remove\"></i>移出购物车</a></td><tr>";
+            }
+
+            htmlString += "<tr><td ></td><td></td><td>总价</td><td>" + totalPrice.ToString("C") + "</td ></tr>";
+
+            return Json(htmlString);
         }
     }
 }
