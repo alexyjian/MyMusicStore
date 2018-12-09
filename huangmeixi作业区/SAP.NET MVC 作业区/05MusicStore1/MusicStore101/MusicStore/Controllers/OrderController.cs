34cc75e68ee0bfa1d6f985b537651b5ff9bb4820
huangmeixiqi <1600582963@qq.com>
2018-12-09 15:52:26 +0800

    下拉菜单页12.09

---
+++ huangmeixi作业区/SAP.NET MVC 作业区/05MusicStore1/MusicStore101/MusicStore/Controllers/OrderController.cs
@@ -0,0 +1,49 @@
+﻿using MusicStoreEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        /// <summary>
+        /// 下单页
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Buy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RemoveDetail(Guid id)
+        {
+            return Json("");
+           
+        }
+
+        /// <summary>
+        /// 处理提交下单
+        /// </summary>
+        /// <param name="Oder"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Buy(Order Oder)
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// 浏览用户提交下单
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
