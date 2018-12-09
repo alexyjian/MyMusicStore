9ad54b98c36475d2613b7ad3aa21328c963c2433
mwailxx <1650098394@qq.com>
2018-12-09 18:03:00 +0800

    12-9下午1

---
+++ 05MusicStore1.1/MusicStore101/MusicStore/Controllers/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStoreEntity;
+
+namespace MusicStore.Controllers
+{
+    public class OrderController : Controller
+    {
+       /// <summary>
+       /// 下单页
+       /// </summary>
+       /// <returns></returns>
+        public ActionResult Buy()
+        {
+            //1.确认用户是否登录 是否登录过期
+
+            //2.查询出当前用户Person 查询该用户的购物车项
+
+            //3.创建新Order对象
+
+            //4.把购物项导入订单明细
+
+            //5.将订单和明细在视图呈现，验证用户收件人、地址、电话、供用户选择确认要购买专辑
+
+            return View();
+        }
+
+        public ActionResult RemoveDetail(Guid id)
+        {
+            return Json("");
+        }
+
+        ///<summary>
+        ///处理用户提交下单
+        ///</summary>
+        ///<param name="oder"></param> 
+    }
+}
