b037d55233dd76d13ea00fcc3aaf3dde2f713108
suweixi <1985748697@qq.com>
2018-12-09 15:48:26 +0800

    下单页

---
+++ Dashan/05MusicStore1.1/MusicStore101/MusicStore/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using MusicStoreEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class OrderController : Controller
+    {
+     
+        public ActionResult Buy()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult RemoveDetail(Guid id)
+        {
+            return Json ("");
+        }
+
+        /// <summary>
+        /// 处理提交下单
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        public ActionResult Buy(Order order)
+        {
+            return View();
+        }
+        /// <summary>
+        /// 浏览用户订单
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
