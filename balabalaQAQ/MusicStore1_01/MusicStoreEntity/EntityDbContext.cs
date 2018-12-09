32bd67bddf99ae46c5fec6965de133ce7d71b2bf
balabalaQAQ <1124048070@qq.com>
2018-12-09 15:29:06 +0800

    购买实体

--- balabalaQAQ/MusicStore1_01/MusicStoreEntity/EntityDbContext.cs
+++ balabalaQAQ/MusicStore1_01/MusicStoreEntity/EntityDbContext.cs
@@ -47,5 +47,8 @@ namespace MusicStoreEntity
         #endregion
 
         public IDbSet<Cart> Cart { get; set; }
+
+        public IDbSet<Order> Order { get; set; }
+        public IDbSet<OrdelDetail> OrdelDetail { get; set; }
     }
 }
