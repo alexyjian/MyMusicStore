5290380bafe6514a35439f840180bbf1dff83426
huangmeixiqi <1600582963@qq.com>
2018-12-09 18:17:37 +0800

    确认订单视图

--- huangmeixi作业区/SAP.NET MVC 作业区/05MusicStore1/MusicStore101/MusicStore/Controllers/OrderController.cs
+++ huangmeixi作业区/SAP.NET MVC 作业区/05MusicStore1/MusicStore101/MusicStore/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using MusicStoreEntity;
+﻿using MusicStore.ViewModels;
+using MusicStoreEntity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,49 @@ namespace MusicStore.Controllers
     public class OrderController : Controller
     {
 
+        private static readonly EntityDbContext _context = new EntityDbContext();
         /// <summary>
         /// 下单页
         /// </summary>
         /// <returns></returns>
         public ActionResult Buy()
         {
+            //确认用户是否登录 是否登录过期
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Accoount", new { returnUrl = Url.Action("Buy", "Order") });
+
+            //查询出当前用户Person 查询该用户的购物项
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+            var carts = _context.Carts.Where(x => x.Person.ID == x.Person.ID).ToList();
+
+            //算购物车的总价
+            decimal? totalPrice = (from item in carts select item.Count * item.Album.Price).Sum();
+
+            //创建新Order对象
+
+            var order = new Order()
+            {
+                AddressPerson = person.Name,
+                MobilNumber = person.MobileNumber,
+                Person = _context.Persons.Find(person.ID),
+                TotalPrice = totalPrice ?? 0.00m,
+            };
+            //把购物车项导入订单明细
+            order.OrderDetails = new List<OrderDetail>();
+            foreach (var item in carts)
+            {
+                var detail = new OrderDetail()
+                {
+                    AlbumID = item.AlbumID,
+                    Album = _context.Albums.Find(item.Album.ID),
+                    Count = item.Count,
+                    Price = item.Album.Price
+                };
+                order.OrderDetails.Add(detail);
+            }
+
+            //将订单和明细在视图呈现，验证用户收件人、地址、电话、供用户选择确认要购买的专辑
+
             return View();
         }
         [HttpPost]
