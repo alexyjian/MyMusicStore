bb755cdb4a944ed4c50a5e4c145b1c1c0537f897
weitian666 <1935836331@qq.com>
2018-12-09 09:54:07 +0800

    删除购物车项

--- 韦添/C#/05MusicStore1.1/MusicStore101/MusicStore/Controllers/ShoppingCartController.cs
+++ 韦添/C#/05MusicStore1.1/MusicStore101/MusicStore/Controllers/ShoppingCartController.cs
@@ -51,7 +51,12 @@ namespace MusicStore.Controllers
             }
             return Json(message);
         }
-public ActionResult Index()
+
+        /// <summary>
+        /// 查看用户自己的购物车
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
         {
             //判断用户是否登录
             if (Session["LoginUserSessionModel"] == null)
@@ -60,20 +65,17 @@ public ActionResult Index()
             //查询出当前登录用户
             var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
 
-            //查询出该用户的购物车项目
+            //查询出该用户的购物车项
             var carts = _context.Carts.Where(x => x.Person.ID == x.Person.ID).ToList();
-
             //算购物车的总价
-            decimal?totalPrice=(from item in carts select item.Count*item.Album.Price).Sum();
-
+            decimal? totalPrice = (from item in carts select item.Count * item.Album.Price).Sum();  //linq表达式 一句完成
             //decimal? totalPrice2 = 0.00M;
             //if (carts.Count == 0)
             //    totalPrice2 = null;
-            //foreach(var item in carts)
+            //foreach (var item in carts)
             //{
             //    totalPrice2 += item.Count * item.Album.Price;
             //}
-
 
             //创建视图模型
             var cartVM = new ShoppingCartViewModel()
@@ -84,6 +86,45 @@ public ActionResult Index()
 
             return View(cartVM);
         }
-  
+
+        /// <summary>
+        /// 删除购物车项
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult RemoveCart(Guid id)
+        {
+            //判断用户是否登录
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("index", "ShoppingCart") });
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+
+            //查询出要处理删除的购物车项
+            var cartItem = _context.Carts.Find(id);
+            //如果购物项数量大于1则减1，如果为1则删除
+            if (cartItem.Count > 1)
+                cartItem.Count--;
+            else
+                _context.Carts.Remove(cartItem);
+            _context.SaveChanges();
+
+            //刷新局部视图  生成html元素注入到<tbody>中
+            var carts = _context.Carts.Where(x => x.Person.ID == person.ID).ToList();
+            var totalPrice = (from item in carts select item.Count * item.Album.Price).Sum();
+            var htmlString = "";
+            foreach (var item in carts)
+            {
+                htmlString += "<tr>";
+                htmlString += " <td><a href='../store/detail/" + item.ID + "'>" + item.Album.Title + "</a></td>";
+                htmlString += "<td>" + item.Album.Price.ToString("C") + "</td>";
+                htmlString += "<td>" + item.Count + "</td>";
+                htmlString += "<td><a href=\"#\" onclick=\"removeCart('" + item.ID + "');\"><i class=\"glyphicon glyphicon-remove\"></i>移出购物车</a></td><tr>";
+            }
+
+            htmlString += "<tr><td ></td><td></td><td>总价</td><td>" + totalPrice.ToString("C") + "</td ></tr>";
+
+            return Json(htmlString);
+        }
     }
 }
