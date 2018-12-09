a4729ae896a818c6f2553c96a51eff5e8873082c
kawayi1727 <1163695520>
2018-12-09 18:19:30 +0800

    确认订单视图

--- chenyuxiu/MusicStore101/MusicStore/Controllers/OrderController.cs
+++ chenyuxiu/MusicStore101/MusicStore/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using MusicStoreEntity;
+﻿using MusicStore.ViewModels;
+using MusicStoreEntity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,8 @@ namespace MusicStore.Controllers
 {
     public class OrderController : Controller
     {
+        private static readonly EntityDbContext _context = new EntityDbContext();
+
         /// <summary>
         /// 下单页
         /// </summary>
@@ -16,7 +19,45 @@ namespace MusicStore.Controllers
         // GET: Order
         public ActionResult Buy()
         {
-            return View();
+            //1.确认用户是否登录 是否登录过期
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new { retumUrl = Url.Action("Buy", "Order") });
+
+
+            //2.查询出当前用户Person 查询该用户的购物项
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+            var carts = _context.Carts.Where(x => x.Person.ID == x.Person.ID).ToList();
+            //算购物车的总价
+            decimal? totalPrice = (from item in carts select item.Count * item.Album.Price).Sum();
+            //3.创建新Orde对象 ，验证用户收件人.地址.电话
+            var order = new Order()
+            {
+                AddressPeron = person.Name,
+                MobilNumber = person.MobileNumber,
+                Person = _context.Persons.Find(person.ID),
+                TotaIPrice = totalPrice ?? 0.00M,
+            };
+
+
+            //4.把购物项导入订单明细
+            order.OrderDetail = new List<OrderDetail>();
+            foreach (var item in carts)
+            {
+                var detail = new OrderDetail()
+                {
+                    AlbumID=item.AlbumID,
+                    Album=_context.Albums.Find (item.Album.ID),
+                    Count =item.Count,
+                    Price =item.Album.Price
+                };
+
+            };
+
+            //将订单和明细在视图呈现，验证用户收件人，地址，电话，供用户选择确认要购买专辑
+
+            //单前订单未持久化，用会话保存方便用户进行编辑
+            Session["Order"] = order;
+            return View(order);
         }
         [HttpPost]
         public ActionResult RemoveDatail(Guid id)
