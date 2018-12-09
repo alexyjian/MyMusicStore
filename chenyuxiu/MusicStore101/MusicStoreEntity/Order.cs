076f598e324c1bc8c607a426f3d55612967f7389
kawayi1727 <1163695520>
2018-12-09 15:53:23 +0800

    下单的Action

---
+++ chenyuxiu/MusicStore101/MusicStoreEntity/Order.cs
@@ -0,0 +1,38 @@
+﻿using MusicStoreEntity.UserAndRole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStoreEntity
+{
+    /// <summary>
+    /// 订单实体
+    /// </summary>
+   public  class Order
+    {
+        public Guid ID { get; set; }
+        public DateTime OrderDateTme { get; set; } //订单的时间
+        public virtual Person Person { get; set; } //所属用户
+         public string AddressPeron { get; set; }//收件人
+        public string Address { get; set; }//收件人地址
+        public string MobilNumber { get; set; } //收件人的手机
+       public decimal TotaIPrice { get; set; } //总价
+        public string TradeNo { get; set; }//支付流水号
+        public bool PaySuccess { get; set; }//是否支付成功
+
+
+        public virtual EnumOrderStatus EnumOrderStatus { get; set; }
+
+        //购买专辑明细.EF中的包含关系
+        public virtual ICollection<OrderDetail> OrderDetail { get; set; }
+
+        public Order()
+        {
+            ID = Guid.NewGuid();
+            OrderDateTme = DateTime.Now;
+            TradeNo = OrderDateTme.ToString("yyyMMddhhmmssffff");
+        }
+    }
+}
