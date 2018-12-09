477f580cd03f86fe5512d384cd86c54126c01348
Pretty33 <321838579@qq.com>
2018-12-09 15:33:32 +0800

    20181209  (下午)建立订单实体

---
+++ WAnnie/05MusiceStore1.1/MusicStore101/MusicStoreEntity/Order.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicStoreEntity.UserAndRole;
+
+namespace MusicStoreEntity
+{
+ public   class Order
+    {
+        /// <summary>
+        /// 订单实体
+        /// </summary>
+        public Guid ID { get; set; }
+        //订单时间
+        public DateTime OrderDateTime { get; set; }
+        //所属用户
+        public virtual Person Person { get; set; }
+        //收件人
+        public string AddressPerson { get; set; }
+        //收件人地址
+        public string Address { get; set; }
+        //收件人手机
+        public string MobilNumber { get; set; }
+        //总价
+        public decimal TotalPrice { get; set; }
+        //支付宝流水号
+        public  string TradeNo { get; set; }
+        //是否支付成功
+        public bool PaySuccess { get; set; }
+
+        //订单状态
+        public  virtual EnumOrderStatus EnumOrderStatus { get; set; }
+
+        //购买专辑明细,EF中的包含关系
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
+
+        public Order()
+        {
+            ID=Guid.NewGuid();
+            OrderDateTime=DateTime.Now;
+            TradeNo = OrderDateTime.ToString("yyyyMMddhhmmssffff");
+        }
+
+    }
+}
---
+++ WAnnie/05MusiceStore1.1/MusicStore101/MusicStoreEntity/OrderDetail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStoreEntity
+{
+    /// <summary>
+    /// 订单明细
+    /// </summary>
+  public  class OrderDetail
+    {
+        public Guid ID { get; set; }
+        //专辑的主键
+        public string AlbumID { get; set; }
+        //专辑
+        public virtual Ablum Ablum { get; set; }
+
+        public decimal Price { get; set; }
+        public int Count { get; set; }
+
+        public OrderDetail()
+        {
+            ID=Guid.NewGuid();
+        }
+
+    }
+}
