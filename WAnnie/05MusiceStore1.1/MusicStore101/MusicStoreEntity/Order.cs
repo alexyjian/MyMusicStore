29b8b36de303ae1f738f4949c2f0e033acf2e99b
Pretty33 <321838579@qq.com>
2018-12-12 15:40:38 +0800

    20181212  更新确认订单视图

--- WAnnie/05MusiceStore1.1/MusicStore101/MusicStoreEntity/Order.cs
+++ WAnnie/05MusiceStore1.1/MusicStore101/MusicStoreEntity/Order.cs
@@ -16,35 +16,52 @@ namespace MusicStoreEntity
     {
         [ScaffoldColumn(false)]
         public Guid ID { get; set; }
+
+
         //订单时间
         public DateTime OrderDateTime { get; set; }
+       
+     
         //所属用户
+        public virtual Person Person { get; set; }
 
+        
+        //收件人
         [Display(Name = "收件人")]
         [Required(ErrorMessage = "收件人不能为空")]
-        public virtual Person Person { get; set; }
-        //收件人
+        public string AddressPerson { get; set; }
+        
 
+        //收件人地址
         [Display(Name = "收件人地址")]
         [Required(ErrorMessage = "收件人地址不能为空")]
-        public string AddressPerson { get; set; }
-        //收件人地址
-
+        public string Address { get; set; }
+        
+        
+        //收件人手机
         [Display(Name = "收件人手机")]
         [Required(ErrorMessage = "收件人手机不能为空")]
-        public string Address { get; set; }
-        //收件人手机
-
         public string MobilNumber { get; set; }
+        
         //总价
+        [ScaffoldColumn(false)]
         public decimal TotalPrice { get; set; }
+        
         //支付宝流水号
+        [ScaffoldColumn(false)]
         public  string TradeNo { get; set; }
-        //是否支付成功
-        public bool PaySuccess { get; set; }
+        
 
+        //是否支付成功
+        [ScaffoldColumn(false)]
+        public bool PaySuccess { get; set; }
+       
+        
         //订单状态
+        [ScaffoldColumn(false)]
+        
         public  virtual EnumOrderStatus EnumOrderStatus { get; set; }
+
 
         //购买专辑明细,EF中的包含关系
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
