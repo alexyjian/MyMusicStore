33d4fe7b708a29a9811f98c7919b836aadc2497a
lrz991222 <353226337@qq.com>
2018-12-12 16:44:16 +0800

    移除订单明细

--- LRZ/MusicStore1.1/MusicStore101/MusicStoreEntity/Order.cs
+++ LRZ/MusicStore1.1/MusicStore101/MusicStoreEntity/Order.cs
@@ -1,10 +1,11 @@
-﻿using MusicStoreEntity.UserAndRole;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using MusicStoreEntity.UserAndRole;
 
 namespace MusicStoreEntity
 {
@@ -13,12 +14,16 @@ namespace MusicStoreEntity
     /// </summary>
     public class Order
     {
+        [ScaffoldColumn(false)]
         public Guid ID { get; set; }
+
+        [ScaffoldColumn(false)]
         public DateTime OrderDateTime { get; set; }   //订单的时间
+
         public virtual Person Person { get; set; }  //所属用户
 
-        [Display(Name ="收件人")]
-        [Required(ErrorMessage ="收件人不能为空。")]
+        [Display(Name = "收件人")]
+        [Required(ErrorMessage = "收件人不能为空。")]
         public string AddressPerson { get; set; }   //收件人
 
         [Display(Name = "收件人地址")]
@@ -28,11 +33,19 @@ namespace MusicStoreEntity
         [Display(Name = "手机")]
         [Required(ErrorMessage = "手机不能为空。")]
         public string MobilNumber { get; set; }  //收件人的手机
+
+        [ScaffoldColumn(false)]
         public decimal TotalPrice { get; set; }   //总价
+
+        [ScaffoldColumn(false)]
         public string TradeNo { get; set; }    //支付流水号
+
+        [ScaffoldColumn(false)]
         public bool PaySuccess { get; set; }    //是否支付成功
 
+        [ScaffoldColumn(false)]
         public virtual EnumOrderStatus EnumOrderStatus { get; set; }  //订单状态
+
         //购买专辑明细,EF中的包含关系
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
 
