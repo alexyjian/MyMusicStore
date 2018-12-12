b2509faf81ee1f637a393f5de8b14fecc08b71a1
Seezhonghuai <1875680955@qq.com>
2018-12-12 15:20:49 +0800

    12-12-1

--- zhonghuai/MusicStore1.1/MusicStore101/MusicStore101/Controllers/OrderController.cs
+++ zhonghuai/MusicStore1.1/MusicStore101/MusicStore101/Controllers/OrderController.cs
@@ -27,38 +27,77 @@ namespace MusicStore101.Controllers
             //2.查询出当前用户Person 查询该用户的购物项
             var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
             var carts = _context.Carts.Where(x => x.Person.ID == x.Person.ID).ToList();
+
+
             //算购物车的总价
             decimal? totalPrice = (from item in carts select item.Count * item.Album.Price).Sum();  //linq表达式 一句完成
 
 
-            //3.创建新Order对象，验证用户收件人、地址、电话
+            //3.创建新Order对象
+            var order = new Order()
+            {
+                AddressPerson = person.Name,
+                MobilNumber = person.MobileNumber,
+                Person = _context.Persons.Find(person.ID),
+                TotalPrice = totalPrice ?? 0.00M,
+            };
 
 
-            //4.把购物项导入订单明细，供用户选择确认要购买专辑
+            //4.把购物项导入订单明细
+            order.OrderDetails = new List<OrderDetail>();
+            foreach (var item in carts)
+            {
+                var detail = new OrderDetail()
+                {
+                    AlbumID = item.AlbumID,
+                    Album = _context.Albums.Find(item.Album.ID),
+                    Count = item.Count,
+                    Price = item.Album.Price
+                };
+                order.OrderDetails.Add(detail);
+            }
 
 
-            //将订单何明细在视图呈现，验证用户收件人、地址、电话、供用户选择确认要购买的专辑
+            //将订单和明细在视图呈现, 验证用户收件人、地址、电话，供用户选择确认要购买专辑
 
-            return View();
+            //当前订单未持久化，用会话保存方便用户进行编辑
+            Session["Order"] = order;
+            return View(order);
         }
 
         [HttpPost]
-        public ActionResult RemoveDetail (Guid id)
+        public ActionResult RemoveDetail(Guid id)
         {
+            //如果回话为空，则重新刷新页面
+            if (Session["Order"] == null)
+                return RedirectToAction("buy");
+
+            //读取回话中的Order对象
+            var order = Session["Order"] as Order;
+            var deleteDetail = order.OrderDetails.SingleOrDefault(x=>x.ID == id);
+
+            //从订单明细列表中移除明细记录
+            order.OrderDetails.Remove(deleteDetail);
+
+            //根据新的order对象重新生成Html脚本，返回json数据，局部刷新视图
+
+
             return Json("");
         }
+
         /// <summary>
         /// 处理用户提交下单
         /// </summary>
+        /// <param name="oder"></param>
         /// <returns></returns>
-        // GET: Order
+        [HttpPost]
         public ActionResult Buy(Order oder)
         {
             return View();
         }
 
         /// <summary>
-        /// 浏览用户
+        /// 浏览用户订单列表
         /// </summary>
         /// <returns></returns>
         public ActionResult Index()
--- zhonghuai/MusicStore1.1/MusicStore101/MusicStoreEntity/Order.cs
+++ zhonghuai/MusicStore1.1/MusicStore101/MusicStoreEntity/Order.cs
@@ -1,6 +1,7 @@
 ﻿using MusicStoreEntity.UserAndRole;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,29 +13,46 @@ namespace MusicStoreEntity
     /// </summary>
   public   class Order
     {
+        [ScaffoldColumn(false)]
         public Guid ID { get; set; }
-        public DateTime OrderDateTime { get; set; }//订单时间
-        public virtual Person Person { get; set; }//所属用户
 
-       
-        public string AddressPerson { get; set; }//收件人
-        public  string Address { get; set; }//收件人地址
-        public string MobilNumber { get; set; }//收件人的手机
-        public  string TotalPrice { get; set; }//总价
-        public string TradeNo { get; set; }//支付流水号
-        public  bool PaySuccess { get; set; }//是否支付成功
+        [ScaffoldColumn(false)]
+        public DateTime OrderDateTime { get; set; }   //订单的时间
 
-        public virtual EnumOrderStatus EnumOrderStatus { get; set; }//订单的状态
-        //购买专辑明细，ef中的包含关系
+        public virtual Person Person { get; set; }  //所属用户
+
+        [Display(Name = "收件人")]
+        [Required(ErrorMessage = "收件人不能为空。")]
+        public string AddressPerson { get; set; }   //收件人
+
+        [Display(Name = "收件人地址")]
+        [Required(ErrorMessage = "收件人地址不能为空。")]
+        public string Address { get; set; }    //收件人地址
+
+        [Display(Name = "手机")]
+        [Required(ErrorMessage = "手机不能为空。")]
+        public string MobilNumber { get; set; }  //收件人的手机
+
+        [ScaffoldColumn(false)]
+        public decimal TotalPrice { get; set; }   //总价
+
+        [ScaffoldColumn(false)]
+        public string TradeNo { get; set; }    //支付流水号
+
+        [ScaffoldColumn(false)]
+        public bool PaySuccess { get; set; }    //是否支付成功
+
+        [ScaffoldColumn(false)]
+        public virtual EnumOrderStatus EnumOrderStatus { get; set; }  //订单状态
+
+        //购买专辑明细,EF中的包含关系
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
 
-        //购买专辑明细
         public Order()
         {
             ID = Guid.NewGuid();
             OrderDateTime = DateTime.Now;
             TradeNo = OrderDateTime.ToString("yyyyMMddhhmmssffff");
         }
-    } 
+    }
 }
- 
