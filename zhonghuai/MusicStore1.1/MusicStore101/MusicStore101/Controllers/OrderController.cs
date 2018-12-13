8c3c0cdfd34248b69437c8d6db84512d8c61c86b
Seezhonghuai <1875680955@qq.com>
2018-12-13 10:45:37 +0800

    12-13-1

--- zhonghuai/MusicStore1.1/MusicStore101/MusicStore101/Controllers/OrderController.cs
+++ zhonghuai/MusicStore1.1/MusicStore101/MusicStore101/Controllers/OrderController.cs
@@ -74,7 +74,7 @@ namespace MusicStore101.Controllers
 
             //读取回话中的Order对象
             var order = Session["Order"] as Order;
-            var deleteDetail = order.OrderDetails.SingleOrDefault(x=>x.ID == id);
+            var deleteDetail = order.OrderDetails.SingleOrDefault(x => x.ID == id);
 
             //从订单明细列表中移除明细记录
             order.OrderDetails.Remove(deleteDetail);
@@ -87,36 +87,71 @@ namespace MusicStore101.Controllers
             foreach (var item in order.OrderDetails)
             {
                 htmlString += "<tr>";
-                htmlString += "<td><a href='"+Url.Action("Detail","Store",new {id= item.Album.ID })+"'>"
-                    +item.Album.Title+"</a></td>";
-                htmlString +="<td>"+item.Album.Price.ToString("C")+"</td>";
-                htmlString += "<td>"+item.Count+"</td>";
-                htmlString += "<td><a href ='#' onclick='RemoveDetail('"+item.ID+"');'><i class='glyphicon glyphicon-remove'></i>我不喜欢了</a></td>";
+                htmlString += "<td><a href='" + Url.Action("Detail", "Store", new { id = item.Album.ID }) + "'>"
+                    + item.Album.Title + "</a></td>";
+                htmlString += "<td>" + item.Album.Price.ToString("C") + "</td>";
+                htmlString += "<td>" + item.Count + "</td>";
+                htmlString += "<td><a href='#' onclick='RemoveDetail('" + item.ID + "');'><i class='glyphicon glyphicon-remove'></i>我不喜欢它了</a></td>";
                 htmlString += "</tr>";
             }
 
-            htmlString += "<tr><td></td><td></td><td>总价</td><td>"+order.TotalPrice.ToString("C")+"</td></tr>";
+            htmlString += "<tr><td></td><td></td><td>总价</td><td>" + order.TotalPrice.ToString("C") + "</td></tr>";
+
             return Json(htmlString);
         }
 
         /// <summary>
         /// 处理用户提交下单
         /// </summary>
-        /// <param name="oder"></param>
+        /// <param name="order"></param>
         /// <returns></returns>
         [HttpPost]
-        public ActionResult Buy(Order oder)
+        public ActionResult Buy(Order order)
         {
-            //1.判断用户登录票据是否过期，如果过期跳转回登录页，登录成功后返回确认购买页
+            //1.判断用户登录凭据是否过期，如果过期跳转回登录页，登录成功后返回确认购买页
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Buy", "Order") });
 
-            //2. 读出当前用户Person
+            //2.读出当前用户Person
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+            order.Person = _context.Persons.Find(person.ID);
 
             //3.从会话中读出订单明细列表
+            order.OrderDetails = new List<OrderDetail>();
+            var details = (Session["Order"] as Order).OrderDetails;
+            foreach (var item in details)
+            {
+                item.Album = _context.Albums.Find(item.Album.ID);
+                order.OrderDetails.Add(item);
+            }
+            order.TotalPrice = (from item in order.OrderDetails select item.Count * item.Album.Price).Sum();
 
-            //4.如果表单验证通过验证，则保存order到数据库（锁定进程），跳转到Pay/AliPay
+            //4.如果表单验证通过，则保存order到数据库（锁定进程），跳转到Pay/AliPay  
+            if (ModelState.IsValid)
+            {
+                //加锁
+                LockedHelp.ThreadLocked(order.ID);
+                try
+                {
+                    _context.Orders.Add(order);
+                    _context.SaveChanges();
+
+                    //清空购物车
+
+                }
+                catch
+                {
+                }
+                finally
+                {
+                    LockedHelp.ThreadUnLocked(order.ID);
+                }
+
+                //跳转到支付页Pay/AliPay 
+                return RedirectToAction("Alipay", "Pay", new { id = order.ID });
+            }
 
             //5.如果验证不通过，返回视图
-
             return View();
         }
 
