754f7fffb4eba483f48a569f3f957ef13fc9270a
balabalaQAQ <1124048070@qq.com>
2018-12-13 19:22:32 +0800

    整理

---
+++ balabalaQAQ/MusicStore1_01/MusicStore1_01/MusicStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicStore.Startup))]
+namespace MusicStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
