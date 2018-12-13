94df3692f02b1e67c0708f85721e74e92e8b0d6c
2876416732 <723817914@qq.com>
2018-12-13 08:59:24 +0800

    线程加解锁类

--- 005MusicStore1.1/MusicStore101/MusicStoreWeb/Controllers/OrderController.cs
+++ 005MusicStore1.1/MusicStore101/MusicStoreWeb/Controllers/OrderController.cs
@@ -1,10 +1,10 @@
-﻿using MusicStore.ViewModels;
-using MusicStoreEntity;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MusicStore.ViewModels;
+using MusicStoreEntity;
 
 namespace MusicStore.Controllers
 {
@@ -18,22 +18,23 @@ namespace MusicStore.Controllers
         /// <returns></returns>
         public ActionResult Buy()
         {
-            //1.确认用户是否登录,是否登录过期
+            //1.确认用户是否登录  是否登录过期
             if (Session["LoginUserSessionModel"] == null)
                 return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Buy", "Order") });
-            //2.查询当前用户Person 查询该用户的购物项
+
+            //2.查询出当前用户Person  查询该用户的购物项
             var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
             var carts = _context.Carts.Where(x => x.Person.ID == x.Person.ID).ToList();
-            //算出购物车的总价
-            decimal? totalPrice = (from item in carts select item.Count * item.Album.Price).Sum(); //linq表达式一句完成
-            
+            //算购物车的总价
+            decimal? totalPrice = (from item in carts select item.Count * item.Album.Price).Sum();  //linq表达式 一句完成
+
             //3.创建新Order对象
             var order = new Order()
             {
                 AddressPerson = person.Name,
                 MobilNumber = person.MobileNumber,
                 Person = _context.Persons.Find(person.ID),
-                TotalPrice = totalPrice??0.00M,
+                TotalPrice = totalPrice ?? 0.00M,
             };
 
             //4.把购物项导入订单明细
@@ -50,9 +51,9 @@ namespace MusicStore.Controllers
                 order.OrderDetails.Add(detail);
             }
 
-            //将订单和明细在视图呈现,验证用户收件人、地址、电话、供用户选择确认要购买的专辑
+            //将订单和明细在视图呈现, 验证用户收件人、地址、电话，供用户选择确认要购买专辑
 
-            //当前订单未持久化,用会话保存方便用户进行编辑
+            //当前订单未持久化，用会话保存方便用户进行编辑
             Session["Order"] = order;
             return View(order);
         }
@@ -60,20 +61,36 @@ namespace MusicStore.Controllers
         [HttpPost]
         public ActionResult RemoveDetail(Guid id)
         {
-            //如果会话为空,则重新刷新页面
+            //如果会话为空，则重新刷新页面
             if (Session["Order"] == null)
                 return RedirectToAction("buy");
 
             //读取会话中的Order对象
             var order = Session["Order"] as Order;
-            var deleteDetail = order.OrderDetails.SingleOrDefault(x =>x.ID == id);
+            var deleteDetail = order.OrderDetails.SingleOrDefault(x => x.ID == id);
             //从订单明细列表中移除明细记录
             order.OrderDetails.Remove(deleteDetail);
 
-            //根据新的order对象重新生成Html脚本,返回json数据,局部刷新视图
+            //根据新的order对象重新生成Html脚本，返回json数据，局部刷新视图
+            var htmlString = "";
+            //订单总价
+            order.TotalPrice = (from item in order.OrderDetails select item.Count * item.Album.Price).Sum();
+            //更新用户编辑到会话
+            Session["Order"] = order;
+            foreach (var item in order.OrderDetails)
+            {
+                htmlString += "<tr>";
+                htmlString += "<td><a href='" + Url.Action("Detail", "Store", new { id = item.Album.ID }) + "'>"
+                    + item.Album.Title + "</a></td>";
+                htmlString += "<td>" + item.Album.Price.ToString("C") + "</td>";
+                htmlString += "<td>" + item.Count + "</td>";
+                htmlString += "<td><a href='#' onclick='RemoveDetail('" + item.ID + "');'><i class='glyphicon glyphicon-remove'></i>我不喜欢它了</a></td>";
+                htmlString += "</tr>";
+            }
 
+            htmlString += "<tr><td></td><td></td><td>总价</td><td>" + order.TotalPrice.ToString("C") + "</td></tr>";
 
-            return Json("");
+            return Json(htmlString);
         }
 
         /// <summary>
@@ -84,6 +101,15 @@ namespace MusicStore.Controllers
         [HttpPost]
         public ActionResult Buy(Order oder)
         {
+            //1.判断用户登录凭据是否过期，如果过期跳转回登录页，登录成功后返回确认购买页
+
+            //2.读出当前用户Person
+
+            //3.从会话中读出订单明细列表
+
+            //4.如果表单验证通过，则保存order到数据库（锁定进程），跳转到Pay/AliPay  
+
+            //5.如果验证不通过，返回视图
             return View();
         }
 
