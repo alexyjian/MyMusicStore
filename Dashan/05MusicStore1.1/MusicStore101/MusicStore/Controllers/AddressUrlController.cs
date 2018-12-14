12e447035b19e7fa323075b747b07367a7893e48
suweixi <1985748697@qq.com>
2018-12-14 18:12:04 +0800

    设置收件人地址

---
+++ Dashan/05MusicStore1.1/MusicStore101/MusicStore/Controllers/AddressUrlController.cs
@@ -0,0 +1,21 @@
+﻿using MusicStore.ViewMoldels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class AddressUrlController : Controller
+    {
+         /// <summary>
+        ///设置收件人地址
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }  
+    }
+}
