234d2996b82d303e702a759b1b4861569fa6cc50
1123781921 <1123781921@qq.com>
2018-12-14 18:06:59 +0800

    12-14

---
+++ lql139/ASP.NET mvc/MusicStore1.1/MusicStore101/MusicStore/ViewModels/UpdatePersonalViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.ViewModels
+{
+    public class UpdatePersonalViewModel
+    {
+        [Display(Name = "名称")]
+        [Required(ErrorMessage = "名称不能为空")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "电话号码不能为空")]
+        [Display(Name = "电话号码")]
+        public string MobileNumber { get; set; }
+
+
+        [Display(Name = "邮箱")]
+        [Required(ErrorMessage = "邮箱不能为空")]
+        public string Email { get; set; }
+    }
+}
