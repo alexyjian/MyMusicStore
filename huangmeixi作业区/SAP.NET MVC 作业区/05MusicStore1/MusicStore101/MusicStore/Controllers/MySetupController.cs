7bcd6a9a9442e47cf80995adca8e5adda4cb5b15
huangmeixiqi <1600582963@qq.com>
2018-12-14 22:39:07 +0800

    设置12.14

---
+++ huangmeixi作业区/SAP.NET MVC 作业区/05MusicStore1/MusicStore101/MusicStore/Controllers/MySetupController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStoreEntity;
+using MusicStoreEntity.UserAndRole;
+using MusicStore.ViewModels;
+
+namespace MusicStore.Controllers
+{
+    public class MySetupController : Controller
+    {
+        private static EntityDbContext _context = new EntityDbContext();
+       
+        public ActionResult Index()
+        {
+            //当前登录验证
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Index", "Order") });
+            //当前用户
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+
+
+            return View("MySetup");
+        }
+
+    }
+}
---
+++ huangmeixi作业区/SAP.NET MVC 作业区/05MusicStore1/MusicStore101/MusicStoreEntity/UserAdd.cs
@@ -0,0 +1,20 @@
+﻿using MusicStoreEntity.UserAndRole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStoreEntity
+{
+    public class UserAdd
+    {
+        public Guid ID { get; set; }
+        public virtual Person Person { get; set; }
+        public string OrderAdd { get; set; }
+        public UserAdd()
+        {
+            ID = Guid.NewGuid();
+        }
+    }
+}
