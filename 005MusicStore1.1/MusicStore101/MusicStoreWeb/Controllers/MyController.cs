d6aa2694eb1aba8011337aea2dcdd38ecb9ea652
2876416732 <723817914@qq.com>
2018-12-16 18:03:19 +0800

    完成个人信息修改，头像更换

--- 005MusicStore1.1/MusicStore101/MusicStoreWeb/Controllers/MyController.cs
+++ 005MusicStore1.1/MusicStore101/MusicStoreWeb/Controllers/MyController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using MusicStore.ViewModels;
 using MusicStoreEntity;
+using System.IO;
 
 namespace MusicStore.Controllers
 {
@@ -30,6 +31,42 @@ namespace MusicStore.Controllers
             ViewBag.AvardaUrl = person.Avarda;
 
             return View(myVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(MyViewModel model)
+        {
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Index", "My") });
+            var person = _context.Persons.Find((Session["LoginUserSessionModel"] as LoginUserSessionModel).Person.ID);
+            //用户原来的头像
+            var oldAvarda = person.Avarda;
+            if (ModelState.IsValid)
+            {
+                //保存头像
+                if (model.Avarda != null)
+                {
+                    var uploadDir = "~/Upload/Avarda/";
+                    //取后缀名
+                    var fileLastName = model.Avarda.FileName.Substring(model.Avarda.FileName.LastIndexOf(".") + 1,
+                        (model.Avarda.FileName.Length - model.Avarda.FileName.LastIndexOf(".") - 1));
+                    var imagePath = Path.Combine(Server.MapPath(uploadDir), person.ID + "." + fileLastName);  //将网站虚拟路径转化为真实的物理路径
+                    model.Avarda.SaveAs(imagePath);
+                    oldAvarda = "/Upload/Avarda/" + person.ID + "." + fileLastName;
+                }
+                //保存个人信息
+                person.MobileNumber = model.MobilNumber;
+                person.Address = model.Address;
+                person.Name = model.Name;
+                person.FirstName = person.Name.Substring(0, 1);
+                person.LastName = person.Name.Substring(1, person.Name.Length - 1);
+                person.Avarda = oldAvarda;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AvardaUrl = oldAvarda;
+            return View();
         }
     }
 }
