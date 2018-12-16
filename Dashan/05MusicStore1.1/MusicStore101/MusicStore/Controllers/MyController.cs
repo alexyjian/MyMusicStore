930ccd84e364d9cdb578141ae868b1e8a7e28032
suweixi <1985748697@qq.com>
2018-12-16 18:11:49 +0800

    修改头像

--- Dashan/05MusicStore1.1/MusicStore101/MusicStore/Controllers/MyController.cs
+++ Dashan/05MusicStore1.1/MusicStore101/MusicStore/Controllers/MyController.cs
@@ -3,6 +3,7 @@ using MusicStore.ViewMoldels;
 using MusicStoreEntity;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -20,7 +21,6 @@ namespace MusicStore.Controllers
         /// <returns></returns>
         public ActionResult Index()
         {
-
             //确认用户是否登录 是否登录过期
             if (Session["loginUserSessionModel"] == null)
             return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Index", "My") });
@@ -35,6 +35,49 @@ namespace MusicStore.Controllers
             ViewBag.AvardaUrl = person.Avarda;
 
           return View(myVM);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(MyViewModel model)
+        {
+            if (Session["loginUserSessionModel"] == null)
+            return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Index", "My") });
+
+            var person =_context.Persons.Find((Session["LoginUserSessionModel"] as LoginUserSessionModel).Person.ID);
+
+            //用户原来的头像
+            var oldAvarda = person.Avarda ;
+
+            if (ModelState.IsValid)
+            {
+                //保存头像
+                if( model.Avarda !=null)
+                {
+                    var uploadDir = "~/Upload/Avarda/";
+                    //取后缀名
+                    var fileLastName = model.Avarda.FileName.Substring
+                        (model.Avarda.FileName.LastIndexOf(".") + 1,
+                        (model.Avarda.FileName.Length - model.Avarda.FileName.LastIndexOf(".") - 1));
+                    var imagePath = Path.Combine(Server.MapPath(uploadDir), person.ID + "." + fileLastName);
+                    model.Avarda.SaveAs(imagePath);
+                    oldAvarda = " /Upload/Avarda/ " + person.ID + "." + fileLastName;
+                }
+
+                // 保存个人信息
+                person.MobileNumber = model.MobilNumber;
+                person.Address = model.Address;
+                person.Name = model.Name;
+                person.FirstName = person.Name.Substring(0, 1);
+                person.LastName = person.Name.Substring(1, person.Name.Length - 1);
+                person.Avarda = oldAvarda;
+
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AvardaUrl = oldAvarda;
+            return View();
         }
     }
 }
