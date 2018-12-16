94aa0213a9077b641c8ed28bd5683941b2bcb448
suweixi <1985748697@qq.com>
2018-12-16 15:59:20 +0800

    我的收获地址

---
+++ Dashan/05MusicStore1.1/MusicStore101/MusicStore/Controllers/MyController.cs
@@ -0,0 +1,40 @@
+﻿
+using MusicStore.ViewMoldels;
+using MusicStoreEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class MyController : Controller
+    {
+
+        private static readonly EntityDbContext _context = new EntityDbContext();
+
+        /// <summary>
+        /// 我的收获地址
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+
+            //确认用户是否登录 是否登录过期
+            if (Session["loginUserSessionModel"] == null)
+            return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Index", "My") });
+
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+                 var myVM= new MyViewModel()
+                {
+                     Name = person .Name,
+                    Address = person.Address,
+                    MobilNumber = person.MobileNumber  
+                };
+            ViewBag.AvardaUrl = person.Avarda;
+
+          return View(myVM);
+        }
+    }
+}
