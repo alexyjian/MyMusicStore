cd70e2b2e12a1dd68f84d56d17f34599fc477ad9
Administrator <Administrator@ts70842>
2018-12-16 15:57:09 +0800

    收件人设置页面

---
+++ 我的作业(后端)/05MusicStore1.1/MusicStore101/MusicStore/Controllers/MyController.cs
@@ -0,0 +1,31 @@
+﻿using MusicStoreEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MusicStore.ViewModels;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class MyController : Controller
+    {
+        private static readonly EntityDBContext _context = new EntityDBContext();
+        // 修改个人信息
+        public ActionResult Index()
+        {
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new {returnUrl=Url.Action("Index","My")});
+
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+            var myVM = new MyViewModel()
+            {
+                Name = person.Name,
+                Address = person.Address,
+                MobilNumber = person.MobileNumber
+            };
+            ViewBag.AvardaUrl = person.Avarda;
+              return View(myVM);
+        }
+    }
+}
---
+++ 我的作业(后端)/05MusicStore1.1/MusicStore101/MusicStore/ViewModels/MyViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.ViewModels
+{
+    /// <summary>
+    /// 用户信息视图模型
+    /// </summary>
+    public class MyViewModel
+    {
+        [Display(Name ="收件人姓名")]
+        public string Name { get; set; }
+
+        [Display(Name = "收件人地址")]
+        public string Address { get; set; }
+
+        [Display(Name = "收件人手机")]
+        public string MobilNumber { get; set; }
+
+        [Display(Name = "头像")]
+        [DataType(DataType.Upload)]
+        public HttpPostedFileBase Avarda { get; set; }
+    }
+}
