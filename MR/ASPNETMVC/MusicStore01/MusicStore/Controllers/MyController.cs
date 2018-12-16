04174df361dba13938349169ac8224a220f57b42
HappyWindManS <3066004354@qq.com>
2018-12-16 15:46:51 +0800

    708Study;12/16,15:46[my]

---
+++ MR/ASPNETMVC/MusicStore01/MusicStore/Controllers/MyController.cs
@@ -0,0 +1,35 @@
+﻿using MusicStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class MyController : Controller
+    {
+        private static readonly EntityDbContext _context = new EntityDbContext();
+
+        // GET: My
+        public ActionResult Index()
+        {
+            //1.判断用户登录凭据是否过期，如果过期跳转回登录页，登录成功后返回确认购买页
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Index", "My") });
+
+            //2.读出当前用户person
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+            var myVM = new MyViewModel()
+            {
+                Name = person.Name,
+                Address = person.Address,
+                TelePhoneNumber = person.TelephoneNumber
+
+            };
+            ViewBag.AVARDAuRL = person.Avarda;
+
+            return View(myVM);
+        }
+    }
+}
---
+++ MR/ASPNETMVC/MusicStore01/MusicStore/ViewModels/MyViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore.ViewModels
+{
+    public class MyViewModel
+    {
+        [Display(Name = "姓名")]
+        public string Name { get; set; }
+
+        //性别验证
+        [Display(Name = "性别")]
+        public bool sex { get; set; }
+
+        //电话号码验证
+        [Display(Name = "手机号")]
+        public string TelePhoneNumber { get; set; }
+
+        //邮箱验证
+        [Display(Name = "邮箱")]
+        public string Email { get; set; }
+
+        //收货地址验证
+        [Display(Name = "收货地址")]
+        public string Address { get; set; }
+
+        //生日     
+        [Display(Name = "生日")]
+        public DateTime Birthay { get; set; }
+
+        [Display(Name="头像")]
+        [DataType(DataType.Upload)]
+        public HttpPostedFileBase Avarda { get; set; }
+    }
+}
