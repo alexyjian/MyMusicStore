6b3be55eb4aab9a7440644fac94d4e4c1e7742a6
kawayi1727 <1163695520>
2018-12-16 10:26:48 +0800

    完成上传头像

--- chenyuxiu/MusicStore101/MusicStoreEntity/UserAndRole/Person.cs
+++ chenyuxiu/MusicStore101/MusicStoreEntity/UserAndRole/Person.cs
@@ -49,6 +49,14 @@ namespace MusicStoreEntity.UserAndRole
         [StringLength(50)]
         public string InquiryPassword { get; set; } // 查询密码，仅仅用于查询是否已经已经建立数据
         
+        //收件人地址
+        public string Address { get; set; }
+
+        //个人头像
+        [DataType(DataType.ImageUrl)]
+        public string Avarda { get; set; } = "/content/images/boy.jpg";
+
+
         public Person()
         {
             this.ID = Guid.NewGuid();
