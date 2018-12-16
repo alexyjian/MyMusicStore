e9bbb30f73718b06dda3c9642cd31c5a91eb9fa6
Seezhonghuai <1875680955@qq.com>
2018-12-16 15:40:23 +0800

    12-16-订单

---
+++ zhonghuai/MusicStore1.1/MusicStore101/MusicStore101/Controllers/MyController.cs
@@ -0,0 +1,35 @@
+﻿using MusicStore101.ViewModels;
+using MusicStoreEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore101.Controllers
+{
+    public class MyController : Controller
+    {
+        private static readonly EntityDbContext _context = new EntityDbContext();
+        // 修改个人信息
+        public ActionResult Index()
+        {
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Buy", "Order") });
+
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+
+
+            var myVm = new MyViewModel()
+            {
+                Name = person.Name,
+                Address = person.Address,
+                MobilNumber = person.MobileNumber
+            };
+            ViewBag.AvardaUrl = person.Avarda;
+
+            return View(myVm);
+
+        }
+    }
+}
