0b19a154411fbf7f23bb8a319778b54a476cddf4
luyusheng7 <2278814961@qq.com>
2018-12-16 18:12:00 +0800

    完成个人信息、头像修改，作业：修改性别、生日

--- luyusheng/05MusicStore1.1/MusicStore101/MusicStore/Controllers/MyController.cs
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStore/Controllers/MyController.cs
@@ -1,6 +1,8 @@
-﻿using MusicStoreEntity;
+﻿using MusicStore.ViewModels;
+using MusicStoreEntity;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,13 +13,63 @@ namespace MusicStore.Controllers
     {
         private static readonly EntityDbContext _context = new EntityDbContext();
 
+        //修改个人信息
         public ActionResult Index()
         {
             //检查用户是否登录 登录是否过期
             if (Session["LoginUserSessionModel"] ==null)
-                return RedirectToAction("login","Account",new {returUrl =Url.Action("Index","Order")});
+                return RedirectToAction("login","Account",new {returUrl =Url.Action("Index","My")});
 
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+            var myVM = new MyViewModel()
+            {
+                Name = person.Name,
+                Address = person.Address,
+                MobilNumber = person.MobileNumber
+            };
+            ViewBag.AvardaUrl = person.Avarda;
 
+            return View(myVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(MyViewModel model)
+        {
+            if(Session["LoginUserSessionModel"] ==null)
+                return RedirectToAction("login","Account", new{returUrl =Url.Action("Index","My")});
+
+            var person =_context.Persons.Find((Session["LoginUserSessionModel"]as LoginUserSessionModel).Person.ID);
+            //用户原来的头像
+            var oldAvarda = person.Avarda;
+            if (ModelState.IsValid)
+            {
+                //保存头像
+                if(model.Avarda != null)
+                {
+                    var uploadDir = "~/Upload/Avarda/";
+                    //
+                    //var fileLastName = model.Avarda.FileName.Substring(model.Avarda.FileName.LastIndexOf(".") + 1,
+                    //    (model.Avarda.FileName.Length - model.Avarda.FileName.LastIndexOf(".") - 1));
+
+                    //将网站虚拟路径转化为真实的物理路径
+                    var imagePath = Path.Combine(Server.MapPath(uploadDir),person.ID + ".jpg");
+                    model.Avarda.SaveAs(imagePath);
+                    oldAvarda = "/Upload/Avarda/" + person.ID + ".jpg";
+                }
+
+                //保存个人信息
+                person.MobileNumber = model.MobilNumber;
+                person.Address = model.Address;
+                person.Name = model.Name;
+                person.FirstName = person.Name.Substring(0, 1);
+                person.LastName = person.Name.Substring(1, person.Name.Length - 1);
+                person.Avarda = oldAvarda;
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            ViewBag.AvardaUrl = oldAvarda;
             return View();
         }
     }
