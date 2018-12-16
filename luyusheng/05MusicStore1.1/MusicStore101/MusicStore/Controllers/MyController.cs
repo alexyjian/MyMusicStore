b551db74f38a43ccf9257a37f96344d4a45326d8
luyusheng7 <2278814961@qq.com>
2018-12-16 17:38:08 +0800

    我的视图

---
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStore/Controllers/MyController.cs
@@ -0,0 +1,24 @@
+﻿using MusicStoreEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class MyController : Controller
+    {
+        private static readonly EntityDbContext _context = new EntityDbContext();
+
+        public ActionResult Index()
+        {
+            //检查用户是否登录 登录是否过期
+            if (Session["LoginUserSessionModel"] ==null)
+                return RedirectToAction("login","Account",new {returUrl =Url.Action("Index","Order")});
+
+
+            return View();
+        }
+    }
+}
