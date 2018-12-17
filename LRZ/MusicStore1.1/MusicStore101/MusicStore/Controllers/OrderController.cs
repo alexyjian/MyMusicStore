44e78af74bb113db0c4b7cc1ef5906c49e4d207c
lrz991222 <353226337@qq.com>
2018-12-17 17:43:17 +0800

    完成订单列表

--- LRZ/MusicStore1.1/MusicStore101/MusicStore/Controllers/OrderController.cs
+++ LRZ/MusicStore1.1/MusicStore101/MusicStore/Controllers/OrderController.cs
@@ -168,7 +168,15 @@ namespace MusicStore.Controllers
         /// <returns></returns>
         public ActionResult Index()
         {
-            return View();
+            //是否登录
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new { returnUrl = Url.Action("Index", "Order") });
+
+            //查询该用户的订单列表
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+            var orders = _context.Orders.Where(x => x.Person.ID == person.ID).ToList();
+
+            return View(orders);
         }
     }
 }
