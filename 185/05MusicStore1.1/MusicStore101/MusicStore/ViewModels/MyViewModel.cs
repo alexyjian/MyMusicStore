aec52ffbf88e93facdf846ce206b2c73d5658689
WaxdM <1575908620@qq.com>
2018-12-17 17:52:39 +0800

    12.17不成功的定位

--- 185/05MusicStore1.1/MusicStore101/MusicStore/ViewModels/MyViewModel.cs
+++ 185/05MusicStore1.1/MusicStore101/MusicStore/ViewModels/MyViewModel.cs
@@ -13,6 +13,10 @@ namespace MusicStore.ViewModels
         [Required (ErrorMessage="收件人姓名不能为空")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "用户名不能为空")]
+        [Display(Name = "性别：")]//性别
+        public bool Sex { get; set; } // 性别
+
         [Display(Name = "收货地址")]
         [Required(ErrorMessage = "收货地址不能为空")]
         public string Address { get; set; }
