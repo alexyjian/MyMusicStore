0921e03d988247c33218d658cb03502a5d6b8c35
Pretty33 <321838579@qq.com>
2018-12-19 18:13:50 +0800

    20181219  添加评论的实体

---
+++ WAnnie/05MusiceStore1.1/MusicStore101/MusicStoreEntity/Reply.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicStoreEntity.UserAndRole;
+
+namespace MusicStoreEntity
+{
+    /// <summary>
+    /// 评论的实体
+    /// </summary>
+  public  class Reply
+    {
+        public Guid ID { get; set; }
+
+        [Display(Name = "标题")]
+        [Required]
+        public virtual string Title { get; set; }
+
+        [Display(Name = "内容")]
+        [Required]
+        public virtual string Content { get; set; }
+
+        [Required]
+        public virtual Ablum Ablum { get; set; }
+
+        [Required]
+        public  virtual Person Person { get; set; }
+
+        //上级回复
+        [Required]
+        public virtual Reply ParentReply { get; set; }
+
+
+        //回复时间
+        public DateTime CreateDateTime { get; set; }
+
+        public Reply()
+        {
+            ID=Guid.NewGuid();
+            CreateDateTime=DateTime.Now;
+        }
+    }
+}
