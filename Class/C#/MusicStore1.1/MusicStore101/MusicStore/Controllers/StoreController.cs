326041fa1333172a79783584ba5d81c66c1b6785
ChaoPiPi <1806616500@qq.com>
2018-12-20 09:06:59 +0800

    点赞

--- Class/C#/MusicStore1.1/MusicStore101/MusicStore/Controllers/StoreController.cs
+++ Class/C#/MusicStore1.1/MusicStore101/MusicStore/Controllers/StoreController.cs
@@ -11,7 +11,11 @@ namespace MusicStore.Controllers
     public class StoreController : Controller
     {
         private static readonly EntityDbContext _dbContext = new EntityDbContext();
-        // GET: Store
+        /// <summary>
+        /// 明细页
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public ActionResult Detail(Guid id)
         {
             var detail = _dbContext.Albums.Find(id);
@@ -20,21 +24,37 @@ namespace MusicStore.Controllers
             return View(detail);
         }
 
+        /// <summary>
+        /// 分类页
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public ActionResult Browser(Guid id)
         {
             var list = _dbContext.Albums.Where(x => x.Genre.ID == id).OrderByDescending(x => x.PublisherDate).ToList();
             return View(list);
         }
 
+        /// <summary>
+        /// 评论页
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
         public ActionResult Commit(string content)
         {
             return View();
         }
 
+        /// <summary>
+        /// 点赞
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPost]
         public ActionResult Zan(Guid id)
         {
             if (Session["LoginUserSessionModel"] == null)
-                return RedirectToAction("Login", "Account", new { returnUrl = Url.Action("Index", "Order") });
+                return RedirectToAction("Login", "Account", new { returnUrl = Url.Action("Index", "Home") });
 
             var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
 
@@ -42,7 +62,7 @@ namespace MusicStore.Controllers
             commentary.ThumbsUp++;
             _dbContext.SaveChanges();
             var htmlString = "";
-            htmlString = "<span class='glyphicon glyphicon-thumbs - up'></span>&nbsp;&nbsp;( " + commentary.ThumbsUp + " )";
+            htmlString = "<span class='glyphicon glyphicon-thumbs-up'></span>&nbsp;&nbsp;( " + commentary.ThumbsUp + " )";
             return Json(htmlString);
         }
     }
