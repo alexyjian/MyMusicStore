2d120579d1aa1ea6460dfb0d6ed0787d1faec421
Administrator <Administrator@ts70842>
2018-12-20 11:22:33 +0800

    试听功能完成

---
+++ 我的作业(后端)/05MusicStore1.1/MusicStore101/MusicStoreEntity/Reply.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using MusicStoreEntity.UserAndRole;
+
+
+namespace MusicStoreEntity
+{
+    /// <summary>
+    /// 回复
+    /// </summary>
+   public  class Reply
+    {
+        public Guid ID { get; set; }
+
+        [Display(Name = "标题")]
+        [Required]
+        public virtual string Title { get; set; }
+
+        [Display(Name = "内容")]
+        [Required]
+        public virtual string Content { get; set; }
+
+        [Required]
+        public virtual Person Person { get; set; }
+
+        [Required]
+        public virtual Album Album { get; set; }
+
+        [Required]
+        public virtual Reply ParentReply { get; set; }   //上级回复
+
+        public DateTime CreateDateTime { get; set; }  //回复时间
+
+        public Reply()
+        {
+            ID = Guid.NewGuid();
+            CreateDateTime = DateTime.Now;
+        }
+    }
+}
