5fc3a071d54f585a9de6855220946ec926c2d351
luyusheng7 <2278814961@qq.com>
2018-12-21 12:12:17 +0800

    完成试听功能

--- luyusheng/05MusicStore1.1/MusicStore101/MusicStoreEntity/Reply.cs
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStoreEntity/Reply.cs
@@ -1,10 +1,10 @@
-﻿using MusicStoreEntity.UserAndRole;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MusicStoreEntity.UserAndRole;
+using System.ComponentModel.DataAnnotations;
 
 namespace MusicStoreEntity
 {
@@ -13,10 +13,9 @@ namespace MusicStoreEntity
     /// </summary>
    public class Reply
     {
-       
         public Guid ID { get; set; }
 
-        [Display(Name = "标题")]
+        [Display(Name ="标题")]
         [Required]
         public virtual string Title { get; set; }
 
@@ -25,15 +24,15 @@ namespace MusicStoreEntity
         public virtual string Content { get; set; }
 
         [Required]
-        public virtual Person Person { get; set; }
+        public virtual  Person Person { get; set; }
 
         [Required]
         public virtual Album Album { get; set; }
 
         [Required]
-        public virtual Reply  ParentReply { get; set; }
+        public virtual Reply ParentReply { get; set; } //上级回复
 
-        public DateTime CreateDateTime { get; set; }
+        public DateTime CreateDateTime { get; set; } //回复时间
 
         public Reply()
         {
