fcfe018df6810e16ad53ed30961db63ac189573b
Pretty33 <321838579@qq.com>
2018-12-26 18:12:46 +0800

    20181226 修改富文本编译器目录

--- WAnnie/05MusiceStore1.1/MusicStore101/MusicStore/ueditor/ueditor.config.js
+++ WAnnie/05MusiceStore1.1/MusicStore101/MusicStore/ueditor/ueditor.config.js
@@ -34,8 +34,12 @@
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
         , toolbars: [[
-            'emotion', //表情
+            'simpleupload', //单图上传
+            'emotion',//表情
             'spechars', //特殊字符
+            'insertvideo', //视频
+            'insertimage', //多图上传
+            'scrawl' //涂鸦
         ]]
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
         //,labelMap:{
