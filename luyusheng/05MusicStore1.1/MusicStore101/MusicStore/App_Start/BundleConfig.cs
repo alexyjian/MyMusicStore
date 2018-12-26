0231aa053a52aee34dc3aa0840b7a00a19f0a8c1
luyusheng7 <2278814961@qq.com>
2018-12-26 18:10:55 +0800

    完成布局和页首

--- luyusheng/05MusicStore1.1/MusicStore101/MusicStore/App_Start/BundleConfig.cs
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStore/App_Start/BundleConfig.cs
@@ -27,6 +27,16 @@ namespace MusicStore
                       "~/Content/bootstrap.css",
                       "~/Content/Site.css"));
 
+            //绑定Ueditor的样式
+            bundles.Add(new StyleBundle("~/UEditor/css").Include(
+               "~/ueditor/themes/iframe.css"));
+
+            //绑定Ueditor的脚本
+            bundles.Add(new ScriptBundle("~/bundles/UEditor").Include(
+                "~/ueditor/ueditor.config.js",
+                "~/ueditor/ueditor.all.js",
+                "~/ueditor/ueditor.all.min.js"));
+
             //绑定JqueryUI的样式库
             bundles.Add(new StyleBundle("~/Content/JqueryUI").Include(
                 "~/Content/themes/base/jquery-ui.css"));
