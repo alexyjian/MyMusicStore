b958da4e06d94ca2ae9de66a1d5b85b69415db53
kawayi1727 <1163695520>
2018-12-27 12:11:11 +0800

    上级回复完成

--- chenyuxiu/MusicStore101/MusicStore/Controllers/StoreController.cs
+++ chenyuxiu/MusicStore101/MusicStore/Controllers/StoreController.cs
@@ -19,13 +19,48 @@ namespace MusicStore.Controllers
         /// <param name="id"></param>
         /// <returns></returns>
         public ActionResult Detail(Guid id)
-        {             
+        {
             var detail = _context.Albums.Find(id);
+            var cmt = _context.Reply.Where(x => x.Album.ID == id && x.ParentReoly == null)
+                .OrderByDescending(x => x.CreateDateTime).ToList();
+            ViewBag.Cmt = _GetHtml(cmt);
             return View(detail);
         }
+
+        /// <summary>
+        /// 生成回复的显示html文本
+        /// </summary>
+        /// <param name="cmt"></param>
+        private string _GetHtml(List<Reply>cmt)
+        {
+            var htmlString = "";
+            htmlString += "<ul class='media-list'>";
+            foreach (var iteme in cmt)
+            {
+                htmlString += "<li class='media'>";
+                htmlString += "<div class='media-left>";
+                htmlString += "<img class='media-object'src='"+iteme.Person.Avarda+
+                    "'alt='头像'style='width:40px;border-radius:50%;'>";
+                htmlString += "</div>";
+                htmlString += "<div class='media-body'>";
+                htmlString += "<h5 class='media-hesding'>" + iteme.Person.Name + "发表于" +
+                    iteme.CreateDateTime.ToString("yyy年MM月dd日 hh点mm分ss秒") + "</h5>";
+                htmlString += iteme.Content;
+                //查询当前回复的下一级回复
+                var sonCmt =_context.Reply.Where(x => x.ParentReoly.ID == iteme.ID).ToList();
+                htmlString +="<h6>回复(<a href='#'>"+ sonCmt.Count + "</a>)条" + "  <a href='#'><i class='glyphicon glyphicon-thumbs-up'></i></a>(" +
+iteme.Like + ")  <a href='#'><i class='glyphicon glyphicon-thumbs-down'></i></a>(" + iteme.Hate + ")</h6>";
+                htmlString += "</div>";
+                htmlString += "</li>";
+            }
+            htmlString += "</ul>";
+            return htmlString;
+        }
+
+
         [HttpPost]
-        [ValidateInput(false )] //关闭验证
-        public ActionResult AddCmt(string id,string cmt,string  reply)
+        [ValidateInput(false)] //关闭验证
+        public ActionResult AddCmt(string id, string cmt, string reply)
         {
 
             if (Session["LoginUserSessionModel"] == null)
@@ -39,13 +74,13 @@ namespace MusicStore.Controllers
             var r = new Reply()
             {
                 Album = alnum,
-                Person =person,
-                Content=cmt,
+                Person = person,
+                Content = cmt,
             };
             //父级回复
-            if(string .IsNullOrEmpty(reply))
+            if (string.IsNullOrEmpty(reply))
             {
-                //顶级回复
+                //顶级回复.ParentReply为空
                 r.ParentReoly = null;
             }
             else
@@ -54,10 +89,15 @@ namespace MusicStore.Controllers
             }
             _context.Reply.Add(r);
             _context.SaveChanges();
+
+            //局部刷新显示成最新的评论
+            var reolies = _context.Reply.Where(x => x.Album.ID == alnum.ID&& x.ParentReoly == null)
+               .OrderByDescending(x => x.CreateDateTime).ToList();
+
+
             return Json("OK");
 
         }
-
 
         /// <summary>
         /// 按分类显示专辑
