9349bf9aeca1dd15fb2bf70d36a8726cc9031c26
luyusheng7 <2278814961@qq.com>
2018-12-27 11:13:22 +0800

    样式调整

--- luyusheng/05MusicStore1.1/MusicStore101/MusicStore/Controllers/StoreController.cs
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStore/Controllers/StoreController.cs
@@ -1,4 +1,5 @@
-﻿using MusicStoreEntity;
+﻿using MusicStore.ViewModels;
+using MusicStoreEntity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,14 +19,76 @@ namespace MusicStore.Controllers
         public ActionResult Detail(Guid id)
         {
             var detail = _context.Albums.Find(id);
+            var cmt = _context.Replys.Where(x => x.Album.ID == id && x.ParentReply == null)
+             .OrderByDescending(x => x.CreateDateTime).ToList();
+            ViewBag.Cmt = _GetHtml(cmt);
             return View(detail);
+        }
+
+        /// <summary>
+        /// 生成回复的显示html文本
+        /// </summary>
+        /// <param name="cmt"></param>
+        /// <returns></returns>
+        private string _GetHtml(List<Reply> cmt)
+        {
+            var htmlString = "";
+            htmlString += "<ul class='media-list'>";
+            foreach (var item in cmt)
+            {
+                htmlString += "<li class='media'>";
+                htmlString += "<div class='media-left'>";
+                htmlString += "<img class='media-object' src='" + item.Person.Avarda +
+                              "' alt='头像' style='width:40px;border-radius:50%;'>";
+                htmlString += "</div>";
+                htmlString += "<div class='media-body'>";
+                htmlString += "<h5 class='media-heading'>" + item.Person.Name + "  发表于" +
+                              item.CreateDateTime.ToString("yyyy年MM月dd日 hh点mm分ss秒") + "</h5>";
+                htmlString += item.Content;
+                //查询当前回复的下一级回复
+                var sonCmt = _context.Replys.Where(x => x.ParentReply.ID == item.ID).ToList();
+                htmlString += "<h6>回复(<a href='#'>" + sonCmt.Count + "</a>)条" + "  <a href='#'><i class='glyphicon glyphicon-thumbs-up'></i></a>(" +
+                              item.Like + ")  <a href='#'><i class='glyphicon glyphicon-thumbs-down'></i></a>(" + item.Hate + ")</h6>";
+                htmlString += "</div>";
+                htmlString += "</li>";
+            }
+            htmlString += "</ul>";
+            return htmlString;
         }
 
         [HttpPost]
         [ValidateInput(false)] //关闭验证
-        public ActionResult AddCmt(string id,string cmt,string reply)
+        public ActionResult AddCmt(string id, string cmt, string reply)
         {
+            if (Session["LoginUserSessionModel"] == null)
+                return Json("nologin");
 
+            var person = _context.Persons.Find((Session["LoginUserSessionModel"] as
+                LoginUserSessionModel).Person.ID);
+            var album = _context.Albums.Find(Guid.Parse(id));
+
+            //创建回复对象
+            var r = new Reply()
+            {
+                Album = album,
+                Person = person,
+                Content = cmt,
+                Title = ""
+            };
+            //父级回复
+            if (string.IsNullOrEmpty(reply))
+            {
+                //顶级回复,ParentReply为空
+                r.ParentReply = null;
+            }
+            else
+            {
+                r.ParentReply = _context.Replys.Find(Guid.Parse(reply));
+            }
+            _context.Replys.Add(r);
+            _context.SaveChanges();
+
+            //局部刷新显示
             return Json("OK");
         }
 
@@ -37,7 +100,7 @@ namespace MusicStore.Controllers
         public ActionResult Browser(Guid id)
         {
             var list = _context.Albums.Where(x => x.Genre.ID == id)
-                .OrderByDescending(x =>x.PublisherDate).ToList();
+                .OrderByDescending(x => x.PublisherDate).ToList();
             return View(list);
         }
 
