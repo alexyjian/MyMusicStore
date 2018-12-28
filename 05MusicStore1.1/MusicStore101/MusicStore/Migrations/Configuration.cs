93c0f2ddafb2790fb7a74e39a0ae6dbb2b14aa92
hxm193514 <1935197914@qq.com>
2018-12-28 23:55:00 +0800

    解决冲突

--- 05MusicStore1.1/MusicStore101/MusicStore/Migrations/Configuration.cs
+++ 05MusicStore1.1/MusicStore101/MusicStore/Migrations/Configuration.cs
@@ -14,10 +14,10 @@ namespace MusicStore.Migrations
 
         protected override void Seed(MusicStoreEntity.EntityDbContext context)
         {
-            UserAndRoleSeed.AddRoles();
-            UserAndRoleSeed.AddUsers();
-            GenreSeed.Seed();
-            GenreSeed.Extend();
+            //UserAndRoleSeed.AddRoles();
+            //UserAndRoleSeed.AddUsers();
+            //GenreSeed.Seed();
+            //GenreSeed.Extend();
         }
     }
 }
