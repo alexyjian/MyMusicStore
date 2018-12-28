5e19ab4cee432eb146caec69b26b64164b5318e8
weitian666 <1935836331@qq.com>
2018-12-28 11:28:12 +0800

    利用bootstrap中的modal显示子回复

--- 韦添/C#/05MusicStore1.1/MusicStore101/MusicStore/Controllers/StoreController.cs
+++ 韦添/C#/05MusicStore1.1/MusicStore101/MusicStore/Controllers/StoreController.cs
@@ -49,7 +49,8 @@ namespace MusicStore.Controllers
                 htmlString += "</div>";
                 //查询当前回复的下一级回复
                 var sonCmt = _context.Replies.Where(x => x.ParentReply.ID == item.ID).ToList();
-                htmlString += "<h6><a href='#div-editor' class='reply' onclick=\"javascript:GetQuote('" + item.ID + "');\">回复</a>(<a href='#' class='reply'>" + sonCmt.Count + "</a>)条" +
+                htmlString += "<h6><a href='#div-editor' class='reply' onclick=\"javascript:GetQuote('" + item.ID +
+                              "');\">回复</a>(<a href='#' class='reply'  onclick=\"javascript:ShowCmt('" + item.ID + "');\">" + sonCmt.Count + "</a>)条" +
                               "<a href='#' class='reply' style='margin:0 20px 0 40px'><i class='glyphicon glyphicon-thumbs-up'></i>(" +
                               item.Like + ")</a><a href='#' class='reply' style='margin:0 20px'><i class='glyphicon glyphicon-thumbs-down'></i>(" + item.Hate + ")</a></h6>";
 
