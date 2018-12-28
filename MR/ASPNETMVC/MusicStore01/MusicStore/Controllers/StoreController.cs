466b2ecef80050bbfb4e73ca7127d98aca1b349f
HappyWindManS <3066004354@qq.com>
2018-12-28 11:42:49 +0800

    708study;12/28,11:42[评论]

--- MR/ASPNETMVC/MusicStore01/MusicStore/Controllers/StoreController.cs
+++ MR/ASPNETMVC/MusicStore01/MusicStore/Controllers/StoreController.cs
@@ -22,41 +22,51 @@ namespace MusicStore.Controllers
             var detail = _context.Albums.Find(id);
             var cmt = _context.Replies.Where(x => x.Album.ID == id && x.ParentReply == null)
                 .OrderByDescending(x => x.CreateDateTime).ToList();
+            string htmlString = _GetHtml(cmt);
+            ViewBag.Cmt = htmlString;
+            return View(detail);
+        }
+
+        private static string _GetHtml(List<Reply> cmt)
+        {
             var htmlString = "";
             htmlString += "<ul class='media-list'>";
-            foreach(var item in cmt)
+            foreach (var item in cmt)
             {
                 htmlString += "<li class='media'>";
-                htmlString += "<div class='media-left'";
-                htmlString += "<img class='media-objevt' src'" + item.Person.Avarda +
-                    "'alt='头像' style='width:40px;border-radius:50%;'>";
+                htmlString += "<div class='media-left'>";
+                htmlString += "<img class='media-object' src='" + item.Person.Avarda +
+                              "' alt='头像' style='width:40px;border-radius:50%;'>";
                 htmlString += "</div>";
                 htmlString += "<div class='media-body'>";
-                htmlString += "<h5 class='media-heading'>" + item.Person.Name + 
-                    "发表于" + item.CreateDateTime.ToString("yyyy年MM月dd日 hh点mm分ss秒") + "</h5>";
+                htmlString += "<div class='media-body' id='Content-" + item.ID + "'>";
+                htmlString += "<h5 class='media-heading'>" + item.Person.Name + "  发表于" +
+                              item.CreateDateTime.ToString("yyyy年MM月dd日 hh点mm分ss秒") + "</h5>";
                 htmlString += item.Content;
                 //查询当前回复的下一级回复
                 var sonCmt = _context.Replies.Where(x => x.ParentReply.ID == item.ID).ToList();
-                htmlString += "<h6>回复(<a href='#'>" + sonCmt.Count + "</a>)条" + "  <a href='#'><i class='glyphicon glyphicon-thumbs-up'></i></a>(" +
-                              item.Like + ")  <a href='#'><i class='glyphicon glyphicon-thumbs-down'></i></a>(" + item.Hate + ")</h6>";
+                htmlString += "<h6><a href='#' class='reply'onclick=\"javascript:GetQuote('" + item.ID + "');\">回复</a>(<a href='#' class='reply'>" + sonCmt.Count + "</a>)条" +
+                              "<a href='#' class='reply' style='margin:0 20px 0 40px'><i class='glyphicon glyphicon-thumbs-up'></i>(" +
+                              item.Like + ")</a><a href='#' class='reply' style='margin:0 20px'><i class='glyphicon glyphicon-thumbs-down'></i>(" + item.Hate + ")</a></h6>";
                 htmlString += "</div>";
                 htmlString += "</li>";
             }
             htmlString += "</ul>";
-            ViewBag.Cmt = htmlString;
-            return View(detail);
+            return htmlString;
         }
+
         [HttpPost]
         [ValidateInput(false)]
         public ActionResult AddCmt(string id,string cmt,string reply)
         {
             if (Session["LoginUserSessionModel"] == null)
                 return Json("nologin");
+
             var person = _context.Persons.Find((Session["LoginUserSessionModel"] as
                 LoginUserSessionModel).Person.ID);
-
             var album = _context.Albums.Find(Guid.Parse(id));
-            //创造回复对象
+
+            //创建回复对象
             var r = new Reply()
             {
                 Album = album,
@@ -65,18 +75,23 @@ namespace MusicStore.Controllers
                 Title = ""
             };
             //父级回复
-            if(string.IsNullOrEmpty(reply))
+            if (string.IsNullOrEmpty(reply))
             {
-                //顶级回复
+                //顶级回复, ParentReply为空
                 r.ParentReply = null;
             }
             else
             {
-                r.ParentReply=_context.Replies.Find(Guid.Parse(reply));
+                r.ParentReply = _context.Replies.Find(Guid.Parse(reply));
             }
+
             _context.Replies.Add(r);
             _context.SaveChanges();
-            return Json("OK");
+
+            //局部刷新显示成最新的评论
+            var replies = _context.Replies.Where(x => x.Album.ID == album.ID && x.ParentReply == null)
+                .OrderByDescending(x => x.CreateDateTime).ToList();
+            return Json(_GetHtml(replies));
         }
 
         /// <summary>
