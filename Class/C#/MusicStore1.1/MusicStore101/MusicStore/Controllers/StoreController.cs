0310225f07bc34daacd952e40950456b84596e87
ChaoPiPi <1806616500@qq.com>
2018-12-28 12:09:55 +0800

    点赞和回复待完善

--- Class/C#/MusicStore1.1/MusicStore101/MusicStore/Controllers/StoreController.cs
+++ Class/C#/MusicStore1.1/MusicStore101/MusicStore/Controllers/StoreController.cs
@@ -19,7 +19,9 @@ namespace MusicStore.Controllers
         public ActionResult Detail(Guid id)
         {
             var detail = _dbContext.Albums.Find(id);
-            ViewBag.AvardaUrl = Session["LoginUserSessionModel"] == null ? "/Content/Images/Body.jpg": (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person.Avarda;
+            ViewBag.AvardaUrl = Session["LoginUserSessionModel"] == null ? "/Content/Images/Body.jpg" : (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person.Avarda;
+            string str = NewMethod(_dbContext.Albums.Find(id));
+            ViewBag.Cmt = str == "" ? "<div class='text-center text-muted user-ment'>还没有评论呢，快来抢沙发~</div>" : str;
             return View(detail);
         }
 
@@ -41,44 +43,38 @@ namespace MusicStore.Controllers
         /// <returns></returns>
         [HttpPost]
         [ValidateInput(false)]
-        public ActionResult Commit(string id, string str,string reply)
+        public ActionResult Commit(string id, string str, string reply)
         {
             if (Session["LoginUserSessionModel"] == null)
                 return Json("nologin");
 
             var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
             var Album = _dbContext.Albums.Find(Guid.Parse(id));
-
-            if (reply == "")
+            var com = new Commentary()
             {
-                var com = new Commentary()
-                {
-                    Context = str,
-                    Album = Album,
-                    Person = _dbContext.Persons.Find(person.ID)
-                };
-                Album.Commentarys.Add(com);
-            }
-            else
-            {
-                var Reply = _dbContext.Commentarys.Find(Guid.Parse(reply));
-                var com = new Commentary()
-                {
-                    Context = str,
-                    Album = Album,
-                    Person = _dbContext.Persons.Find(person.ID),
-                    commentary = Reply
-                };
-                Album.Commentarys.Add(com);
-            }
-
+                Context = str,
+                Album = Album,
+                Person = _dbContext.Persons.Find(person.ID)
+            };
+            com.commentary = reply == "" ? null : _dbContext.Commentarys.Find(Guid.Parse(reply));
+            Album.Commentarys.Add(com);
             _dbContext.SaveChanges();
+            string htmlString = NewMethod(Album);
+            return Json(htmlString);
+        }
 
+        /// <summary>
+        /// 提取
+        /// </summary>
+        /// <param name="Album"></param>
+        /// <returns></returns>
+        private static string NewMethod(Album Album)
+        {
             var pls = _dbContext.Commentarys.Where(x => x.Album.ID == Album.ID).OrderByDescending(x => x.PublisherDate).ToList();
             string htmlString = "";
             foreach (var item in pls)
             {
-                htmlString += "<div class='pl-list'>";
+                htmlString += "<div id=" + item.ID + " class='pl-list'>";
                 htmlString += "<img class='pl-Avarda MyAvarda' src='" + @item.Person.Avarda + "' />";
                 htmlString += "<div class='pl-user'>";
                 htmlString += "<a href='#'>" + @item.Person.Name + "</a>：" + @item.Context;
@@ -88,10 +84,13 @@ namespace MusicStore.Controllers
                     htmlString += "<a href='#'>" + @item.commentary.Person.Name + "：</a>" + @item.commentary.Context + "</div>";
                 }
                 htmlString += "</div><p class='user-ment text-right'>";
-                htmlString += "<a id=" + @item.ID + " href ='#' onclick=" + '"' + "zan('" + @item.ID + "')" + '"' + "><span class='glyphicon glyphicon-thumbs-up'></span>&nbsp;&nbsp;( " + @item.ThumbsUp + " )</a><a href='javascript:;' class='hf' data-id="+'"'+item.ID+'"'+">回复</a>";
+                htmlString += "<a data-likeid=" + item.ID + " href ='#' onclick=\"zan('" + @item.ID
+                    + "')\"><span class='glyphicon glyphicon-thumbs-up'></span>&nbsp;&nbsp;( " + @item.ThumbsUp
+                    + " )</a><a href='javascript:;' class='hf' data-replyid=\"'" + item.ID + "'\" onclick=\"div_reply();\">回复</a>";
                 htmlString += "<span class='text-muted time'>" + @item.PublisherDate.ToString("MM月dd日 HH:mm") + "</span></p></div>";
             }
-            return Json(htmlString);
+
+            return htmlString;
         }
 
         /// <summary>
@@ -110,8 +109,8 @@ namespace MusicStore.Controllers
             var commentary = _dbContext.Commentarys.Find(id);
             commentary.ThumbsUp++;
             _dbContext.SaveChanges();
-            var htmlString = "";
-            htmlString = "<span class='glyphicon glyphicon-thumbs-up'></span>&nbsp;&nbsp;( " + commentary.ThumbsUp + " )";
+            var a = _dbContext.Commentarys.Find(id).Album;
+            var htmlString = NewMethod(a);
             return Json(htmlString);
         }
     }
