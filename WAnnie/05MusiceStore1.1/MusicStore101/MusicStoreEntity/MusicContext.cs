f875d063656d339d643126684b02eac0dae107e1
Pretty33 <321838579@qq.com>
2018-12-30 12:14:35 +0800

    20181230  点赞或踩的实体

--- WAnnie/05MusiceStore1.1/MusicStore101/MusicStoreEntity/MusicContext.cs
+++ WAnnie/05MusiceStore1.1/MusicStore101/MusicStoreEntity/MusicContext.cs
@@ -31,17 +31,16 @@ namespace MusicStoreEntity
          
         #region  音乐商店实体
 
-        public IDbSet<Genre> Genres { get; set;}
-        public IDbSet<Artist> Artists { get; set; }
-        public IDbSet<Ablum> Ablums { get; set; }
-
-        public IDbSet<Cart> Carts { get; set; }
+        public IDbSet<Genre> Genres { get; set; }  // 音乐分类
+        public IDbSet<Artist> Artists { get; set; } // 添加歌手
+        public IDbSet<Ablum> Ablums { get; set; } // 添加专辑
+        public IDbSet<Cart> Carts { get; set; } //购物车
 
 
         public IDbSet<Order> Orders { get; set; }  // 订单实体
         public IDbSet<OrderDetail> OrderDetails { get; set; }  // 订单明细
-
-        public IDbSet<Reply> Replys { get; set; }
+        public IDbSet<Reply> Replys { get; set; } //评论
+        public IDbSet<LikeReply> LikeReplies { get; set; } //点赞或踩
 
         #endregion
     }
