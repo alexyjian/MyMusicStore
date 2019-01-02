2be20842b57dfdc5f712cd797301d916d82f87f5
luyusheng7 <2278814961@qq.com>
2019-01-02 17:39:31 +0800

    点赞的框架

--- luyusheng/05MusicStore1.1/MusicStore101/MusicStoreEntity/EntityDbContext.cs
+++ luyusheng/05MusicStore1.1/MusicStore101/MusicStoreEntity/EntityDbContext.cs
@@ -32,6 +32,7 @@ namespace MusicStoreEntity
         public IDbSet<Genre> Genres { get; set; }
         public IDbSet<Artist> Artists { get; set; }
         public IDbSet<Album> Albums { get; set; }
+        public IDbSet<LikeReply> LikeReply { get; set; }
 
         public IDbSet<Cart> Carts { get; set; }
         public IDbSet<Order> Orders { get; set; }
