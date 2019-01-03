e6186899bc56af4c8ebb2776e824b2105a2a76e0
lrz991222 <353226337@qq.com>
2019-01-03 12:02:58 +0800

    赞

--- LRZ/MusicStore1.1/MusicStore101/MusicStoreEntity/EntityDbContext.cs
+++ LRZ/MusicStore1.1/MusicStore101/MusicStoreEntity/EntityDbContext.cs
@@ -34,6 +34,7 @@ namespace MusicStoreEntity
         public IDbSet<Artist> Artists { get; set; }
         public IDbSet<Album> Albums { get; set; }
         public IDbSet<Reply> Replies { get; set; }
+        public IDbSet<LikeReply> LikeReplies { get; set; }
 
         public IDbSet<Cart> Carts { get; set; }
         public IDbSet<Order> Orders { get; set; }
